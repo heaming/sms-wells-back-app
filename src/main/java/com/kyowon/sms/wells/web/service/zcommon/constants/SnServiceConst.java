83d043e0c568a17cc6fa1f74fa834e60f45bec2d
gs.piit122 <kdycorp@kyowon.co.kr>
2023-01-12 15:07:37 +0900

    fix : SnServiceConst 중복사용 제거

--- src/main/java/com/kyowon/sms/wells/web/service/zcommon/constants/SnServiceConst.java
+++ src/main/java/com/kyowon/sms/wells/web/service/zcommon/constants/SnServiceConst.java
@@ -6,6 +6,7 @@ public class SnServiceConst {
 
     private SnServiceConst() {}
 
+    @Deprecated
     public static final String REST_URL_WELLS_SERVICE = "/api/v1/sms/wells/service";
 
     public static final String REST_URL_V1 = CommConst.REST_URL_V1 + "/sms/wells/service";
