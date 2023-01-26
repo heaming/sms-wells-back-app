e2d00e41cd9671faed0113ff11752b5904513f15
MyoungBin Joo <gs.piit165@kyowon.co.kr>
2023-01-26 11:22:04 +0900

    fix: RequestBody, NotEmpty 추가

--- src/main/java/com/kyowon/sms/wells/web/contract/risk/rest/WctaContractController.java
+++ src/main/java/com/kyowon/sms/wells/web/contract/risk/rest/WctaContractController.java
@@ -2,9 +2,12 @@ package com.kyowon.sms.wells.web.contract.risk.rest;
 
 import java.util.List;
 
+import javax.validation.constraints.NotEmpty;
+
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,7 +30,7 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping(CtContractConst.REST_URL_V1 + "/approval-ask-divides")
 public class WctaContractController {
 
-    private final WctaContractService service;
+    private WctaContractService service;
 
     @ApiOperation(value = "승인 요청 구분 조회", notes = "기준일자로 유효시작, 종료일시 에 존재하며, 데이터삭제여부가 Y가 아닌확정승인기준 조회를 한다.")
     @ApiImplicitParams(value = {
@@ -43,7 +46,11 @@ public class WctaContractController {
 
     @ApiOperation(value = "승인 요청 리스트 삭제", notes = "계약약승인요청구분코드, 유효시작일시 존재여부 체크 후 삭제한다.")
     @DeleteMapping
-    public int removeApprovalAskDivides(List<RemoveReq> dtos) {
+    public int removeApprovalAskDivides(
+        @RequestBody
+        @NotEmpty
+        List<RemoveReq> dtos
+    ) {
         return service.removeApprovalAskDivides(dtos);
     }
 
