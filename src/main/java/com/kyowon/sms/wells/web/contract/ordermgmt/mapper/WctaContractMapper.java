3fe05a557ffa925dc723295c58016cd3fb4a3f9d
MyoungBin Joo <gs.piit165@kyowon.co.kr>
2023-02-01 15:16:39 +0900

    feat: wells 확정 승인 기준 관리 (W-SS-U-0045M01) 신규 개발

--- src/main/java/com/kyowon/sms/wells/web/contract/ordermgmt/mapper/WctaContractMapper.java
+++ src/main/java/com/kyowon/sms/wells/web/contract/ordermgmt/mapper/WctaContractMapper.java
@@ -4,10 +4,15 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.kyowon.sms.wells.web.contract.ordermgmt.dto.WctaContractDto.SearchConfirmApprovalBaseReq;
+import com.kyowon.sms.wells.web.contract.ordermgmt.dto.WctaContractDto.SearchConfirmApprovalBaseRes;
 import com.kyowon.sms.wells.web.contract.ordermgmt.dto.WctaContractDto.SearchConfirmAprPsicAksRes;
 import com.kyowon.sms.wells.web.contract.ordermgmt.dto.WctaContractDto.SearchConfirmAprPsicPrchssRes;
 import com.kyowon.sms.wells.web.contract.ordermgmt.dto.WctaContractDto.SearchRes;
 import com.kyowon.sms.wells.web.contract.ordermgmt.dvo.WctaCntrAprAkDvCdDvo;
+import com.kyowon.sms.wells.web.contract.ordermgmt.dvo.WctaCntrAprBaseBasDvo;
+import com.sds.sflex.system.config.datasource.PageInfo;
+import com.sds.sflex.system.config.datasource.PagingResult;
 
 @Mapper
 public interface WctaContractMapper {
@@ -17,5 +22,22 @@ public interface WctaContractMapper {
 
     public List<SearchConfirmAprPsicPrchssRes> selectConfirmAprPsicPrchss(String cntrNo);
 
-    public int deleteApprovalAskDivides(WctaCntrAprAkDvCdDvo dvos);
+    public List<SearchConfirmApprovalBaseRes> selectConfirmApprovalBases(
+        List<SearchConfirmApprovalBaseReq> dto
+    );
+
+    public PagingResult<SearchConfirmApprovalBaseRes> selectConfirmApprovalBases(
+        List<SearchConfirmApprovalBaseReq> dto, PageInfo pageInfo
+    );
+
+    public int selectCountConfirmApprovalBases(WctaCntrAprBaseBasDvo dvo);
+
+    public int deleteApprovalAskDivides(WctaCntrAprAkDvCdDvo dvo);
+
+    public int insertConfirmApprovalBases(WctaCntrAprBaseBasDvo dvo);
+
+    public int updateConfirmApprovalBases(WctaCntrAprBaseBasDvo dvo);
+
+    public int deleteConfirmApprovalBases(WctaCntrAprBaseBasDvo dvo);
+
 }
