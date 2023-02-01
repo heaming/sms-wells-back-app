c59ad6c266c8dcad36eba494a69550bd8b0cb192
Your Name <gs.piitxx@kyowon.com>
2023-02-01 09:32:42 +0900

    입출금 관리 모듈 공통 소스 추가
    Signed-off-by: Your Name <gs.piit160@kyowon.com>

---
+++ src/main/java/com/kyowon/sms/wells/web/withdrawal/zcommon/constants/WithdrawalConst.java
@@ -0,0 +1,8 @@
+package com.kyowon.sms.wells.web.withdrawal.zcommon.constants;
+
+import com.sds.sflex.system.config.constant.CommConst;
+
+public class WithdrawalConst {
+
+    public static final String REST_URL_V1 = CommConst.REST_URL_V1 + "/sms/wells/withdrawal";
+}
