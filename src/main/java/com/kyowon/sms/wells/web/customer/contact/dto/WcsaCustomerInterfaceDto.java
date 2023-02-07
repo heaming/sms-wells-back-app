b76af8760dc2dc9e408b779b8caec7952d404ce4
jeongeonkim <gs.piit142@kyowon.co.kr>
2023-02-07 17:53:39 +0900

    chore : 고객센터 wells 고객정보 I/F 리턴 형태 변경(camel->정의서 항목ID형태)

--- src/main/java/com/kyowon/sms/wells/web/customer/contact/dto/WcsaCustomerInterfaceDto.java
+++ src/main/java/com/kyowon/sms/wells/web/customer/contact/dto/WcsaCustomerInterfaceDto.java
@@ -19,84 +19,83 @@ public class WcsaCustomerInterfaceDto {
 
     /**
      * 고객번호 기준으로 고객정보를 조회 (IF정보 : EAI_WCUI1011_WELLS 고객정보 조회) Search Result Dto
-     * @param cstNo 고객번호
-     * @param itgCstNo 통합고객번호
-     * @param cikVal ci키값
-     * @param sfkVal 세이프키값
-     * @param copnDvCd 법인격구분코드
-     * @param cstInflwPhDvCd 고객유입경로코드
-     * @param cstKnm 고객한글명
-     * @param lnfDvCd 내외국인구분코드
-     * @param bryyMmdd 생년월일
-     * @param sexDvCd 성별구분코드
-     * @param emadr 이메일주소
-     * @param bzrno 사업자등록번호
-     * @param crpSpmtDrmNm 법인점포명
-     * @param locaraTno 지역전화번호
-     * @param exno 전화국번호
-     * @param idvTno 개별전화번호
-     * @param cralLocaraTno 휴대지역전화번호
-     * @param mexno 휴대전화국번호
-     * @param cralIdvTno 휴대개별전화번호
-     * @param adrId 주소ID
-     * @param zip 우편번호
-     * @param basAdr 기본주소
-     * @param dtlAdr 상세주소
-     * @param prtnrNo 파트너번호
-     * @param sapEmpno sap사원번호
-     * @param fmlDscOjYn 가족할인대상여부
-     * @param hsCtfYn 본인인증여부
-     * @param empYn 직원여부
-     * @param cstGdCd 고객등급코드
-     * @param nusdRsonCd 미사용사유코드
-     * @param dtaDlYn 데이터삭제여부
-     * @param fstRgstDtm 최초등록일시
-     * @param fnlMdfcDtm 최종수정일시
-     * @param rsCd 결과코드
-     * @param rsMsg 결과메시지
+     * @param CST_NO 고객번호
+     * @param ITG_CST_NO 통합고객번호
+     * @param CIK_VAL ci키값
+     * @param SFK_VAL 세이프키값
+     * @param COPN_DV_CD 법인격구분코드
+     * @param CST_INFLW_PH_DV_CD 고객유입경로코드
+     * @param CST_KNM 고객한글명
+     * @param LNF_DV_CD 내외국인구분코드
+     * @param BRYY_MMDD 생년월일
+     * @param SEX_DV_CD 성별구분코드
+     * @param EMADR 이메일주소
+     * @param BZRNO 사업자등록번호
+     * @param CRP_SPMT_DRM_NM 법인점포명
+     * @param LOCARA_TNO 지역전화번호
+     * @param EXNO 전화국번호
+     * @param IDV_TNO 개별전화번호
+     * @param CRAL_LOCARA_TNO 휴대지역전화번호
+     * @param MEXNO 휴대전화국번호
+     * @param CRAL_IDV_TNO 휴대개별전화번호
+     * @param ADR_ID 주소ID
+     * @param ZIP 우편번호
+     * @param BAS_ADR 기본주소
+     * @param DTL_ADR 상세주소
+     * @param PRTNR_NO 파트너번호
+     * @param SAP_EMPNO sap사원번호
+     * @param FML_DSC_OJ_YN 가족할인대상여부
+     * @param HS_CTF_YN 본인인증여부
+     * @param EMP_YN 직원여부
+     * @param CST_GD_CD 고객등급코드
+     * @param NUSD_RSON_CD 미사용사유코드
+     * @param DTA_DL_YN 데이터삭제여부
+     * @param FST_RGST_DTM 최초등록일시
+     * @param FNL_MDFC_DTM 최종수정일시
+     * @param RS_CD 결과코드
+     * @param RS_MSG 결과메시지
      */
     @ApiModel(value = "WcsaCustomerInterfaceDto-SearchCustomerRes")
     public record SearchCustomerRes(
-        String cstNo, /* 고객번호*/
-        String itgCstNo, /* 통합고객번호 */
-        String cikVal, /* ci키값 */
-        String sfkVal, /* 세이프키값 */
-        String copnDvCd, /* 법인격구분코드 */
-        String cstInflwPhDvCd, /* 고객유입경로코드 */
-        String cstKnm, /* 고객한글명 */
-        String lnfDvCd, /* 내외국인구분코드 */
-        String bryyMmdd, /* 생년월일 */
-        String sexDvCd, /* 성별구분코드 */
-        String emadr, /*이메일주소*/
-        String bzrno, /*사업자등록번호*/
-        String crpSpmtDrmNm, /* 법인점포명 */
-        String locaraTno, /* 지역전화번호 */
-        String exno, /*전화국번호*/
-        String idvTno, /* 개별전화번호 */
-        String cralLocaraTno, /* 휴대지역전화번호 */
-        String mexno, /*휴대전화국번호*/
-        String cralIdvTno, /* 휴대개별전화번호 */
-        String adrId, /* 주소ID */
-        String zip, /*우편번호*/
-        String basAdr, /* 기본주소 */
-        String dtlAdr, /* 상세주소 */
-        String prtnrNo, /* 파트너번호 */
-        String sapEmpno, /* sap사원번호 */
-        String fmlDscOjYn, /* 가족할인대상여부 */
-        String hsCtfYn, /* 본인인증여부 */
-        String empYn, /* 직원여부 */
-        String cstGdCd, /* 고객등급코드 */
-        String nusdRsonCd, /* 미사용사유코드 */
-        String dtaDlYn, /* 데이터삭제여부 */
-        String fstRgstDtm, /* 최초등록일시 */
-        String fnlMdfcDtm, /* 최종수정일시 */
-        String rsCd, /* 결과코드 */
-        String rsMsg /* 결과메시지 */
-
+        String CST_NO,
+        String ITG_CST_NO,
+        String CIK_VAL,
+        String SFK_VAL,
+        String COPN_DV_CD,
+        String CST_INFLW_PH_DV_CD,
+        String CST_KNM,
+        String LNF_DV_CD,
+        String BRYY_MMDD,
+        String SEX_DV_CD,
+        String EMADR,
+        String BZRNO,
+        String CRP_SPMT_DRM_NM,
+        String LOCARA_TNO,
+        String EXNO,
+        String IDV_TNO,
+        String CRAL_LOCARA_TNO,
+        String MEXNO,
+        String CRAL_IDV_TNO,
+        String ADR_ID,
+        String ZIP,
+        String BAS_ADR,
+        String DTL_ADR,
+        String PRTNR_NO,
+        String SAP_EMPNO,
+        String FML_DSC_OJ_YN,
+        String HS_CTF_YN,
+        String EMP_YN,
+        String CST_GD_CD,
+        String NUSD_RSON_CD,
+        String DTA_DL_YN,
+        String FST_RGST_DTM,
+        String FNL_MDFC_DTM,
+        String RS_CD,
+        String RS_MSG
     ) {
         public SearchCustomerRes {
-            exno = StringUtils.isNotEmpty(exno) ? DbEncUtil.dec(exno) : exno;
-            mexno = StringUtils.isNotEmpty(mexno) ? DbEncUtil.dec(mexno) : mexno;
+            EXNO = StringUtils.isNotEmpty(EXNO) ? DbEncUtil.dec(EXNO) : EXNO;
+            MEXNO = StringUtils.isNotEmpty(MEXNO) ? DbEncUtil.dec(MEXNO) : MEXNO;
         }
     }
 
