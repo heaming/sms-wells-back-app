b955d2af77c56a9a936860dd8ee62b7f66732616
BaeJunHo <gs.piit145@kyowon.com>
2023-02-08 17:22:57 +0900

    feat: 교재자재 특화로직 COMMON에서 EDU&WELLS로 분기

---
+++ src/main/java/com/kyowon/sms/wells/web/product/manage/mapper/WpdcMaterialMngtMapper.java
@@ -0,0 +1,36 @@
+package com.kyowon.sms.wells.web.product.manage.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.kyowon.sms.common.web.product.manage.dto.ZpdcMaterialMgtDto;
+import com.kyowon.sms.common.web.product.manage.dto.ZpdcMaterialMgtDto.SearchSapReq;
+import com.kyowon.sms.common.web.product.manage.dto.ZpdcMaterialMgtDto.SearchSapRes;
+import com.kyowon.sms.common.web.product.manage.dvo.ZpdcEachTbPdbsPdRelDvo;
+import com.sds.sflex.system.config.datasource.PageInfo;
+import com.sds.sflex.system.config.datasource.PagingResult;
+
+/* wpdc-product-material-mgt.xml */
+@Mapper
+public interface WpdcMaterialMngtMapper {
+
+    PagingResult<SearchSapRes> selectMaterialSapPages(@Param("dto")
+    SearchSapReq dto, PageInfo pageInfo);
+
+    int deleteTbPdbsPdRel(@Param("pdCd")
+    String pdCd,
+        @Param("info")
+        List<ZpdcMaterialMgtDto.TbPdbsPdRel> tbPdbsPdRels,
+        @Param("delMode")
+        String delMode
+    );
+
+    // 연결상품
+    int mergeEachTbPdbsPdRel(@Param("info")
+    ZpdcEachTbPdbsPdRelDvo info);
+
+    int deleteEachPdbsPdRels(String pdCd);
+
+}
---
+++ src/main/java/com/kyowon/sms/wells/web/product/zcommon/constants/PdProductConst.java
@@ -0,0 +1,10 @@
+package com.kyowon.sms.wells.web.product.zcommon.constants;
+
+import com.sds.sflex.system.config.constant.CommConst;
+
+public class PdProductConst {
+
+    // 상품 기본 URL
+    public static final String REST_URL_V1 = CommConst.REST_URL_V1 + "/sms/wells/product";
+
+}
