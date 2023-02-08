62aa39372189e08345ce7cf9030026db77b74848
gs.piit159 <gs.piit159@kyowon.com>
2023-02-08 17:09:27 +0900

    fix: 렌탈 약정만료 및 멤버십 현황 조회(W-SS-U-0015M01) 수정

--- src/main/java/com/kyowon/sms/wells/web/contract/orderstatus/service/WctdExpiredRetentionCntrService.java
+++ src/main/java/com/kyowon/sms/wells/web/contract/orderstatus/service/WctdExpiredRetentionCntrService.java
@@ -1,17 +1,13 @@
 package com.kyowon.sms.wells.web.contract.orderstatus.service;
 
-import static com.kyowon.sms.wells.web.contract.orderstatus.dto.WctdExpiredRetentionCntrDto.*;
+import static com.kyowon.sms.wells.web.contract.orderstatus.dto.WctdExpiredRetentionCntrDto.SearchReq;
+import static com.kyowon.sms.wells.web.contract.orderstatus.dto.WctdExpiredRetentionCntrDto.SearchRes;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import com.google.api.client.util.Lists;
-import com.kyowon.sms.wells.web.contract.orderstatus.converter.WctdExpiredRetentionCntrConverter;
 import com.kyowon.sms.wells.web.contract.orderstatus.mapper.WctdExpiredRetentionCntrMapper;
-import com.kyowon.sms.wells.web.contract.zcommon.constants.CtContractConst;
 import com.sds.sflex.system.config.datasource.PageInfo;
 import com.sds.sflex.system.config.datasource.PagingResult;
 
@@ -21,29 +17,12 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class WctdExpiredRetentionCntrService {
     private final WctdExpiredRetentionCntrMapper mapper;
-    private final WctdExpiredRetentionCntrConverter converter;
 
-    @Transactional
-    public List<SearchRes> setMshCntrInfo(List<SearchCntrRes> cntrs) {
-        List<SearchRes> results = Lists.newArrayList();
-        for (SearchCntrRes cntr : cntrs) {
-            FindMshCntrRes mshCntrRes = mapper.getMembershipCntrInfo(
-                cntr.cntrNo(), cntr.cntrSn(), Arrays.asList(CtContractConst.CntrStatCd.CANCELLATION.getDtlCds())
-            );
-            results.add(converter.mapCntrResAndMshCntrResToSearchRes(cntr, mshCntrRes));
-        }
-        return results;
-    }
-
-    @Transactional
     public PagingResult<SearchRes> getExpiredRetentionCntrPages(SearchReq dto, PageInfo pageInfo) {
-        PagingResult<SearchCntrRes> result = mapper.selectExpiredRetentionCntrPages(dto, pageInfo);
-        return new PagingResult(setMshCntrInfo(result.getList()), result.getPageInfo());
+        return mapper.selectExpiredRetentionCntrPages(dto, pageInfo);
     }
 
-    @Transactional
     public List<SearchRes> getExpiredRetentionCntrsForExcelDownload(SearchReq dto) {
-        List<SearchCntrRes> result = mapper.selectExpiredRetentionCntrPages(dto);
-        return setMshCntrInfo(result);
+        return mapper.selectExpiredRetentionCntrPages(dto);
     }
 }
