f04da6c8bd809de699ca38233bd02a53196c7c24
BaeJunHo <gs.piit145@kyowon.com>
2023-02-10 13:16:51 +0900

    feat: 수정메소드 rest api 변경 및 불필요기능 제거, 누락메소드 추가.

--- src/main/java/com/kyowon/sms/wells/web/product/manage/service/WpdcMaterialMgtService.java
+++ src/main/java/com/kyowon/sms/wells/web/product/manage/service/WpdcMaterialMgtService.java
@@ -7,6 +7,7 @@ import org.apache.commons.collections.CollectionUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.kyowon.sms.common.web.product.category.service.ZpdaClassificationMgtService;
 import com.kyowon.sms.common.web.product.manage.converter.ZpdcMaterialConverter;
 import com.kyowon.sms.common.web.product.manage.converter.ZpdcProductConverter;
 import com.kyowon.sms.common.web.product.manage.dto.ZpdcMaterialMgtDto;
@@ -20,9 +21,7 @@ import com.kyowon.sms.common.web.product.manage.service.ZpdcHistoryMgtService;
 import com.kyowon.sms.common.web.product.manage.service.ZpdcProductService;
 import com.kyowon.sms.common.web.product.zcommon.constants.PdProductConst;
 import com.kyowon.sms.wells.web.product.manage.mapper.WpdcMaterialMgtMapper;
-import com.sds.sflex.common.common.dvo.UserSessionDvo;
 import com.sds.sflex.common.utils.DateUtil;
-import com.sds.sflex.system.config.context.SFLEXContextHolder;
 import com.sds.sflex.system.config.datasource.PageInfo;
 import com.sds.sflex.system.config.datasource.PagingResult;
 import com.sds.sflex.system.config.validation.BizAssert;
@@ -55,29 +54,27 @@ public class WpdcMaterialMgtService {
     public ZpdcProductDto.TbPdbsPdBas saveMaterial(ZpdcMaterialMgtDto.SaveReq dto)
         throws Exception {
 
+        int processCount = 0;
         ZpdcProductDvo dvo = productConverter.mapPdBasToProductDvo(dto.tbPdbsPdBas());
 
-        int processCount = 0;
-        UserSessionDvo session = SFLEXContextHolder.getContext().getUserSession();
-        if (session.getTenantCd().equals("W")) {
-            // 교재/자재 : PD_TP_DTL_M(제품) , AS부품: PD_TP_DTL_A (As)부품
-            dvo.setPdTpDtlCd(PdProductConst.PD_TP_DTL_CD_M);
-        }
+        // #1. Wells 특화
+        // 교재/자재 : PD_TP_DTL_M(제품) , AS부품: PD_TP_DTL_A (As)부품
+        dvo.setPdTpDtlCd(PdProductConst.PD_TP_DTL_CD_M);
 
-        // #1. 분류체계 분류 계층값 FILL-IN
+        // #2. 분류체계 분류 계층값 FILL-IN
         dvo = clsfService.getClassifcationHierarchy(dvo);
 
-        // #2. 상품 마스터 INSERT
+        // #3. 상품 마스터 INSERT
         processCount = productMapper.insertProduct(dvo);
 
-        // #3. 각사 속성 INSERT
+        // #4. 각사 속성 INSERT
         BizAssert.isTrue(processCount == 1, "MSG_ALT_SVE_ERR");
         productService.saveEachCompanyPropDtl(dvo.getPdCd(), dto.tbPdbsPdEcomPrpDtl());
 
-        // #4. 연결상품 INSERT
+        // #5. 연결상품 INSERT
         this.editEachTbPdbsPdRel(dvo.getPdCd(), dto.tbPdbsPdRel());
 
-        // #5. 이력 INSERT
+        // #6. 이력 INSERT
         String startDtm = DateUtil.getDate(new Date());
         hisService.createProductHistory(dvo.getPdCd(), startDtm);
 
