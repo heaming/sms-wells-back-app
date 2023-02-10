bb4a3c23583a926acc4b07aa24274c480a1cea5e
JSY <gs.piit164@kyowon.co.kr>
2023-02-10 11:08:16 +0900

    fix : 비정도영업 조치사항 조회(W-SS-U-0144M01) 조회 조건 변경에 따른 쿼리문 변경

--- src/main/java/com/kyowon/sms/wells/web/contract/risk/rest/WctcRiskAuditController.java
+++ src/main/java/com/kyowon/sms/wells/web/contract/risk/rest/WctcRiskAuditController.java
@@ -6,11 +6,7 @@ import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.kyowon.sms.wells.web.contract.risk.dto.WctcRiskAuditDto.SearchReq;
 import com.kyowon.sms.wells.web.contract.risk.dto.WctcRiskAuditDto.SearchRes;
@@ -34,15 +30,16 @@ public class WctcRiskAuditController {
 
     @ApiOperation(value = "비정도영업 조치사항 조회", notes = "조회조건에 따른 비정도영업 조치사항 목록을 조회한다.")
     @ApiImplicitParams(value = {
-        @ApiImplicitParam(name = "searchGubun", value = "조회구분", paramType = "query"),
-        @ApiImplicitParam(name = "startDate", value = "조회시작날짜", paramType = "query"),
-        @ApiImplicitParam(name = "endDate", value = "조회마지막날짜", paramType = "query"),
+        @ApiImplicitParam(name = "searchGubun", value = "조회구분", paramType = "query", required = true, example = "1"),
+        @ApiImplicitParam(name = "startDate", value = "조회시작날짜", paramType = "query", example = "20220101"),
+        @ApiImplicitParam(name = "endDate", value = "조회마지막날짜", paramType = "query", example = "20221231"),
         @ApiImplicitParam(name = "startMonth", value = "조회시작월", paramType = "query"),
         @ApiImplicitParam(name = "endMonth", value = "조회마지막월", paramType = "query"),
         @ApiImplicitParam(name = "generalDivision", value = "총괄단", paramType = "query"),
         @ApiImplicitParam(name = "regionalGroup", value = "지역단", paramType = "query"),
         @ApiImplicitParam(name = "branchOffice", value = "지점", paramType = "query"),
         @ApiImplicitParam(name = "employeeNo", value = "사번", paramType = "query"),
+        @ApiImplicitParam(name = "positionDivide", value = "직위구분", paramType = "query"),
     })
     @GetMapping("/irregular-sales-actions")
     public List<SearchRes> getIrregularBznsInqr(
