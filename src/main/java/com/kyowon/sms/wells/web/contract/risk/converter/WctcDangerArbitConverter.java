720f4a8b89d3fd11d00a2221d0240241a3ec8c93
JSY <gs.piit164@kyowon.co.kr>
2023-02-13 13:44:27 +0900

    fix : 비정도영업 조치사항 관리(W-SS-U-0143M01) 쿼리문 수정

--- src/main/java/com/kyowon/sms/wells/web/contract/risk/converter/WctcDangerArbitConverter.java
+++ src/main/java/com/kyowon/sms/wells/web/contract/risk/converter/WctcDangerArbitConverter.java
@@ -8,7 +8,10 @@ import com.kyowon.sms.wells.web.contract.risk.dvo.WctcDangerArbitDvo;
 
 @Mapper(componentModel = "spring")
 public interface WctcDangerArbitConverter {
-    @Mapping(source = "dangArbitCdNm", target = "dangArbitCd")
-    @Mapping(source = "dangArbitOgNm", target = "dangArbitOgId")
+    @Mapping(source = "dangOjPrtnrOgNm", target = "dangMngtOgTpCd")
+    @Mapping(source = "dangOjPrtnrNo", target = "dangMngtPrtnrNo")
+    @Mapping(source = "dangOcStrtmm", target = "dangOcStrtdt")
+    @Mapping(source = "dangOjPrtnrPstnDvNm", target = "dangOjPstnDvCd")
+    @Mapping(source = "dangChkNm", target = "dangUncvrCn")
     WctcDangerArbitDvo mapSaveReqWctcDangerArbitDvo(WctcDangerArbitDto.SaveReq dto);
 }
