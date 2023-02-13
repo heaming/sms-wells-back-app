19b1f860cf65033408661f59b7b4cc64943b0e96
MyoungBin Joo <gs.piit165@kyowon.co.kr>
2023-02-13 14:21:10 +0900

    chore : wells 확정 승인 기준 관리 - 조회 시 누락된 파라미터 수정

--- src/main/java/com/kyowon/sms/wells/web/contract/ordermgmt/dto/WctaContractDto.java
+++ src/main/java/com/kyowon/sms/wells/web/contract/ordermgmt/dto/WctaContractDto.java
@@ -130,6 +130,8 @@ public class WctaContractDto {
     public record SearchConfirmApprovalBaseRes(
 
         String cntrAprBaseId,
+
+        String cntrAprSellDvCd,
         String cntrAprAkDvCd,
         String cntrAprChnlDvVal,
         String cntrAprIchrDvCd,
