4fb62cc6eace19d4f6ececcc11352bdc1ef871f9
JSY <gs.piit164@kyowon.co.kr>
2023-02-15 18:47:46 +0900

    review : 리뷰 수정사항 반영

--- src/main/java/com/kyowon/sms/wells/web/contract/ordermgmt/service/WctaContractService.java
+++ src/main/java/com/kyowon/sms/wells/web/contract/ordermgmt/service/WctaContractService.java
@@ -105,16 +105,4 @@ public class WctaContractService {
         }
         return processCount;
     }
-
-    public List<SearchBpdRentalAccMgtPssRes> getBpdRentalAccMgtPss(
-        SearchBpdRentalAccMgtPssReq dto
-    ) {
-        return mapper.selectBpdRentalAccMgtPss(dto);
-    }
-
-    public List<SearchByoRentalAccMgtInqrsRes> getByoRentalAccMgtInqrs(
-        SearchByoRentalAccMgtInqrsReq dto
-    ) {
-        return mapper.selectByoRentalAccMgtInqrs(dto);
-    }
 }
