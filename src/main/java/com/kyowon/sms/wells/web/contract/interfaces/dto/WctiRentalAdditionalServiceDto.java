0bd58c82e2b148916d96decdd3121a4b66001a31
younuk.choi <gs.piit186@kyowon.co.kr>
2023-02-17 17:25:06 +0900

    feat:(고객센터 I/F) 렌탈 부가서비스 이력 조회 신규 개발

---
+++ src/main/java/com/kyowon/sms/wells/web/contract/interfaces/dto/WctiRentalAdditionalServiceDto.java
@@ -0,0 +1,34 @@
+package com.kyowon.sms.wells.web.contract.interfaces.dto;
+
+import javax.validation.constraints.NotBlank;
+
+import io.swagger.annotations.ApiModel;
+
+public class WctiRentalAdditionalServiceDto {
+    // *********************************************************
+    // Request Dto
+    // *********************************************************
+    // 렌탈 부가서비스 Search Request Dto
+    @ApiModel("WctiRentalAdditionalServiceDto-SearchReq")
+    public record SearchReq(
+            @NotBlank
+            String CNTR_NO, //계약번호(필수)
+            @NotBlank
+            String CNTR_SN //계약일련번호(필수)
+    ) {}
+
+    // *********************************************************
+    // Result Dto
+    // *********************************************************
+    // 렌탈 부가서비스 Search Result Dto
+    @ApiModel("WctiRentalAdditionalServiceDto-SearchRes")
+    public record SearchRes(
+            String IST_DT,
+            String REQD_DT,
+            String ADN_SV_CS_AMT,
+            String RENTAL_AMT,
+            String TOT_AMT
+    ) {}
+}
+
+
