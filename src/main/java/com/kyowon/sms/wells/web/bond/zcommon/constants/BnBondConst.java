185fdcd6b380ef61a0af710e197c813b666e21eb
gugyeongu <gs.piit163@kyowon.co.kr>
2023-02-20 13:06:46 +0900

    feat: 채권 잘못 선언한 상수 수정

--- src/main/java/com/kyowon/sms/wells/web/bond/zcommon/constants/BnBondConst.java
+++ src/main/java/com/kyowon/sms/wells/web/bond/zcommon/constants/BnBondConst.java
@@ -3,5 +3,5 @@ package com.kyowon.sms.wells.web.bond.zcommon.constants;
 import com.sds.sflex.system.config.constant.CommConst;
 
 public class BnBondConst {
-    public static final String REST_URL_V1 = CommConst.REST_URL_V1 + "/sms/edu/bond";
+    public static final String REST_URL_V1 = CommConst.REST_URL_V1 + "/sms/wells/bond";
 }
