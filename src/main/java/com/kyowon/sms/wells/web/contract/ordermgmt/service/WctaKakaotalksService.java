ff68a5a77b246c43b9641130558ec3eeb36f08ba
jihoon.kim <gs.piit193@kyowon.co.kr>
2023-02-20 17:40:45 +0900

    review : 카카오톡 발송 내역 조회 W-SS-U-0096P02 tempCodes 수정

--- src/main/java/com/kyowon/sms/wells/web/contract/ordermgmt/service/WctaKakaotalksService.java
+++ src/main/java/com/kyowon/sms/wells/web/contract/ordermgmt/service/WctaKakaotalksService.java
@@ -20,28 +20,28 @@ public class WctaKakaotalksService {
     private final WctaKakaotalksMapper mapper;
 
     public List<SearchRes> getKakaotalkFwIzs(SearchReq dto) {
-        List<String> tempCodeLists = new ArrayList<String>();
+        List<String> tempCodes = new ArrayList<String>();
         if (dto.concDiv().equals("R")) {
-            tempCodeLists.add("wells51865"); // 구 템플릿
-            tempCodeLists.add("wells17945"); // 신 템플릿
-            tempCodeLists.add("Wells18182"); // 템플릿 테스트 데이터
+            tempCodes.add("wells51865"); // 구 템플릿
+            tempCodes.add("wells17945"); // 신 템플릿
+            tempCodes.add("Wells18182"); // 템플릿 테스트 데이터
         } else {//if(StringUtil.isEquals("N", concDiv) || StringUtil.isEquals("U", concDiv) || StringUtil.isEquals("A", concDiv)){
-            tempCodeLists.add("Wells5ECC2"); // 구 계약 템플릿
-            tempCodeLists.add("wells17904"); // 구 템플릿(계약)
-            tempCodeLists.add("wells17947"); // 신 템플릿(계약)
-            tempCodeLists.add("Wells18104"); // 신 템플릿(계약) - 2021.06.08 (공유렌탈 소개 페이지 URL 링크 추가)
-            tempCodeLists.add("Wells18157"); // 신 템플릿(계약) - 2021.10.20 (IoT 안내 URL 링크 추가)
-            tempCodeLists.add("wells51866"); // 구 템플릿(모종일시불)
-            tempCodeLists.add("wells17944"); // 신 템플릿(모종일시불)
-            tempCodeLists.add("wells17905"); // 구 템플릿(설치자용)
-            tempCodeLists.add("wells17948"); // 신 템플릿(설치자용)
-            tempCodeLists.add("Wells18183"); // 건강케어 서비스 알림톡
-            tempCodeLists.add("Wells18188"); // 건강케어 서비스 알림톡(수정)
-            tempCodeLists.add("Wells18206"); // 피버 바이크 플러스 온라인 강의 무료 구독 신청
-            tempCodeLists.add("Wells18202"); // 장기 할부 안내 문구 추가 템플릿(iot)
-            tempCodeLists.add("Wells18203"); // 장기 할부 안내 문구 추가 템플릿(일반)
+            tempCodes.add("Wells5ECC2"); // 구 계약 템플릿
+            tempCodes.add("wells17904"); // 구 템플릿(계약)
+            tempCodes.add("wells17947"); // 신 템플릿(계약)
+            tempCodes.add("Wells18104"); // 신 템플릿(계약) - 2021.06.08 (공유렌탈 소개 페이지 URL 링크 추가)
+            tempCodes.add("Wells18157"); // 신 템플릿(계약) - 2021.10.20 (IoT 안내 URL 링크 추가)
+            tempCodes.add("wells51866"); // 구 템플릿(모종일시불)
+            tempCodes.add("wells17944"); // 신 템플릿(모종일시불)
+            tempCodes.add("wells17905"); // 구 템플릿(설치자용)
+            tempCodes.add("wells17948"); // 신 템플릿(설치자용)
+            tempCodes.add("Wells18183"); // 건강케어 서비스 알림톡
+            tempCodes.add("Wells18188"); // 건강케어 서비스 알림톡(수정)
+            tempCodes.add("Wells18206"); // 피버 바이크 플러스 온라인 강의 무료 구독 신청
+            tempCodes.add("Wells18202"); // 장기 할부 안내 문구 추가 템플릿(iot)
+            tempCodes.add("Wells18203"); // 장기 할부 안내 문구 추가 템플릿(일반)
         }
 
-        return mapper.selectKakaotalkFwIzs(dto.mtPr(), tempCodeLists);
+        return mapper.selectKakaotalkFwIzs(dto.mtPr(), tempCodes);
     }
 }
