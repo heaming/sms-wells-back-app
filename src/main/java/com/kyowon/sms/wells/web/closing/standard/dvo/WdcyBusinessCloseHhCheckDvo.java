e038ad3fcb3296fcfa7943a2aa47eb74386d439a
우승민 <gs.piit183@kyowon.co.kr>
2023-02-27 18:56:46 +0900

    feat: 일마감통제확인 서비스 수정

--- src/main/java/com/kyowon/sms/wells/web/closing/standard/dvo/WdcyBusinessCloseHhCheckDvo.java
+++ src/main/java/com/kyowon/sms/wells/web/closing/standard/dvo/WdcyBusinessCloseHhCheckDvo.java
@@ -18,9 +18,9 @@ import lombok.ToString;
 public class WdcyBusinessCloseHhCheckDvo {
 
     //테이블 정의가 안된 상태로 작성된 DVO 추후 테이블 구조가 완료 되는 경우 맞게 수정
-    private String stDt;
+    private String strtdt;
     private String strtHh;
-    private String edDt;
+    private String enddt;
     private String endHh;
     private String perfDt; /* 실적 일자 */
     private String procsPsbYn; /* 처리가능 여부 */
