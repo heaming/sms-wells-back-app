ffc6e2bc7a264434e7f166658bb03af43365df7c
김승수 <gs.piit184@kyowon.com>
2023-03-03 09:00:37 +0900

    chore : 어노테이션 제거

--- src/main/java/com/kyowon/sms/wells/web/service/orgcode/dvo/WsndBusinessVehiclesMgtDvo.java
+++ src/main/java/com/kyowon/sms/wells/web/service/orgcode/dvo/WsndBusinessVehiclesMgtDvo.java
@@ -1,8 +1,5 @@
 package com.kyowon.sms.wells.web.service.orgcode.dvo;
 
-import com.sds.sflex.system.config.annotation.DBDecField;
-import com.sds.sflex.system.config.annotation.DBEncField;
-
 import lombok.Getter;
 import lombok.Setter;
 
@@ -18,11 +15,7 @@ public class WsndBusinessVehiclesMgtDvo {
     String dsbEnddt;
     String vhcMngtTpCd;
     String insrAgeCd;
-    @DBEncField
-    @DBDecField
     String rflngCdnoEncr;
-    @DBEncField
-    @DBDecField
     String hipsCdnoEncr;
     String vhcDsbRmkCn;
     String orglFnlMdfcDtm;
