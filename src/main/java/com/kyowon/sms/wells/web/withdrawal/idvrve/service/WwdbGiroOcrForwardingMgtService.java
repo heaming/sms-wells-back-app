a17ec523822784b1a27a62ec12c0d7b522eb5702
heungjun.lee <gs.piit168@kyowon.com>
2023-03-03 15:32:49 +0900

    수납 - 개별수납 - 수정
    Signed-off-by: heungjun.lee <gs.piit168@kyowon.com>

--- src/main/java/com/kyowon/sms/wells/web/withdrawal/idvrve/service/WwdbGiroOcrForwardingMgtService.java
+++ src/main/java/com/kyowon/sms/wells/web/withdrawal/idvrve/service/WwdbGiroOcrForwardingMgtService.java
@@ -68,7 +68,9 @@ public class WwdbGiroOcrForwardingMgtService {
 
         for (SaveReq dto : dtos) {
             WwdbGiroOcrForwardingMgtDvo dvo = convert.mapSaveGiroOcrForwardingDvo(dto);
-            log.info(dto.rowState());
+            String cntr = dvo.getCntr();
+            dvo.setCntrNo(cntr.substring(0, 12));
+            dvo.setCntrSn(cntr.substring(12));
 
             switch (dto.rowState()) {
                 case CommConst.ROW_STATE_CREATED -> {
