2ec0a26330393ac0504c8612d086d9cb0cb10e27
younuk.choi <gs.piit186@kyowon.co.kr>
2023-03-06 09:34:14 +0900

    feat:  (고객센터I/F) 라이프 - wells 철거 정보 조회  - 신규개발

---
+++ src/main/java/com/kyowon/sms/wells/web/contract/interfaces/dto/WctiRequidationDateDto.java
@@ -0,0 +1,35 @@
+package com.kyowon.sms.wells.web.contract.interfaces.dto;
+
+import javax.validation.constraints.NotBlank;
+
+import io.swagger.annotations.ApiModel;
+
+public class WctiRequidationDateDto {
+    // *********************************************************
+    // Request Dto
+    // *********************************************************
+    // 계약건의 철거 정보 Find Request Dto
+    @ApiModel("WctiRequidationDateDto-FindReq")
+    public record FindReq(
+            @NotBlank
+            String CNTR_NO,
+            @NotBlank
+            String CNTR_SN
+    ) {
+    }
+
+    // *********************************************************
+    // Result Dto
+    // *********************************************************
+    // 계약건의 철거 정보 Find Result Dto
+    @ApiModel("WctiRequidationDateDto-FindRes")
+    public record FindRes(
+        String CNTR_NO,
+        String CNTR_SN,
+        String CNTR_DT,
+        String IST_DT,
+        String REQD_DT,
+        String REQD_IST_DC_GAP
+    ) {
+    }
+}
