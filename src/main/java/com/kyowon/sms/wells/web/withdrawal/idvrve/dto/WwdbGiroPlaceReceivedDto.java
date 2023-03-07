1272e30ad51616be177b226dd9062a353c3b6224
heungjun.lee <gs.piit168@kyowon.com>
2023-03-07 21:29:10 +0900

    단위테스트 수납 - 개별수납 - 수정
    Signed-off-by: heungjun.lee <gs.piit168@kyowon.com>

--- src/main/java/com/kyowon/sms/wells/web/withdrawal/idvrve/dto/WwdbGiroPlaceReceivedDto.java
+++ src/main/java/com/kyowon/sms/wells/web/withdrawal/idvrve/dto/WwdbGiroPlaceReceivedDto.java
@@ -21,13 +21,12 @@ public class WwdbGiroPlaceReceivedDto {
         String giroBizDvCd, //지로업무구분코드 
         String giroBizTpCd, //지로업무유형코드 판매유형
         String cstFnm, //고객명
-        String zip, //우편번호
-        String adrDvCd, //주소구분코드
+        String zip, //우편번호        
         String basAdr, //기본주소
-        String dtlAdr, //상세주소
-        String dtaDlYn, //삭제여부
+        String dtlAdr, //상세주소        
         String fstRgstUsrId, //업무담당
-        String fnlMdfcDtm //일시
+        String giroPlrcvRgstDt, //일시
+        String giroPlrcvAdrId //주소ID
 
     ) {}
 
@@ -40,11 +39,13 @@ public class WwdbGiroPlaceReceivedDto {
         String giroBizTpCd, //지로업무유형코드 판매유형
         @NotBlank
         String cstFnm, //고객명
-        String zip, //우편번호
-        String adrDvCd, //주소구분코드
-        String basAdr, //기본주소
-        String dtlAdr, //상세주소        
-        String fstRgstUsrId //업무담당
+        //        String zip, //우편번호
+        //        String adrDvCd, //주소구분코드
+        //        String basAdr, //기본주소
+        //        String dtlAdr, //상세주소        
+        String fstRgstUsrId, //업무담당
+        String giroPlrcvAdrId, //주소ID
+        String giroPlrcvRgstDt //등록일자
 
     ) {}
 }
