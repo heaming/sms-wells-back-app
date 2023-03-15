414f32cf0e83dfb66a9e764530a668a72e1da0f2
juno.cha <gs.piit134@kyowon.com>
2023-03-15 13:23:48 +0900

    fix : 기 개발된 화면 수정 - grid name property 추가 - Excel Download 로직 수정 - TB_GBCO_ZIP_BAS.ISLND_YN 컬럼명 수정

--- src/main/java/com/kyowon/sms/wells/web/service/allocate/mapper/WsncFixationVisitMapper.java
+++ src/main/java/com/kyowon/sms/wells/web/service/allocate/mapper/WsncFixationVisitMapper.java
@@ -1,9 +1,11 @@
 package com.kyowon.sms.wells.web.service.allocate.mapper;
 
-import com.kyowon.sms.wells.web.service.allocate.dto.WsncFixationVisitDto;
-import com.kyowon.sms.wells.web.service.allocate.dvo.WsncFixationVisitDvo;
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
+import com.kyowon.sms.wells.web.service.allocate.dto.WsncFixationVisitDto;
+import com.kyowon.sms.wells.web.service.allocate.dvo.WsncFixationVisitDvo;
 import com.sds.sflex.system.config.datasource.PageInfo;
 import com.sds.sflex.system.config.datasource.PagingResult;
 
@@ -11,6 +13,10 @@ import com.sds.sflex.system.config.datasource.PagingResult;
 public interface WsncFixationVisitMapper {
     PagingResult<WsncFixationVisitDto.SearchRes> selectFixationVisits(
         WsncFixationVisitDto.SearchReq dto, PageInfo pageInfo
+    );
+
+    List<WsncFixationVisitDto.SearchRes> selectFixationVisits(
+        WsncFixationVisitDto.SearchReq dto
     );
 
     WsncFixationVisitDto.SearchRegRes selectFixationVisit(
