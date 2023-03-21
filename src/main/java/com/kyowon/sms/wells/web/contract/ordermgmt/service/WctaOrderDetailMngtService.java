197c5f8e0fcef9d71d291bc0b712ebb149620a67
jihoon.kim <gs.piit193@kyowon.co.kr>
2023-03-21 15:01:59 +0900

    feat: 주문상세조회/관리 및 wells 렌탈 접수자료 개발

---
+++ src/main/java/com/kyowon/sms/wells/web/contract/ordermgmt/service/WctaOrderDetailMngtService.java
@@ -0,0 +1,30 @@
+package com.kyowon.sms.wells.web.contract.ordermgmt.service;
+
+import static com.kyowon.sms.wells.web.contract.ordermgmt.dto.WctaOrderDetailMngtDto.SearchReq;
+import static com.kyowon.sms.wells.web.contract.ordermgmt.dto.WctaOrderDetailMngtDto.SearchRes;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.kyowon.sms.wells.web.contract.ordermgmt.mapper.WctaOrderDetailMngtMapper;
+import com.sds.sflex.system.config.datasource.PageInfo;
+import com.sds.sflex.system.config.datasource.PagingResult;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class WctaOrderDetailMngtService {
+    private final WctaOrderDetailMngtMapper mapper;
+
+    public PagingResult<SearchRes> getOrderDetailRentalPages(SearchReq dto, PageInfo pageInfo) {
+        return mapper.selectOrderDetailRentalPages(dto, pageInfo);
+    }
+
+    public List<SearchRes> getOrderDtlRentalExcels(SearchReq dto) {
+        return mapper.selectOrderDetailRentalPages(dto);
+    }
+}
