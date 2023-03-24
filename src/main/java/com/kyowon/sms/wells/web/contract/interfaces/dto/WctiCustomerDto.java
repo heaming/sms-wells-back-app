0c375be2567f56a74caed077138089a4fec423d2
younuk.choi <gs.piit186@kyowon.co.kr>
2023-03-24 10:56:49 +0900

    fix:전화번호 검색 시 암/복호화 추가(dvo 사용) / 고객센터I/F 시, request/response 파라미터  선언 방식 변경

--- src/main/java/com/kyowon/sms/wells/web/contract/interfaces/dto/WctiCustomerDto.java
+++ src/main/java/com/kyowon/sms/wells/web/contract/interfaces/dto/WctiCustomerDto.java
@@ -1,5 +1,7 @@
 package com.kyowon.sms.wells.web.contract.interfaces.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import io.swagger.annotations.ApiModel;
 
 public class WctiCustomerDto {
@@ -9,14 +11,22 @@ public class WctiCustomerDto {
     // 고객 통합 리스트 Search Request Dto
     @ApiModel("WctiCustomerDto-SearchReq")
     public record SearchReq(
-        String CST_NM,
-        String CRAL_LOCARA_TNO,
-        String MEXNO,
-        String CRAL_IDV_TNO,
-        String LOCARA_TNO,
-        String EXNO,
-        String IDV_TNO,
-        String CST_NO
+        @JsonProperty("CST_NM")
+        String cstNm,
+        @JsonProperty("CRAL_LOCARA_TNO")
+        String cralLocaraTno,
+        @JsonProperty("MEXNO")
+        String mexno,
+        @JsonProperty("CRAL_IDV_TNO")
+        String cralIdvTno,
+        @JsonProperty("LOCARA_TNO")
+        String locaraTno,
+        @JsonProperty("EXNO")
+        String exno,
+        @JsonProperty("IDV_TNO")
+        String idvTno,
+        @JsonProperty("CST_NO")
+        String cstNo
     ) {}
 
     // *********************************************************
@@ -25,18 +35,31 @@ public class WctiCustomerDto {
     // 고객 통합 리스트 Search Result Dto
     @ApiModel("WctiCustomerDto-SearchRes")
     public record SearchRes(
-        String CST_DV,
-        String CST_NO,
-        String ITG_CST_NO,
-        String CST_KNM,
-        String CRAL_LOCARA_TNO,
-        String MEXNO,
-        String CRAL_IDV_TNO,
-        String LOCARA_TNO,
-        String EXNO,
-        String IDV_TNO,
-        String EMADR,
-        String BRYY_MMDD,
-        String SEX_DV_CD
+        @JsonProperty("CST_DV")
+        String cstDv,
+        @JsonProperty("CST_NO")
+        String cstNo,
+        @JsonProperty("ITG_CST_NO")
+        String itgCstNo,
+        @JsonProperty("CST_KNM")
+        String cstKnm,
+        @JsonProperty("CRAL_LOCARA_TNO")
+        String cralLocaraTno,
+        @JsonProperty("MEXNO")
+        String mexno,
+        @JsonProperty("CRAL_IDV_TNO")
+        String cralIdvTno,
+        @JsonProperty("LOCARA_TNO")
+        String locaraTno,
+        @JsonProperty("EXNO")
+        String exno,
+        @JsonProperty("IDV_TNO")
+        String idvTno,
+        @JsonProperty("EMADR")
+        String emadr,
+        @JsonProperty("BRYY_MMDD")
+        String bryyMmdd,
+        @JsonProperty("SEX_DV_CD")
+        String sexDvCd
     ) {}
 }
