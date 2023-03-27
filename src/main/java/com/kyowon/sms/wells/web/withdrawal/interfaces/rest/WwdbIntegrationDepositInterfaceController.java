abc81b92903a7a6a80732889bf717a6f63fe6c9e
선승진 <gs.piit138@kyowon.co.kr>
2023-03-27 11:07:45 +0900

    chore: JsonProperty 적용 및 컬럼명 변경 수정

--- src/main/java/com/kyowon/sms/wells/web/withdrawal/interfaces/rest/WwdbIntegrationDepositInterfaceController.java
+++ src/main/java/com/kyowon/sms/wells/web/withdrawal/interfaces/rest/WwdbIntegrationDepositInterfaceController.java
@@ -1,31 +1,20 @@
 package com.kyowon.sms.wells.web.withdrawal.interfaces.rest;
 
-import java.util.List;
-
-import javax.validation.Valid;
-
+import com.kyowon.sms.wells.web.withdrawal.interfaces.dto.WwdbIntegrationDepositInterfaceDto.*;
+import com.kyowon.sms.wells.web.withdrawal.interfaces.service.WwdbIntegrationDepositInterfaceService;
+import com.kyowon.sms.wells.web.withdrawal.zcommon.constants.WdWithdrawalConst;
+import com.sds.sflex.system.config.annotation.InterfaceController;
+import com.sds.sflex.system.config.webclient.ivo.EaiWrapper;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.kyowon.sms.wells.web.withdrawal.interfaces.dto.WwdbIntegrationDepositInterfaceDto;
-import com.kyowon.sms.wells.web.withdrawal.interfaces.dto.WwdbIntegrationDepositInterfaceDto.SearchCompanyCodeReq;
-import com.kyowon.sms.wells.web.withdrawal.interfaces.dto.WwdbIntegrationDepositInterfaceDto.SearchCompanyCodeRes;
-import com.kyowon.sms.wells.web.withdrawal.interfaces.dto.WwdbIntegrationDepositInterfaceDto.SearchDepoCodeReq;
-import com.kyowon.sms.wells.web.withdrawal.interfaces.dto.WwdbIntegrationDepositInterfaceDto.SearchDepoCodeRes;
-import com.kyowon.sms.wells.web.withdrawal.interfaces.dto.WwdbIntegrationDepositInterfaceDto.SearchDetailTradeReq;
-import com.kyowon.sms.wells.web.withdrawal.interfaces.dto.WwdbIntegrationDepositInterfaceDto.SearchDetailTradeRes;
-import com.kyowon.sms.wells.web.withdrawal.interfaces.dto.WwdbIntegrationDepositInterfaceDto.SearchItemizationReq;
-import com.kyowon.sms.wells.web.withdrawal.interfaces.dto.WwdbIntegrationDepositInterfaceDto.SearchItemizationRes;
-import com.kyowon.sms.wells.web.withdrawal.interfaces.service.WwdbIntegrationDepositInterfaceService;
-import com.kyowon.sms.wells.web.withdrawal.zcommon.constants.WdWithdrawalConst;
-import com.sds.sflex.system.config.annotation.InterfaceController;
-import com.sds.sflex.system.config.webclient.ivo.EaiWrapper;
-
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import lombok.RequiredArgsConstructor;
+import javax.validation.Valid;
+import java.util.List;
 
 @InterfaceController
 @Api(tags = "[WWDB] 공통 통합입금 내역 I/F API")
@@ -53,7 +42,7 @@ public class WwdbIntegrationDepositInterfaceController {
 
         return resWrapper;
     }
-    
+
     @ApiOperation(value = "[EAI_WDEI1012] 공통 통합입금 내역 입금유형 코드 조회")
     @PostMapping("/deposit-codes")
     public EaiWrapper getIntegrationDepositDepoCode(
@@ -71,7 +60,7 @@ public class WwdbIntegrationDepositInterfaceController {
 
         return resWrapper;
     }
-    
+
     @ApiOperation(value = "[EAI_WDEI1013] 공통 통합입금 내역 세부 거래내역 조회")
     @PostMapping("/detail-trades")
     public EaiWrapper getIntegrationDepositDetailTrades(
@@ -89,7 +78,7 @@ public class WwdbIntegrationDepositInterfaceController {
 
         return resWrapper;
     }
-    
+
     @ApiOperation(value = "[EAI_WDEI1014] 공통 통합입금 내역 조회")
     @PostMapping("/itemizations")
     public EaiWrapper getIntegrationDepositItemizations(
@@ -107,5 +96,5 @@ public class WwdbIntegrationDepositInterfaceController {
 
         return resWrapper;
     }
-    
+
 }
