8d8df48ec8f2b1deb1ffdee954e068ab17127905
김승수 <gs.piit184@kyowon.com>
2023-03-30 21:07:25 +0900

    feat: W-SV-U-0196M01(엔지니어 공구 이력관리) 개발

--- src/main/java/com/kyowon/sms/wells/web/service/stock/dto/WsnaEngineerToolDto.java
+++ src/main/java/com/kyowon/sms/wells/web/service/stock/dto/WsnaEngineerToolDto.java
@@ -3,32 +3,63 @@ package com.kyowon.sms.wells.web.service.stock.dto;
 import java.util.List;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 
 import io.swagger.annotations.ApiModel;
 
 public class WsnaEngineerToolDto {
-    @ApiModel(value = "WsnaEngineerToolDsbDto-SearchReq")
+    @ApiModel(value = "WsnaEngineerToolDto-SearchReq")
     public record SearchReq(
-
+        String pymdt,
+        String ogCd,
+        String egerPrtnrNo,
+        String prtnrKnm,
+        String toolPdCdStrt,
+        String toolPdCdEnd,
+        String sapMatCdStrt,
+        String sapMatCdEnd
     ) {}
 
-    @ApiModel(value = "WsnaEngineerToolDsbDto-SearchRes")
+    @ApiModel(value = "WsnaEngineerToolDto-SearchRes")
     public record SearchRes(
-
+        String ogTpCd,
+        String egerPrtnrNo,
+        String toolPdCd,
+        String dsbSn,
+        String toolQty,
+        String ogCd,
+        String ogNm,
+        String hgrOgNm,
+        String prtnrNo,
+        String prtnrKnm,
+        String svpdNmAbbr1,
+        String svpdSapCd,
+        String pymdts,
+        int maxToolQty
     ) {}
 
-    @ApiModel(value = "WsnaEngineerToolDsbDto-SearchPartsRes")
+    @ApiModel(value = "WsnaEngineerToolDto-RemoveReq")
+    public record RemoveReq(
+        @NotBlank
+        String ogTpCd,
+        @NotBlank
+        String egerPrtnrNo,
+        @NotBlank
+        String toolPdCd
+    ) {}
+
+    @ApiModel(value = "WsnaEngineerToolDto-SearchPartsRes")
     public record SearchPartsRes(
         String svpdSapCd,
         String svpdPdCd,
         String svpdNmAbbr1
     ) {}
 
-    @ApiModel(value = "WsnaEngineerToolDsbDto-CreateReq")
+    @ApiModel(value = "WsnaEngineerToolDto-CreateReq")
     public record CreateReq(
-        @NotBlank
+        @NotEmpty
         List<String> pdCds,
-        @NotBlank
+        @NotEmpty
         List<String> egerPrtnrNos
     ) {}
 }
--- src/main/java/com/kyowon/sms/wells/web/service/stock/service/WsnaEngineerToolService.java
+++ src/main/java/com/kyowon/sms/wells/web/service/stock/service/WsnaEngineerToolService.java
@@ -6,14 +6,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.kyowon.sms.wells.web.service.stock.converter.WsnaEngineerToolConverter;
-import com.kyowon.sms.wells.web.service.stock.dto.WsnaEngineerToolDto.CreateReq;
-import com.kyowon.sms.wells.web.service.stock.dto.WsnaEngineerToolDto.SearchPartsRes;
-import com.kyowon.sms.wells.web.service.stock.dto.WsnaEngineerToolDto.SearchReq;
-import com.kyowon.sms.wells.web.service.stock.dto.WsnaEngineerToolDto.SearchRes;
+import com.kyowon.sms.wells.web.service.stock.dto.WsnaEngineerToolDto.*;
 import com.kyowon.sms.wells.web.service.stock.dvo.WsnaEngineerToolDvo;
 import com.kyowon.sms.wells.web.service.stock.mapper.WsnaEngineerToolMapper;
 import com.sds.sflex.system.config.datasource.PageInfo;
 import com.sds.sflex.system.config.datasource.PagingResult;
+import com.sds.sflex.system.config.validation.BizAssert;
 
 import lombok.RequiredArgsConstructor;
 
@@ -25,6 +23,25 @@ public class WsnaEngineerToolService {
 
     public List<SearchRes> getEngineerToolDsbHist(SearchReq dto) {
         return mapper.selectEngineerToolDsbHist(dto);
+    }
+
+    public PagingResult<SearchRes> getEngineerToolDsbHistPages(SearchReq dto, PageInfo pageInfo) {
+        return mapper.selectEngineerToolDsbHist(dto, pageInfo);
+    }
+
+    @Transactional
+    public int removeEngineerTools(List<RemoveReq> dtos) {
+        int processCount = 0;
+
+        for (RemoveReq dto : dtos) {
+            WsnaEngineerToolDvo dvo = converter.mapRemoveReqToEngineerToolDsb(dto);
+            int result = mapper.deleteEngineerTools(dvo);
+
+            BizAssert.isTrue(result == 1, "MSG_ALT_SVE_ERR");
+            processCount += result;
+        }
+
+        return processCount;
     }
 
     public PagingResult<SearchPartsRes> getEngineerToolParts(PageInfo pageInfo) {
