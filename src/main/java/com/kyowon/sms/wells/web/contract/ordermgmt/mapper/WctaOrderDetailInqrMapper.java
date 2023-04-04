82288b3286493b0d445e3cec4aad581f07a45d77
JSY <gs.piit164@kyowon.co.kr>
2023-04-04 20:10:15 +0900

    fix : 휴대전화번호 복호화 설정

--- src/main/java/com/kyowon/sms/wells/web/contract/ordermgmt/mapper/WctaOrderDetailInqrMapper.java
+++ src/main/java/com/kyowon/sms/wells/web/contract/ordermgmt/mapper/WctaOrderDetailInqrMapper.java
@@ -5,16 +5,17 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.kyowon.sms.wells.web.contract.ordermgmt.dto.WctaOrderDetailInqrDto;
+import com.kyowon.sms.wells.web.contract.ordermgmt.dvo.WctaOrderDetailInqrDvo;
 import com.sds.sflex.system.config.datasource.PageInfo;
 import com.sds.sflex.system.config.datasource.PagingResult;
 
 @Mapper
 public interface WctaOrderDetailInqrMapper {
-    PagingResult<WctaOrderDetailInqrDto.SearchRes> selectOrderDetailSpayCntrtPages(
+    PagingResult<WctaOrderDetailInqrDvo> selectOrderDetailSpayCntrtPages(
         WctaOrderDetailInqrDto.SearchReq dto, PageInfo pageInfo
     );
 
-    List<WctaOrderDetailInqrDto.SearchRes> selectOrderDetailSpayCntrtPages(
+    List<WctaOrderDetailInqrDvo> selectOrderDetailSpayCntrtPages(
         WctaOrderDetailInqrDto.SearchReq dto
     );
 }
--- src/main/java/com/kyowon/sms/wells/web/contract/ordermgmt/service/WctaOrderDetailInqrService.java
+++ src/main/java/com/kyowon/sms/wells/web/contract/ordermgmt/service/WctaOrderDetailInqrService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.kyowon.sms.wells.web.contract.ordermgmt.converter.WctaOrderDetailInqrConverter;
 import com.kyowon.sms.wells.web.contract.ordermgmt.dto.WctaOrderDetailInqrDto;
+import com.kyowon.sms.wells.web.contract.ordermgmt.dvo.WctaOrderDetailInqrDvo;
 import com.kyowon.sms.wells.web.contract.ordermgmt.mapper.WctaOrderDetailInqrMapper;
 import com.sds.sflex.system.config.datasource.PageInfo;
 import com.sds.sflex.system.config.datasource.PagingResult;
@@ -15,16 +17,37 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class WctaOrderDetailInqrService {
     private final WctaOrderDetailInqrMapper mapper;
+    private final WctaOrderDetailInqrConverter converter;
 
     public PagingResult<WctaOrderDetailInqrDto.SearchRes> getOrderDetailSpayCntrtPages(
         WctaOrderDetailInqrDto.SearchReq dto, PageInfo pageInfo
     ) {
-        return mapper.selectOrderDetailSpayCntrtPages(dto, pageInfo);
+        PagingResult<WctaOrderDetailInqrDvo> dvos = mapper.selectOrderDetailSpayCntrtPages(dto, pageInfo);
+        PagingResult<WctaOrderDetailInqrDto.SearchRes> reqs = new PagingResult<WctaOrderDetailInqrDto.SearchRes>();
+
+        for (int i = 0; i < dvos.size(); i++) {
+            WctaOrderDetailInqrDvo dvo = dvos.get(i);
+            WctaOrderDetailInqrDto.SearchRes req = null;
+            req = converter.mapSearchResToWctaOrderDetailInqrDvo(dvo);
+            reqs.add(req);
+        }
+        System.out.println("장성 reqs= " + reqs);
+        System.out.println("장성 reqs1= " + reqs.get(1));
+        return reqs;
     }
 
     public List<WctaOrderDetailInqrDto.SearchRes> getOrderDetailSpayCntrtPagesExcelDownload(
         WctaOrderDetailInqrDto.SearchReq dto
     ) {
-        return mapper.selectOrderDetailSpayCntrtPages(dto);
+        List<WctaOrderDetailInqrDvo> dvos = mapper.selectOrderDetailSpayCntrtPages(dto);
+        List<WctaOrderDetailInqrDto.SearchRes> reqs = new PagingResult<WctaOrderDetailInqrDto.SearchRes>();
+        ;
+        for (int i = 0; i < dvos.size(); i++) {
+            WctaOrderDetailInqrDvo dvo = dvos.get(i);
+            WctaOrderDetailInqrDto.SearchRes req = null;
+            req = converter.mapSearchResToWctaOrderDetailInqrDvo(dvo);
+            reqs.add(req);
+        }
+        return reqs;
     }
 }
