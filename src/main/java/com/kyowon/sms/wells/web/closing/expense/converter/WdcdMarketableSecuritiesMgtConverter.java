e7fc0bcee35718f4a6f4cd34dcca134a1371db5c
Your Name <gs.piit172@kyowon.co.kr>
2023-04-07 16:41:27 +0900

    fix : @Mappe componentModel 추가

--- src/main/java/com/kyowon/sms/wells/web/closing/expense/converter/WdcdMarketableSecuritiesMgtConverter.java
+++ src/main/java/com/kyowon/sms/wells/web/closing/expense/converter/WdcdMarketableSecuritiesMgtConverter.java
@@ -2,7 +2,10 @@ package com.kyowon.sms.wells.web.closing.expense.converter;
 
 import com.kyowon.sms.wells.web.closing.expense.dto.WdcdMarketableSecuritiesMgtDto.EditReq;
 import com.kyowon.sms.wells.web.closing.expense.dvo.WdcdMarketableSecuritiesDvo;
+import org.mapstruct.Mapper;
 
+
+@Mapper(componentModel = "spring")
 public interface WdcdMarketableSecuritiesMgtConverter {
 
     WdcdMarketableSecuritiesDvo mapEditReqToWdcdMarketableSecuritiesDvo(EditReq req);
