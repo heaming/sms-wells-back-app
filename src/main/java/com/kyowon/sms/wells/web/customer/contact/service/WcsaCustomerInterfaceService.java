472d487e01ec4863c2ad704c0c152e5aebbe62c5
jeongeonkim <gs.piit142@kyowon.co.kr>
2023-04-12 13:20:32 +0900

    review: 공통서비스 리턴 dto -> dvo로 변경

--- src/main/java/com/kyowon/sms/wells/web/customer/contact/service/WcsaCustomerInterfaceService.java
+++ src/main/java/com/kyowon/sms/wells/web/customer/contact/service/WcsaCustomerInterfaceService.java
@@ -5,9 +5,9 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
-import com.kyowon.sms.common.web.customer.common.dto.ZcsaCustomerInfoDto.SearchCustomersRes;
 import com.kyowon.sms.common.web.customer.common.dvo.ZcsaCustomerInfoDvo;
 import com.kyowon.sms.common.web.customer.common.dvo.ZcsaCustomerInfoReqDvo;
+import com.kyowon.sms.common.web.customer.common.dvo.ZcsaCustomerInfoResDvo;
 import com.kyowon.sms.common.web.customer.common.service.ZcsaCustomerInfoService;
 import com.kyowon.sms.wells.web.customer.contact.converter.WcsaCustomerInterfaceConverter;
 import com.kyowon.sms.wells.web.customer.contact.dto.WcsaCustomerInterfaceDto.SearchCustomerInfoReq;
@@ -50,7 +50,7 @@ public class WcsaCustomerInterfaceService {
         //  2. 고객 조회 서비스 호출
         ZcsaCustomerInfoReqDvo param = converter.copy(dto);
 
-        List<SearchCustomersRes> pextCustomer = zcsaCustomerInfoService.getCustomers(param);
+        List<ZcsaCustomerInfoResDvo> pextCustomer = zcsaCustomerInfoService.getCustomers(param);
         if (pextCustomer.isEmpty()) {
             ifResDvo.setRsCd("S");//계약자 정보가 없습니다!
             ifResDvo.setRsMsg(null);//정상의 경우 결과코드(rsCd)만 송신한다.
@@ -58,15 +58,15 @@ public class WcsaCustomerInterfaceService {
         }
 
         //  3. 개인고객 / 법인고객 확인 (법인격구분에 따라 다르게 요청)
-        if ("1".equals(pextCustomer.get(0).copnDvCd())) {
+        if ("1".equals(pextCustomer.get(0).getCopnDvCd())) {
             //    - 개인고객 호출
             ZcsaCustomerInfoDvo indvCustomerDtl = zcsaCustomerInfoService
-                .getIndvCustomerInfo(pextCustomer.get(0).cstNo());
+                .getIndvCustomerInfo(pextCustomer.get(0).getCstNo());
             return converter.mapCreateCustomerDtlToCustomerRes(indvCustomerDtl, ifResDvo);
         } else {
             //    - 법인고객 호출
             ZcsaCustomerInfoDvo crpCustomerDtl = zcsaCustomerInfoService
-                .getCrpCustomerInfo(pextCustomer.get(0).cstNo());
+                .getCrpCustomerInfo(pextCustomer.get(0).getCstNo());
             return converter.mapCreateCustomerDtlToCustomerRes(crpCustomerDtl, ifResDvo);
         }
 
