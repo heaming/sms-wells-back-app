7ed6fc4fee4a24d845fd01c197005123fbc3b093
inho.choi <gs.piit181@kyowon.co.kr>
2023-04-12 14:15:29 +0900

    fix : 파일 누락

---
+++ src/main/java/com/kyowon/sms/wells/web/service/stock/dvo/WsnaNormalOutOfStorageDvo.java
@@ -0,0 +1,10 @@
+package com.kyowon.sms.wells.web.service.stock.dvo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class WsnaNormalOutOfStorageDvo {
+    String qty;
+}
