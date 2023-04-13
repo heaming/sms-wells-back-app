56b852362dd154a1d86dcff735b4252619dd367d
juno.cha <gs.piit134@kyowon.com>
2023-04-13 15:55:27 +0900

    fix : 주기표 생성 로직 수정 및 소스 정리, W-SV-S-0051(모종 일시불 구매 고객 정보 및 방문 스케쥴 인서트업데이트) & W-SV-S-0052(홍삼 캡슐 정기구매 고객 스케쥴 인서트업데이트) 임시 commit

--- src/main/java/com/kyowon/sms/wells/web/service/allocate/dvo/WsncBsPeriodChartReqDvo.java
+++ src/main/java/com/kyowon/sms/wells/web/service/allocate/dvo/WsncBsPeriodChartReqDvo.java
@@ -33,6 +33,6 @@ public class WsncBsPeriodChartReqDvo {
     String crtErrCn;
     String pgrmId;
     String vVs107CnclDt;
-    String newGdsCD;
+    String newGdsCd;
     String newPartList;
 }
--- src/main/java/com/kyowon/sms/wells/web/service/allocate/rest/WsncBsPeriodChartController.java
+++ src/main/java/com/kyowon/sms/wells/web/service/allocate/rest/WsncBsPeriodChartController.java
@@ -53,4 +53,50 @@ public class WsncBsPeriodChartController {
         return SaveResponse.builder().processCount(service.processBsPeriodChartBs04(dto)).build();
     }
 
+    @ApiOperation(value = "홈카페 캡슐 B/S주기표를 생성", notes = "홈카페 캡슐 B/S주기표를 생성")
+    @ApiImplicitParams(value = {
+        @ApiImplicitParam(name = "cntrNo", value = "계약번호", paramType = "query", required = true),
+        @ApiImplicitParam(name = "cntrSn", value = "계약일련번호", paramType = "query", required = true),
+    })
+    @PostMapping("/home-cafe")
+    public SaveResponse processBSPeriodChartBs01(WsncBsPeriodChartDto.SearchReq dto) throws Exception {
+        return SaveResponse.builder().processCount(service.processBsPeriodChartBs01(dto)).build();
+    }
+
+    @ApiOperation(value = "웰스팜 모종 BS주기표를 생성", notes = "웰스팜 모종 BS주기표를 생성")
+    @ApiImplicitParams(value = {
+        @ApiImplicitParam(name = "cntrNo", value = "계약번호", paramType = "query", required = true),
+        @ApiImplicitParam(name = "cntrSn", value = "계약일련번호", paramType = "query", required = true),
+    })
+    @PostMapping("/wells-farm")
+    public SaveResponse processBSPeriodChartBs05(WsncBsPeriodChartDto.SearchReq dto) throws Exception {
+        return SaveResponse.builder().processCount(service.processBsPeriodChartBs05(dto)).build();
+    }
+
+
+
+
+
+
+    @ApiOperation(value = "모종 일시불 구매 고객 정보 및 방문 스케쥴 인서트업데이트", notes = "모종 일시불 구매 고객 정보 및 방문 스케쥴 인서트업데이트")
+    @ApiImplicitParams(value = {
+        @ApiImplicitParam(name = "cntrNo", value = "계약번호", paramType = "query", required = true),
+        @ApiImplicitParam(name = "cntrSn", value = "계약일련번호", paramType = "query", required = true),
+    })
+    @PostMapping("/plant-schedule")
+    public SaveResponse processSpMkPlantSchedule(WsncBsPeriodChartDto.SearchReq dto) throws Exception {
+        return SaveResponse.builder().processCount(service.processSpMkPlantSchedule(dto)).build();
+    }
+
+    @ApiOperation(value = "홍삼 캡슐 정기구매 고객 스케쥴 인서트업데이트", notes = "홍삼 캡슐 정기구매 고객 스케쥴 인서트업데이트")
+    @ApiImplicitParams(value = {
+        @ApiImplicitParam(name = "cntrNo", value = "계약번호", paramType = "query", required = true),
+        @ApiImplicitParam(name = "cntrSn", value = "계약일련번호", paramType = "query", required = true),
+    })
+    @PostMapping("/redginseng-schedule")
+    public SaveResponse processSpMkRedginsengSchedule(WsncBsPeriodChartDto.SearchReq dto) throws Exception {
+        return SaveResponse.builder().processCount(service.processSpMkRedginsengSchedule(dto)).build();
+    }
+
+
 }
