62cdccb951574120f175f215e034188a29298c41
gs.piit41 <gs.piit41>
2023-04-13 15:18:52 +0900

    feat 재약정 대상고객 서비스 개발

---
+++ src/main/java/com/kyowon/sms/wells/web/contract/ordermgmt/service/WctaMembershipService.java
@@ -0,0 +1,21 @@
+package com.kyowon.sms.wells.web.contract.ordermgmt.service;
+
+import com.kyowon.sms.wells.web.contract.ordermgmt.mapper.WctaMembershipMapper;
+import com.sds.sflex.system.config.datasource.PageInfo;
+import com.sds.sflex.system.config.datasource.PagingResult;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import static com.kyowon.sms.wells.web.contract.ordermgmt.dto.WctaMembershipDto.SearchReq;
+import static com.kyowon.sms.wells.web.contract.ordermgmt.dto.WctaMembershipDto.SearchRes;
+
+@Service
+@RequiredArgsConstructor
+public class WctaMembershipService {
+
+    private final WctaMembershipMapper mapper;
+
+    public PagingResult<SearchRes> getMembershipCustomerPages(SearchReq dto, PageInfo pageInfo) {
+        return mapper.selectMembershipCustomers(dto, pageInfo);
+    }
+}
