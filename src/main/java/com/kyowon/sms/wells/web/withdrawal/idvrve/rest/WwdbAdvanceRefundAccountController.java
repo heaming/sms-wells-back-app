54812dcd84eb1d9414ec8e7b667b776cd8ff8481
sonkiseok <gs.piit187@kyowon.com>
2023-04-14 13:54:50 +0900

    fix: 코드리뷰 수정
    * GetMapping에서 Parameter를 @ApiImplicitParams로 정의

--- src/main/java/com/kyowon/sms/wells/web/withdrawal/idvrve/rest/WwdbAdvanceRefundAccountController.java
+++ src/main/java/com/kyowon/sms/wells/web/withdrawal/idvrve/rest/WwdbAdvanceRefundAccountController.java
@@ -16,8 +16,9 @@ import com.sds.sflex.system.config.datasource.PageInfo;
 import com.sds.sflex.system.config.datasource.PagingResult;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 
 @Api(tags = "[입금관리 - 개별수납] 선환불계좌조회")
@@ -29,9 +30,14 @@ public class WwdbAdvanceRefundAccountController {
     private final WwdbAdvanceRefundAccountService service;
 
     @ApiOperation(value = "선환불계좌조회 목록", notes = "선환불계좌조회 목록")
+    @ApiImplicitParams({
+        @ApiImplicitParam(name = "stlmTpCd", value = "대상구분", required = false),
+        @ApiImplicitParam(name = "startDay", value = "처리시작일자", required = false),
+        @ApiImplicitParam(name = "endDay", value = "처리종료일자", required = false),
+        @ApiImplicitParam(name = "cshRfndDvCd", value = "업무구분", required = false),
+    })
     @GetMapping("/paging")
     public PagingResult<SearchAdvanceRefundAccountRes> getAdvanceRefundAccountPages(
-        @ApiParam
         @Valid
         SearchAdvanceRefundAccountReq req,
         @Valid
@@ -41,6 +47,12 @@ public class WwdbAdvanceRefundAccountController {
     }
 
     @ApiOperation(value = "선환불계좌조회 엑셀 다운로드", notes = "선환불계좌조회 엑셀 다운로드")
+    @ApiImplicitParams({
+        @ApiImplicitParam(name = "stlmTpCd", value = "대상구분", required = false),
+        @ApiImplicitParam(name = "startDay", value = "처리시작일자", required = false),
+        @ApiImplicitParam(name = "endDay", value = "처리종료일자", required = false),
+        @ApiImplicitParam(name = "cshRfndDvCd", value = "업무구분", required = false),
+    })
     @GetMapping("/excel-download")
     public List<SearchAdvanceRefundAccountRes> getAdvanceRefundAccountExcels(
         SearchAdvanceRefundAccountReq req
