a01d37bb365f246c3465b089d1043f32b12339fe
Kicheol Choi <gs.piit170@kyowon.com>
2023-04-14 11:24:35 +0900

    chore: 기타선수금 생성(WELLS) - 수정
    * parameters 간소화

--- src/main/java/com/kyowon/sms/wells/web/closing/payment/mapper/WdcaEtcAnticipationAmtMapper.java
+++ src/main/java/com/kyowon/sms/wells/web/closing/payment/mapper/WdcaEtcAnticipationAmtMapper.java
@@ -1,45 +1,51 @@
 package com.kyowon.sms.wells.web.closing.payment.mapper;
 
-import com.kyowon.sms.wells.web.closing.payment.dvo.WdcaEtcAnticipationAmtDvo;
 import org.apache.ibatis.annotations.Mapper;
+
+import com.kyowon.sms.wells.web.closing.payment.dvo.WdcaEtcAnticipationAmtDvo;
 
 // TODO: 테이블 미정의로 추후 재작업 필요
 @Mapper
 public interface WdcaEtcAnticipationAmtMapper {
     /**
     * 기타선수금 번호 채번
-    * @return 기타선수금번호 MAX + 1
+    * @param itgDpNo 통합입금번호
+    * @return 기타선수금 번호 MAX + 1
     */
-    WdcaEtcAnticipationAmtDvo selectMaxEtcAnticipationAmtNo();
+    String selectMaxEtcAnticipationNo(
+        String itgDpNo
+    );
 
     /**
-    * 기타선수금 테이블 저장
+    * 기타선수금 일련번호 채번
     * @param etcAtamNo 기타선수금번호
-    * @param etcAtamOcDt 기타선수금발생일자
-    * @param itgDpNo 통합입금번호
-    * @param etcAtamAmt 기타선수금금액
-    * @param etcAtamTpCd 기타선수금유형코드
-    * @param etcAtamBlam 기타선수금잔액
-    * @param cntrNo 계약번호
-    * @param cntrSn 계약일련번호
-    * @param coCd 회사코드
-    * @param rveCd 수납코드
-    * @param ichrPrtnrNo 담당파트너번호
-    * @param custNo 고객번호
-    * @return count
+    * @return 기타선수금 일련번호 MAX + 1
     */
-    int insertEtcAnticipationAmt(
-        String etcAtamNo,
-        String etcAtamOcDt,
-        String itgDpNo,
-        String etcAtamAmt,
-        String etcAtamTpCd,
-        String etcAtamBlam,
-        String cntrNo,
-        String cntrSn,
-        String coCd,
-        String rveCd,
-        String ichrPrtnrNo,
-        String custNo
+    int selectMaxEtcAnticipationSn(
+        String etcAtamNo
+    );
+
+    /**
+    * 기타선수금 기본 테이블 저장
+    * @param dvo 기타선수금 DVO
+    */
+    int insertEtcBasic(
+        WdcaEtcAnticipationAmtDvo dvo
+    );
+
+    /**
+    * 기타선수금 처리내역 테이블 저장
+    * @param dvo 기타선수금 DVO
+    */
+    int insertEtcProcess(
+        WdcaEtcAnticipationAmtDvo dvo
+    );
+
+    /**
+    * 기타선수금 잔액 수정
+    * @param etcAtamNo 기타선수금번호
+    */
+    int updateEtcBasic(
+        String etcAtamNo
     );
 }
