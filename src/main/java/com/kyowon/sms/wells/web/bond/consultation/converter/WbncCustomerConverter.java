9350a1e8712a23b84c00407cad5a952ade7a6da9
choi sung hun <gs.piit151@kyowon.com>
2023-04-18 11:18:25 +0900

    chore: 고객상세 상담 등록 기능추가 / 쿼리수정

---
+++ src/main/java/com/kyowon/sms/wells/web/bond/consultation/converter/WbncCustomerConverter.java
@@ -0,0 +1,17 @@
+package com.kyowon.sms.wells.web.bond.consultation.converter;
+
+import org.apache.commons.lang.StringUtils;
+import org.mapstruct.Mapper;
+
+import com.kyowon.sms.wells.web.bond.consultation.dto.WbncCustomerDto.SaveCounselReq;
+import com.kyowon.sms.wells.web.bond.consultation.dto.WbncCustomerDto.SaveUnuitmCnReq;
+import com.kyowon.sms.wells.web.bond.consultation.dvo.WbncCustomerDvo;
+
+@Mapper(componentModel = "spring", imports = {StringUtils.class})
+public interface WbncCustomerConverter {
+
+    WbncCustomerDvo mapSaveReqToEbncUnuitmCnDvo(SaveUnuitmCnReq dto);
+
+    WbncCustomerDvo mapSaveReqToEbncCounselDvo(SaveCounselReq dto);
+
+}
---
+++ src/main/java/com/kyowon/sms/wells/web/bond/consultation/dvo/WbncCustomerDvo.java
@@ -0,0 +1,39 @@
+package com.kyowon.sms.wells.web.bond.consultation.dvo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class WbncCustomerDvo {
+    String bndUnuitmId;
+    String bndCntrTpCd;
+    String bndBizDvCd;
+    String cstNo;
+    String cntrNo;
+    String cntrSn;
+    String clctnOjOgTpCd;
+    String clctnOjPrtnrNo;
+    String cnslUnuitmCn;
+    String cvcpCn;
+    String telCnslId;
+    String clctnOjPrtnrBzrno;
+    String clctamPrtnrNo;
+    String telCnslPhCd;
+    String bndCnslMtrDvCd;
+    String telCnslRsCd;
+    String cnslCstPrpCd;
+    String telCnslCn;
+    String bndClnPsblDvCd;
+    String bndCnslCstStatCd;
+    String bndClnPrcsDvCd;
+    String dprNm;
+    String rdgId;
+    String promBooId;
+    String promDt;
+    String promHh;
+    String clctamPromTpCd;
+    String clnPromAmt;
+    String promCn;
+    String promFshYn;
+}
