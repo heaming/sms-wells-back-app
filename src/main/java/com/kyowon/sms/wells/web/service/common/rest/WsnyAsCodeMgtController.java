438cd261600a3a85a16b2423c86e50044625ef39
gs.piit122 <kdycorp@kyowon.co.kr>
2023-04-19 09:46:23 +0900

    fix: 오류정리(단위테스트 대비)

--- src/main/java/com/kyowon/sms/wells/web/service/common/rest/WsnyAsCodeMgtController.java
+++ src/main/java/com/kyowon/sms/wells/web/service/common/rest/WsnyAsCodeMgtController.java
@@ -3,6 +3,10 @@ package com.kyowon.sms.wells.web.service.common.rest;
 import com.kyowon.sms.wells.web.service.common.dto.WsnyAsCodeMgtDto;
 import com.kyowon.sms.wells.web.service.common.service.WsnyAsCodeMgtService;
 import com.kyowon.sms.wells.web.service.zcommon.constants.SnServiceConst;
+import com.sds.sflex.common.common.dto.ExcelUploadDto;
+import com.sds.sflex.common.common.dvo.ExcelMetaDvo;
+import com.sds.sflex.common.common.dvo.ExcelUploadErrorDvo;
+import com.sds.sflex.common.common.service.ExcelReadService;
 import com.sds.sflex.system.config.datasource.PageInfo;
 import com.sds.sflex.system.config.datasource.PagingResult;
 import io.swagger.annotations.Api;
@@ -15,9 +19,12 @@ import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping(SnServiceConst.REST_URL_V1 + "/as-codes")
@@ -28,6 +35,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class WsnyAsCodeMgtController {
 
     private final WsnyAsCodeMgtService service;
+
 
     @ApiOperation(value = "AS 코드 관리 목록조회", notes = "조회조건에 일치하는 AS 코드 정보를 조회한다.")
     @ApiImplicitParams(value = {
@@ -44,6 +52,32 @@ public class WsnyAsCodeMgtController {
         PageInfo pageInfo
     ) {
         return service.getAsCodePages(dto, pageInfo);
+    }
+
+    @ApiOperation(value = "AS 코드 관리 목록조회 Excel Download", notes = "조회조건에 일치하는 AS 코드 정보를 조회한다.")
+    @ApiImplicitParams(value = {
+        @ApiImplicitParam(name = "pdGrpCd", value = "상품그룹", paramType = "query", example = "1"),
+        @ApiImplicitParam(name = "pdCd", value = "상품명", paramType = "query"),
+        @ApiImplicitParam(name = "svTpCd", value = "서비스 유형", paramType = "query", example = "3"),
+        @ApiImplicitParam(name = "asLctCd", value = "A/S위치", paramType = "query", example = "T236"),
+        @ApiImplicitParam(name = "apyChk", value = "적용일자선택", paramType = "query", example = "1"),
+        @ApiImplicitParam(name = "applyDate", value = "적용일자", paramType = "query", dataType = "date", example = "20220101")
+    })
+    @GetMapping("/excel-download")
+    public PagingResult<WsnyAsCodeMgtDto.SearchRes> getAsCodeExcelDownload(
+        WsnyAsCodeMgtDto.SearchReq dto, @Valid
+        PageInfo pageInfo
+    ) {
+        return service.getAsCodePages(dto, pageInfo);
+    }
+
+    @PostMapping("/excel-upload")
+    public ExcelUploadDto.UploadRes uploadExcel(
+        @RequestParam("file")
+        MultipartFile file
+    ) throws Exception {
+        return service.uploadExcel(file);
+
     }
 
 }
--- src/main/java/com/kyowon/sms/wells/web/service/common/service/WsnyAsCodeMgtService.java
+++ src/main/java/com/kyowon/sms/wells/web/service/common/service/WsnyAsCodeMgtService.java
@@ -1,13 +1,26 @@
 package com.kyowon.sms.wells.web.service.common.service;
 
+import com.kyowon.sms.common.web.closing.mileage.dvo.ZdceSmartMileageExcelDvo;
+import com.kyowon.sms.common.web.closing.mileage.util.ZdceMileageUtil;
 import com.kyowon.sms.wells.web.service.common.converter.WsnyAsCodeMgtConverter;
 import com.kyowon.sms.wells.web.service.common.dto.WsnyAsCodeMgtDto;
+import com.kyowon.sms.wells.web.service.common.dvo.WsnyAsCodeMgtDvo;
 import com.kyowon.sms.wells.web.service.common.mapper.WsnyAsCodeMgtMapper;
+import com.sds.sflex.common.common.dto.ExcelUploadDto;
+import com.sds.sflex.common.common.dvo.ExcelMetaDvo;
+import com.sds.sflex.common.common.dvo.ExcelUploadErrorDvo;
+import com.sds.sflex.common.common.service.ExcelReadService;
+import com.sds.sflex.system.config.core.service.MessageResourceService;
 import com.sds.sflex.system.config.datasource.PageInfo;
 import com.sds.sflex.system.config.datasource.PagingResult;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  *
@@ -25,6 +38,9 @@ public class WsnyAsCodeMgtService {
 
     private final WsnyAsCodeMgtMapper mapper;
     private final WsnyAsCodeMgtConverter converter;
+    private final Map<String, String> header;
+    private final MessageResourceService messageResourceService;
+    private final ExcelReadService excelReadService;
 
     public PagingResult<WsnyAsCodeMgtDto.SearchRes> getAsCodePages(
         WsnyAsCodeMgtDto.SearchReq dto, PageInfo pageInfo
@@ -32,6 +48,31 @@ public class WsnyAsCodeMgtService {
         return new PagingResult<>(
             converter.mapAllSearchResToDvo(mapper.selectAsCodePages(dto, pageInfo)), pageInfo
         );
+    }
+
+    public ExcelUploadDto.UploadRes uploadExcel(
+        final MultipartFile file
+    ) throws Exception {
+
+        header.put("evnNm", messageResourceService.getMessage("MSG_TXT_RV_RSON"));
+        header.put("mlgExnDt", messageResourceService.getMessage("MSG_TXT_VALID_PERIOD"));
+
+        final List<WsnyAsCodeMgtDvo> dvos = excelReadService
+            .readExcel(file, new ExcelMetaDvo(1, header), WsnyAsCodeMgtDvo.class);
+
+        //처리한 뒤 오류가 있으면 뱉어주고,  없으면 status = "S" 로 내려준다.
+        List<ExcelUploadErrorDvo> excelUploadErrorDvos = new ArrayList<>();
+
+        for (WsnyAsCodeMgtDvo dvo : dvos) {
+            log.debug(dvo.getAsCode());
+        }
+
+        //    ...
+
+        return ExcelUploadDto.UploadRes.builder()
+            .status(excelUploadErrorDvos.isEmpty() ? "S" : "E")
+            .errorInfo(excelUploadErrorDvos)
+            .build();
     }
 
 }
