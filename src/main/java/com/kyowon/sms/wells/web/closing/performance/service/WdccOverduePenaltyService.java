5d959a7386767a2f5ae1612d5b596fefc7bb8bc5
Your Name <gs.piit172@kyowon.co.kr>
2023-04-21 17:40:51 +0900

    chore : 조회조건 수정 및 컬럼명 수정

--- src/main/java/com/kyowon/sms/wells/web/closing/performance/service/WdccOverduePenaltyService.java
+++ src/main/java/com/kyowon/sms/wells/web/closing/performance/service/WdccOverduePenaltyService.java
@@ -1,7 +1,6 @@
 package com.kyowon.sms.wells.web.closing.performance.service;
 
-import com.kyowon.sms.wells.web.closing.performance.dto.WdccOverduePenaltyDto.SearchReq;
-import com.kyowon.sms.wells.web.closing.performance.dto.WdccOverduePenaltyDto.SearchRes;
+import com.kyowon.sms.wells.web.closing.performance.dto.WdccOverduePenaltyDto.*;
 import com.kyowon.sms.wells.web.closing.performance.mapper.WdccOverduePenaltyMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,27 +15,37 @@ public class WdccOverduePenaltyService {
 
     private final WdccOverduePenaltyMapper mapper;
 
-    public List<SearchRes> getSalesBondAdditionalChargesAggregate(
-        SearchReq req
-    ) {
-        return mapper.selectSalesBondAdditionalChargesAggregate(req);
+    public List<FindCodeRes> getCode() {
+        return mapper.selectCode();
     }
 
-    public List<SearchRes> getSalesBondAdditionalChargesDates(
+    public List<SearchMainGridRes> getMainGridAggregate(
         SearchReq req
     ) {
-        return mapper.selectSalesBondAdditionalChargesDates(req);
+        return mapper.selectMainGridAggregate(req);
     }
 
-    public List<SearchRes> getSalesBondAdditionalChargesOrders(
+    public List<SearchMainGridRes> getMainGridDates(
         SearchReq req
     ) {
-        return mapper.selectSalesBondAdditionalChargesOrders(req);
+        return mapper.selectMainGridDates(req);
     }
 
-    public List<SearchRes> getSalesBondAdditionalChargesMembers(
+    public List<SearchSubGridRes> getSubGridOrder(
         SearchReq req
     ) {
-        return mapper.selectSalesBondAdditionalChargesMembers(req);
+        return mapper.selectSubGridOrder(req);
+    }
+
+    public List<SearchThirdGridRes> getThirdGridAggregate(
+        SearchReq req
+    ) {
+        return mapper.selectThirdGridAggregate(req);
+    }
+
+    public List<SearchFourthGridRes> getFourthGridOrder(
+        SearchReq req
+    ) {
+        return mapper.selectFourthGridOrder(req);
     }
 }
