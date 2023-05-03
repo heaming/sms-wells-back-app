5884b50a27d08013082c47817c6509984085a1f2
gs.piit201 <gs.piit201@kyowon.co.kr>
2023-05-03 14:04:38 +0900

    feat : 인터페이스 신규 개발
    *(W-WD-I-0012)-wells 카드 자동이체 가능일자 조회
    *(W-WD-I-0013)-wells 계좌 자동이체 가능일자 조회
    
    Signed-off-by: gs.piit201 <gs.piit201@kyowon.co.kr>

---
+++ src/main/java/com/kyowon/sms/wells/web/withdrawal/interfaces/dto/WwdaAutomaticTransferPossibleDateInterfaceDto.java
@@ -0,0 +1,31 @@
+package com.kyowon.sms.wells.web.withdrawal.interfaces.dto;
+
+import javax.validation.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import io.swagger.annotations.ApiModel;
+
+public class WwdaAutomaticTransferPossibleDateInterfaceDto {
+
+    /*
+        wells 자동이체 가능일자 조회 Req Dto
+     */
+    @ApiModel("WwdaAutomaticTransferPossibleDateInterfaceDto-SearchReq")
+    public record SearchReq(
+        @NotBlank
+        @JsonProperty("BIL_YM")
+        String bilYm // 청구년월
+    ) {}
+
+    /*
+        wells 자동이체 가능일자 조회 Res Dto
+     */
+    @ApiModel("WwdaAutomaticTransferPossibleDateInterfaceDto-SearchRes")
+    public record SearchRes(
+        @JsonProperty("RGL_FNT_D")
+        String rglFntD, // 정규이체일
+        @JsonProperty("BIL_TP_CD_NM")
+        String bilTpCdNm // 청구유형코드명
+    ) {}
+}
