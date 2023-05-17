253680afd24b32b4af1c645ff538fa949cb779e5
Jeon SeoIn <gs.piit56@kyowon.com>
2023-05-17 16:53:27 +0900

    chore: 엔지니어 기능 추가 및 실 쿼리 적용

--- src/main/java/com/kyowon/sms/wells/web/fee/calculation/dvo/WfebEgerAllowanceDvo.java
+++ src/main/java/com/kyowon/sms/wells/web/fee/calculation/dvo/WfebEgerAllowanceDvo.java
@@ -1,8 +1,25 @@
 package com.kyowon.sms.wells.web.fee.calculation.dvo;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class WfebEgerAllowanceDvo {
 
     String param1;
     String param2;
+
+    String perfYm;
+    String ogId;
+    String ogCd;
+    String rsbDvCd;
+    String prtnrNo;
+    String feeCd;
+    int feeAmt;
+    String baseYm;
+    String type;
+    String confirm;
+    String hdof;
 
 }
