df54e9c1ea8db6437760a7036fd53571d4bc894f
juno.cha <gs.piit134@kyowon.com>
2023-05-19 15:54:12 +0900

    feature : EAI Interface Controller Test 1차 commit

--- src/main/java/com/kyowon/sms/wells/web/service/interfaces/rest/WsniEaiCommonInterfaceController.java
+++ src/main/java/com/kyowon/sms/wells/web/service/interfaces/rest/WsniEaiCommonInterfaceController.java
@@ -17,7 +17,7 @@ import java.util.Map;
 
 @InterfaceController
 @Api(tags = SnServiceConst.REST_INTERFACE_DOC_V1)
-@RequestMapping(SnServiceConst.REST_INTERFACE_URL_V1 + "/sn-eai-common")
+@RequestMapping(SnServiceConst.REST_INTERFACE_URL_V1 + "/sn-eai-commons")
 @RequiredArgsConstructor
 @Validated
 public class WsniEaiCommonInterfaceController {
