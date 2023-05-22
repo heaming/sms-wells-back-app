cd2f4284724e0bc9fe261f241fe5352343331084
김승수 <gs.piit184@kyowon.com>
2023-05-22 14:21:50 +0900

    feat: W-SV-U-0294M01(BS소모품 배부집계 현황) 개발

---
+++ src/main/java/com/kyowon/sms/wells/web/service/stock/mapper/WsnaBsCsmbDeliveryAggregateMapper.java
@@ -0,0 +1,20 @@
+package com.kyowon.sms.wells.web.service.stock.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.kyowon.sms.wells.web.service.stock.dto.WsnaBsCsmbDeliveryAggregateDto.SearchQtysRes;
+import com.kyowon.sms.wells.web.service.stock.dto.WsnaBsCsmbDeliveryAggregateDto.SearchReq;
+import com.kyowon.sms.wells.web.service.stock.dvo.WsnaBsCsmbDeliveryAggregateDvo;
+import com.sds.sflex.system.config.datasource.PageInfo;
+import com.sds.sflex.system.config.datasource.PagingResult;
+
+@Mapper
+public interface WsnaBsCsmbDeliveryAggregateMapper {
+    List<WsnaBsCsmbDeliveryAggregateDvo> selectDeliveryAggregate(SearchReq dto);
+
+    PagingResult<WsnaBsCsmbDeliveryAggregateDvo> selectDeliveryAggregate(SearchReq dto, PageInfo pageInfo);
+
+    List<SearchQtysRes> selectItemQtys(String mngtYmFrom, String mngtYmTo);
+}
---
+++ src/main/java/com/kyowon/sms/wells/web/service/stock/rest/WsnaBsCsmbDeliveryAggregateController.java
@@ -0,0 +1,50 @@
+package com.kyowon.sms.wells.web.service.stock.rest;
+
+import java.util.List;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kyowon.sms.wells.web.service.stock.dto.WsnaBsCsmbDeliveryAggregateDto.SearchQtysRes;
+import com.kyowon.sms.wells.web.service.stock.dto.WsnaBsCsmbDeliveryAggregateDto.SearchReq;
+import com.kyowon.sms.wells.web.service.stock.dto.WsnaBsCsmbDeliveryAggregateDto.SearchRes;
+import com.kyowon.sms.wells.web.service.stock.service.WsnaBsCsmbDeliveryAggregateService;
+import com.kyowon.sms.wells.web.service.zcommon.constants.SnServiceConst;
+import com.sds.sflex.system.config.datasource.PageInfo;
+import com.sds.sflex.system.config.datasource.PagingResult;
+
+import io.swagger.annotations.Api;
+import lombok.RequiredArgsConstructor;
+
+@Api(tags = "[WSNA] B/S소모품 배부집계 현황")
+@Validated
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(SnServiceConst.REST_URL_V1 + "/delivery-aggregates")
+public class WsnaBsCsmbDeliveryAggregateController {
+
+    private final WsnaBsCsmbDeliveryAggregateService service;
+
+    @GetMapping
+    public List<SearchRes> getDeliveryAggregates(SearchReq dto) {
+        return service.getDeliveryAggregates(dto);
+    }
+
+    @GetMapping("/paging")
+    public PagingResult<SearchRes> getDeliveryAggregatePages(SearchReq dto, PageInfo pageInfo) {
+        return service.getDeliveryAggregatePages(dto, pageInfo);
+    }
+
+    @GetMapping("/{mngtYmFrom}-{mngtYmTo}")
+    public List<SearchQtysRes> getItemQtys(
+        @PathVariable
+        String mngtYmFrom,
+        @PathVariable
+        String mngtYmTo
+    ) {
+        return service.getItemQtys(mngtYmFrom, mngtYmTo);
+    }
+}
