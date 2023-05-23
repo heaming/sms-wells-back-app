8739f674a828d28737badcba5377a1a025984a6d
박주형 <gs.piit41@kyowon.co.kr>
2023-05-23 14:39:46 +0900

    feat: 삼성전자 상품관리 확정일 업로드 개발

--- src/main/java/com/kyowon/sms/wells/web/contract/salesstatus/dto/WcteSecProductDto.java
+++ src/main/java/com/kyowon/sms/wells/web/contract/salesstatus/dto/WcteSecProductDto.java
@@ -10,10 +10,13 @@ public class WcteSecProductDto {
     // *********************************************************
     // Request Dto
     // *********************************************************
-    // 삼성전자 주문 정보 관리 Search Request Dto
+    // 삼성전자 주문 정보 예약일 관리 Search Request Dto
     @Builder
-    @ApiModel("WcteSecProductDto-SearchReq")
-    public record SearchReq(
+    @ApiModel(
+        value = "WcteSecProductDto-SearchReservationReq",
+        description = "삼성전자 주문 정보 예약일 Search Request Dto"
+    )
+    public record SearchReservationReq(
         @NotBlank
         @ValidDate
         String strtdt,
@@ -25,12 +28,52 @@ public class WcteSecProductDto {
     ) {
     }
 
+    @Builder
+    @ApiModel(
+        value = "WcteSecProductDto-SearchConfirmReq",
+        description = "삼성전자 주문 정보 확정일 Search Request Dto"
+    )
+    public record SearchConfirmReq(
+        @NotBlank
+        @ValidDate
+        String strtdt,
+
+        @NotBlank
+        @ValidDate
+        String enddt,
+        String sellTpCd,
+        String sppBzsOrdId,
+        String pdctIdno
+    ) {
+    }
+
+    @Builder
+    @ApiModel(
+        value = "WcteSecProductDto-CreateConfirmReq",
+        description = "삼성전자 주문 정보 확정일 Request Dto"
+    )
+    public record CreateConfirmReq(
+        @NotBlank
+        String cntrNo,
+        int cntrSn,
+        @NotBlank
+        @ValidDate
+        String sppFshDt,
+        @NotBlank
+        String pdctIdno,
+        @NotBlank
+        String sppBzsModelId,
+        @NotBlank
+        String sppBzsOrdId
+    ) {
+    }
+
     // *********************************************************
     // Result Dto
     // *********************************************************
     // 삼성전자 주문 정보 관리 Search Result Dto
-    @ApiModel("WcteSecProductDto-SearchRes")
-    public record SearchRes(
+    @ApiModel("WcteSecProductDto-SearchReservationRes")
+    public record SearchReservationRes(
         String ogCd,
         String hooPrtnrNo,
         String sellPrtnrNo,
@@ -45,6 +88,31 @@ public class WcteSecProductDto {
         String resDt,
         String stocStrDt,
         String fstRgstDtm
+    ) {
+    }
+
+    @ApiModel(
+        value = "WcteSecProductDto-SearchConfirmRes",
+        description= "삼성전자 주문 정보 확정일 Response Dto"
+    )
+    public record SearchConfirmRes(
+        String cntrNo,
+        int cntrSn,
+        String sellTpCd,
+        String pdHclsfId,
+        String pdMclsfId,
+        String rcgvpKnm,
+        String sppBzsOrdId,
+        String sppFshDt,
+        String pdCd,
+        String pdNm,
+        String sppFshRgstDtm,
+        String batWkFshDtm,
+        String canDt,
+        String cttRsCd,
+        String pdctIdno,
+        String sppBzsModelId,
+        String rgstFeeFlpymYn
     ) {
     }
 }
--- src/main/java/com/kyowon/sms/wells/web/contract/salesstatus/service/WcteSecProductService.java
+++ src/main/java/com/kyowon/sms/wells/web/contract/salesstatus/service/WcteSecProductService.java
@@ -1,29 +1,110 @@
 package com.kyowon.sms.wells.web.contract.salesstatus.service;
 
-import java.util.List;
-
-import org.springframework.stereotype.Service;
-
+import com.kyowon.sms.wells.web.contract.salesstatus.converter.WcteSecProductConverter;
+import com.kyowon.sms.wells.web.contract.salesstatus.dvo.WcteConfirmValidationDvo;
+import com.kyowon.sms.wells.web.contract.salesstatus.dvo.WcteInvoiceProcessIzDvo;
+import com.kyowon.sms.wells.web.contract.salesstatus.dvo.WctePdOstrIzDvo;
 import com.kyowon.sms.wells.web.contract.salesstatus.mapper.WcteSecProductMapper;
-
-import static com.kyowon.sms.wells.web.contract.salesstatus.dto.WcteSecProductDto.*;
-
 import com.sds.sflex.system.config.datasource.PageInfo;
 import com.sds.sflex.system.config.datasource.PagingResult;
-
+import com.sds.sflex.system.config.exception.BizException;
+import com.sds.sflex.system.config.validation.BizAssert;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static com.kyowon.sms.wells.web.contract.salesstatus.dto.WcteSecProductDto.*;
 
 @Service
 @RequiredArgsConstructor
 public class WcteSecProductService {
 
     private final WcteSecProductMapper mapper;
+    private final WcteSecProductConverter converter;
 
-    public PagingResult<SearchRes> getReservationPages(SearchReq dto, PageInfo pageInfo) {
+    public PagingResult<SearchReservationRes> getReservationPages(SearchReservationReq dto, PageInfo pageInfo) {
         return mapper.selectReservationPages(dto, pageInfo);
     }
 
-    public List<SearchRes> getReservationsForExcelDownload(SearchReq dto) {
+    public List<SearchReservationRes> getReservationsForExcelDownload(SearchReservationReq dto) {
         return mapper.selectReservationPages(dto);
+    }
+
+    public PagingResult<SearchConfirmRes> getConfirmPages(SearchConfirmReq dto, PageInfo pageInfo) {
+        return mapper.selectConfirms(dto, pageInfo);
+    }
+
+    /**
+     * 삼성전자 확정일 목록 조회
+     * @param dto SearchConfirmReq
+     * @return 확정일 리스트
+     */
+    public List<SearchConfirmRes> getConfirms(SearchConfirmReq dto) {
+        return mapper.selectConfirms(dto);
+    }
+
+    /**
+     * 확정일 단건 생성
+     * @param dto 확정일 생성 요청 객체
+     * @return 업데이트 건수
+     */
+    public int createConfirm(CreateConfirmReq dto) {
+        WcteConfirmValidationDvo validationObject = mapper.selectConfirmValidation(dto.cntrNo(), dto.cntrSn()).orElseThrow(
+            () -> new BizException("해당 계약 번호를 가진 계약은 존재하지 않습니다.")
+        );
+
+        if (!StringUtils.isEmpty(validationObject.getCanDt())) {
+            throw new BizException("취소된 고객입니다!");
+        }
+        if ("Y".equals(validationObject.getCntrCnfmYn())) {
+            throw new BizException("매출확정된 고객입니다!");
+        }
+        if (!StringUtils.isEmpty(validationObject.getIstDt())) {
+            throw new BizException("이미 설치가 되었습니다!");
+        }
+
+        /* TODO: 확인 필요, MSG */
+        if (!"Y".equals(validationObject.getSecPdYn())) {
+            throw new BizException("삼성제품이 아닙니다!");
+        }
+
+        if ("Y".equals(validationObject.getOstrRgstYn())) {
+            throw new BizException("제품 출고 요청이 이미 되었습니다!");
+        }
+
+        /* 계약 상세에 배송예정일자 업데이트 */
+        int processCount = mapper.updateShippingDueDate(dto.cntrNo(), dto.cntrSn(), dto.sppFshDt());
+        BizAssert.isTrue(processCount == 1, "MSG_ALT_SVE_ERR" ) ;
+
+        /* 계약 상세 변경 이력 생성 */
+        processCount = mapper.insertContractDetailChangeHistory(dto.cntrNo(), dto.cntrSn());
+        BizAssert.isTrue(processCount == 1, "MSG_ALT_SVE_ERR" );
+
+        /* 매출 조정 자료 생성: 제품고유번호출고내역 생성 */
+        WctePdOstrIzDvo productOutOfStorageIz = converter.mapCreateConfirmReqToWcteProdu1ctOutOfStorageIz(dto);
+        productOutOfStorageIz.setSellTpCd(validationObject.getSellTpCd());
+        processCount = mapper.insertProductOutOfStorageIz(productOutOfStorageIz);
+        BizAssert.isTrue(processCount == 1, "MSG_ALT_SVE_ERR" ) ;
+
+        /* 삼성API 테이블 기록: 배송업체송장처리내역 생성 */
+        WcteInvoiceProcessIzDvo invoiceProcessIz = converter.mapCreateConfirmReqToWcteInvoiceProcessIz(dto);
+        processCount = mapper.insertInvoiceProcessIz(invoiceProcessIz);
+        BizAssert.isTrue(processCount == 1, "MSG_ALT_SVE_ERR" ) ;
+
+        /*배송업체송장처리이력 생성*/
+        processCount = mapper.insertInvoiceProcessHistory(mapper.selectInvoiceProcessIzPk(invoiceProcessIz));
+        BizAssert.isTrue(processCount == 1, "MSG_ALT_SVE_ERR" );
+
+        return processCount;
+    }
+
+    public int createConfirms(List<CreateConfirmReq> list) {
+        int processCount = 0;
+        for (CreateConfirmReq req : list) {
+            processCount += createConfirm(req);
+        }
+        return processCount;
     }
 }
