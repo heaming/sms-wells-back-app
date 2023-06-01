feacac647be00651f2cecac92ce95d9531d8c3dd
MyoungBin Joo <gs.piit165@kyowon.co.kr>
2023-06-01 20:46:31 +0900

    chore: 주문상세페이지 내부 팝업 현금영수증 기본정보변경 추가

--- src/main/java/com/kyowon/sms/wells/web/contract/ordermgmt/service/WctaOrderDetailCssrService.java
+++ src/main/java/com/kyowon/sms/wells/web/contract/ordermgmt/service/WctaOrderDetailCssrService.java
@@ -3,9 +3,14 @@ package com.kyowon.sms.wells.web.contract.ordermgmt.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.kyowon.sms.wells.web.contract.ordermgmt.converter.WctaOrderDetailCssrConverter;
+import com.kyowon.sms.wells.web.contract.ordermgmt.dto.WctaOrderDetailCssrDto.FindBaseRcpReq;
 import com.kyowon.sms.wells.web.contract.ordermgmt.dto.WctaOrderDetailCssrDto.FindBaseRcpRes;
+import com.kyowon.sms.wells.web.contract.ordermgmt.dto.WctaOrderDetailCssrDto.SaveRcpReq;
 import com.kyowon.sms.wells.web.contract.ordermgmt.dto.WctaOrderDetailCssrDto.SearchRcpRes;
+import com.kyowon.sms.wells.web.contract.ordermgmt.dvo.WctaOrderDetailCssrDvo;
 import com.kyowon.sms.wells.web.contract.ordermgmt.mapper.WctaOrderDetailCssrMapper;
 
 import lombok.RequiredArgsConstructor;
@@ -15,14 +20,32 @@ import lombok.RequiredArgsConstructor;
 public class WctaOrderDetailCssrService {
 
     private final WctaOrderDetailCssrMapper mapper;
+    private final WctaOrderDetailCssrConverter converter;
 
     public List<SearchRcpRes> getCashSalesReceipts(String cntrNo, String cntrSn) {
         return mapper.selectCashSalesReceipts(cntrNo, cntrSn);
     }
 
-    public FindBaseRcpRes getContractBaseInformation(String cntrNo, String cntrSn) {
-        /* TODO: 테이블 모델링 완료 후 수정 예정 */
-        return mapper.selectContractBaseInformation(cntrNo, cntrSn);
+    public FindBaseRcpRes getContractBaseInformation(FindBaseRcpReq dto) {
+        return mapper.selectContractBaseInformation(dto);
+    }
+
+    @Transactional
+    public int saveCashSalesReceipt(SaveRcpReq dto) {
+        WctaOrderDetailCssrDvo dvo = converter.mapSaveRcpReqToWctaOrderDetailCssrDvo(dto);
+
+        int processCount = 0;
+        int checkCount = mapper.selectContractBaseCheck(dvo);
+
+        if (checkCount <= 0) {
+            processCount += mapper.insertCashSalesReceipt(dvo);
+            processCount += mapper.insertCashSalesReceiptApprovalPresentState(dvo);
+        } else {
+            processCount += mapper.insertCashSalesReceiptApprovalPresentState(dvo);
+            processCount += mapper.updateCashSalesReceiptApprovalPresentState(dvo);
+        }
+
+        return processCount;
     }
 
 }
