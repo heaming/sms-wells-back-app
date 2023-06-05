b4bfdff6d21d90e52088dde14c6999eb02ba6bdd
heungjun.lee <gs.piit168@kyowon.com>
2023-06-05 09:10:06 +0900

    단위테스트 누락 소스 커밋
    Signed-off-by: heungjun.lee <gs.piit168@kyowon.com>

---
+++ src/main/java/com/kyowon/sms/wells/web/withdrawal/idvrve/dvo/WwdbMutualAidAllianceBulkDepositDvo.java
@@ -0,0 +1,12 @@
+package com.kyowon.sms.wells.web.withdrawal.idvrve.dvo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class WwdbMutualAidAllianceBulkDepositDvo {
+    private String dpCprcnfAmt;
+    private String rveAkNo;
+    private String itgDpNo;
+}
