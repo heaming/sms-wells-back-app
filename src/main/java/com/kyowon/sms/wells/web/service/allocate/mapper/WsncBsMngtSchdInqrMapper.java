d91144b49d39eb3b809074945c93b9eba060af2b
segi <segi33@kyowon.co.kr>
2023-06-06 09:59:54 +0900

    방문담당 아이콘 팝업창 및 사번 결과 표시 추가

---
+++ src/main/java/com/kyowon/sms/wells/web/service/allocate/mapper/WsncBsMngtSchdInqrMapper.java
@@ -0,0 +1,15 @@
+package com.kyowon.sms.wells.web.service.allocate.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.kyowon.sms.wells.web.service.allocate.dto.WsncBsMngtSchdInqrDto;
+
+@Mapper
+public interface WsncBsMngtSchdInqrMapper {
+    List<WsncBsMngtSchdInqrDto.SearchRes> selectBsMngtSchdInqrAgrg(
+        WsncBsMngtSchdInqrDto.SearchReq dto
+    );
+
+}
