cda2f4311ffd1b3c1d746d4dbb977aea9cae520f
김승수 <gs.piit184@kyowon.com>
2023-06-12 21:39:33 +0900

    fix: 단위테스트 오류 조치

--- src/main/java/com/kyowon/sms/wells/web/service/stock/rest/WsnaBsCsmbDeliveryAggregateController.java
+++ src/main/java/com/kyowon/sms/wells/web/service/stock/rest/WsnaBsCsmbDeliveryAggregateController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,13 +37,11 @@ public class WsnaBsCsmbDeliveryAggregateController {
         return service.getDeliveryAggregatePages(dto, pageInfo);
     }
 
-    @GetMapping("/{mngtYmFrom}-{mngtYmTo}")
+    @GetMapping("/item-quantity")
     public List<SearchQtysRes> getItemQtys(
-        @PathVariable
-        String mngtYmFrom,
-        @PathVariable
-        String mngtYmTo
+        SearchReq dto,
+        PageInfo pageInfo
     ) {
-        return service.getItemQtys(mngtYmFrom, mngtYmTo);
+        return service.getItemQtys(dto, pageInfo);
     }
 }
