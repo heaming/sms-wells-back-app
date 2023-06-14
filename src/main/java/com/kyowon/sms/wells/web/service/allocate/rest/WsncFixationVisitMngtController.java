1ecac6a8588228ce42f98044e22f4dedd06d9505
juno.cha <gs.piit134@kyowon.com>
2023-06-14 17:19:11 +0900

    fix : 고정등록 관리, 고정방문 등록 팝업 로직 수정(Test source 삭제)

--- src/main/java/com/kyowon/sms/wells/web/service/allocate/rest/WsncFixationVisitMngtController.java
+++ src/main/java/com/kyowon/sms/wells/web/service/allocate/rest/WsncFixationVisitMngtController.java
@@ -58,48 +58,7 @@ public class WsncFixationVisitMngtController {
     public WsncFixationVisitDto.SearchRegRes getFixationVisit(
         WsncFixationVisitDto.SearchRegReq dto
     ) {
-        //        return wsncFixationVisitMgntService.getFixationVisit(dto);
-        WsncFixationVisitDto.SearchRegRes tempRes = wsncFixationVisitMgntService.getFixationVisit(dto);
-
-        //TODO : Test를 위한 Garbage Data setting(추후 삭제)
-        log.info("cntrNo ::: " + dto.cntrNo());
-        if ("test".equals(dto.cntrNo())) {
-            log.info("Test Data setting");
-            tempRes = new WsncFixationVisitDto.SearchRegRes(
-                "test1", //String cntrNo,
-                "1", //String cntrSn,
-                "1", //String chSn,
-                "test4", //String cstKnm,
-                "010", //String cralLocaraTno,
-                "3333", //String mexnoEncr,
-                "4444", //String cralIdvTno,
-                "test8", //String rnadr,
-                "test9", //String rdadr,
-                "test10", //String rcgvpKnm,
-                "010", //String cralLocaraTnoInstall,
-                "1111", //String mexnoEncrInstall,
-                "2222", //String cralIdvTnoInstall,
-                "test14", //String rnadrInstall,
-                "test15", //String rdadrInstall,
-                "test16", //String pdNm,
-//                "2", //String pdPrpVal01,(0~9)
-                "용도3", //String pdPrpVal01,(0~9)
-                //Insert Data
-                "202212", //String apyStrtYm,
-                "1", //String chRqrDvCd,
-                "20221205105833", //String fnlMdfcDtm,
-                "1", //String fxnPrtnrDvCd,
-                "1",
-                "test22", //String fxnPrtnrNo,
-                "test33", //String fxnPrtnrKnm,
-                "힘들어요", //String chRsonCn,
-                "N", //String dtaDlYn,
-
-                "test25", //String prtnrKnm,
-                "99991231" //String rsgnDt
-            );
-        }
-        return tempRes;
+        return wsncFixationVisitMgntService.getFixationVisit(dto);
     }
 
     @ApiOperation(value = "고정방문 등록 팝업 - 고정방문 등록 및 수정", notes = "고정방문 등록")
