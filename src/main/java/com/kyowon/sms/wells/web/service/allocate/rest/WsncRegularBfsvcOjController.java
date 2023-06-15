c3ab046c74b48c74b94c6e28ec1b559e0afec0bd
juno.cha <gs.piit134@kyowon.com>
2023-06-15 16:30:39 +0900

    fix : 정기B/S 대상 선정 Service 호출 로직 수정

--- src/main/java/com/kyowon/sms/wells/web/service/allocate/rest/WsncRegularBfsvcOjController.java
+++ src/main/java/com/kyowon/sms/wells/web/service/allocate/rest/WsncRegularBfsvcOjController.java
@@ -1,18 +1,21 @@
 package com.kyowon.sms.wells.web.service.allocate.rest;
 
-import com.kyowon.sms.wells.web.service.allocate.dto.WsncRegularBfsvcOjDto;
-import com.sds.sflex.system.config.constant.CommConst;
-import com.sds.sflex.system.config.response.SaveResponse;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import javax.validation.Valid;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
+import com.kyowon.sms.wells.web.service.allocate.dto.WsncRegularBfsvcOjDto;
+import com.kyowon.sms.wells.web.service.allocate.service.WsncRegularBfsvcOjService;
+import com.sds.sflex.system.config.constant.CommConst;
+import com.sds.sflex.system.config.response.SaveResponse;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Api(tags = "[WSNF] 정기 B/S 대상 선정")
 @RequiredArgsConstructor
@@ -20,6 +23,9 @@ import javax.validation.Valid;
 @RequestMapping(CommConst.REST_URL_V1 + "/sms/wells/service/regular-bs-object")
 @Slf4j
 public class WsncRegularBfsvcOjController {
+
+    private final WsncRegularBfsvcOjService service;
+
     @ApiOperation(value = "정기 B/S 대상 선정 - 생성", notes = "정기 B/S 대상 선정")
     @PostMapping
     public SaveResponse createRegularBfsvcOj(
@@ -29,13 +35,12 @@ public class WsncRegularBfsvcOjController {
     ) throws Exception {
         log.info(
             "[WsncRegularBfsvcOjController.createRegularBfsvcOj] WsncRegularBfsvcOjJob-Create batch Job execute."
-                + dto.allocateYm() + " / " + dto.createTarget()
+                + dto.asnOjYm() + " / " + dto.createTarget()
         );
 
         //TODO Job Execution
-
         return SaveResponse.builder()
-            .processCount(1)
+            .processCount(service.createRegularBfsvcOj(dto))
             .build();
     }
 }
