f74cca9a55315d8510c924cb38a5ff5270b6ca85
gs.piit185 <gs.piit185@kyowon.co.kr>
2023-06-15 15:05:50 +0900

    feat : 수수료지급명세서 수정

--- src/main/java/com/kyowon/sms/wells/web/fee/confirm/mapper/WfeeFeeSpecificationMapper.java
+++ src/main/java/com/kyowon/sms/wells/web/fee/confirm/mapper/WfeeFeeSpecificationMapper.java
@@ -14,25 +14,25 @@ public interface WfeeFeeSpecificationMapper {
     //M추진단 /플래너
     List<HashMap<String, Object>> selectMPlannerFeeSpecifications(
         SearchReq dto, String feeCdStr // 수수료 계산단위 유형코드
-        , String feeCdFields, String feeCdInStr
+        , String feeCdFields, String feeSumField, String feeCdInStr
     );
 
     //P추진단 /플래너
     List<HashMap<String, Object>> selectPPlannerFeeSpecifications(
         SearchReq dto, String feeCdStr // 수수료 계산단위 유형코드
-        , String feeCdFields, String feeCdInStr
+        , String feeCdFields, String feeSumField, String feeCdInStr
     );
 
     //M추진단  / 지점장
     List<HashMap<String, Object>> selectMManagerFeeSpecifications(
         SearchReq dto, String feeCdStr // 수수료 계산단위 유형코드
-        , String feeCdFields, String feeCdInStr
+        , String feeCdFields, String feeSumField, String feeCdInStr
     );
 
     //P추진단 / 지점장
     List<HashMap<String, Object>> selectPManagerFeeSpecifications(
         SearchReq dto, String feeCdStr // 수수료 계산단위 유형코드
-        , String feeCdFields, String feeCdInStr
+        , String feeCdFields, String feeSumField, String feeCdInStr
     );
 
 }
