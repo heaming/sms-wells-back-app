d972c83d1e88c96b2af8892afaaaf04c9fa51f82
송태성 <gs.piit58@kyowon.com>
2023-06-15 16:43:24 +0900

    chore : as소모품입고관리 품목재고내역 추가 및 삭제로직 추가

--- src/main/java/com/kyowon/sms/wells/web/service/stock/converter/WsnaAsConsumablesStoreConverter.java
+++ src/main/java/com/kyowon/sms/wells/web/service/stock/converter/WsnaAsConsumablesStoreConverter.java
@@ -1,7 +1,9 @@
 package com.kyowon.sms.wells.web.service.stock.converter;
 
 import com.kyowon.sms.wells.web.service.stock.dvo.WsnaAsConsumablesStoreDvo;
+import com.kyowon.sms.wells.web.service.stock.dvo.WsnaItemStockItemizationReqDvo;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import static com.kyowon.sms.wells.web.service.stock.dto.WsnaAsConsumablesStoreDto.*;
 
@@ -11,5 +13,7 @@ public interface WsnaAsConsumablesStoreConverter {
     WsnaAsConsumablesStoreDvo mapSaveReqToAsConsumablesStoreDvo(SaveReq dto);
 
     WsnaAsConsumablesStoreDvo mapRemoveReqToAsConsumablesStoreDvo(RemoveReq dto);
+
+    WsnaItemStockItemizationReqDvo mapItemAsConsumablesStoreDvo(WsnaAsConsumablesStoreDvo dvo);
 
 }
