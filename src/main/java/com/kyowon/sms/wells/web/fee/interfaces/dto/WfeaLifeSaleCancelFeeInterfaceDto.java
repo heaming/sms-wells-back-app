bd34bb4c06bb9f80cce05584a06670f286f802a7
Min.Je <gs.piit191@kyowon.com>
2023-06-23 14:27:34 +0900

    feat: 단건 삭제됨

--- src/main/java/com/kyowon/sms/wells/web/fee/interfaces/dto/WfeaLifeSaleCancelFeeInterfaceDto.java
+++ src/main/java/com/kyowon/sms/wells/web/fee/interfaces/dto/WfeaLifeSaleCancelFeeInterfaceDto.java
@@ -82,69 +82,7 @@ public class WfeaLifeSaleCancelFeeInterfaceDto {
     @ApiModel("WfeaLifeSaleCancelFeeInterfaceDto-SaveReq")
     public record SaveReq(
         @JsonProperty("LIST")
-        List<IfRequest> data,
-        @JsonProperty("AKSDTE")
-        String aksdte, /* 발생월 */
-        @JsonProperty("ITM01")
-        String itm01, /* 예상확정구분코드 */
-        @JsonProperty("ITM02")
-        String itm02, /* 상조고객코드 */
-        @JsonProperty("AKDSEQ")
-        String akdseq, /* 회차 */
-        @JsonProperty("AKSGUB")
-        String aksgub, /* 구분 */
-        @JsonProperty("AKSALE")
-        String aksale, /* 판매구분 */
-        @JsonProperty("AKDCDE")
-        String akdcde, /* 사번 */
-        @JsonProperty("AKDBON")
-        String akdbon, /* 지점장 */
-        @JsonProperty("AKCODE")
-        String akcode, /* 상조상품코드 */
-        @JsonProperty("ITM03")
-        String itm03, /* 상조상품명 */
-        @JsonProperty("AKCRTE")
-        String akcrte, /* 접수일 */
-        @JsonProperty("AKSLTE")
-        String akslte, /* 계약일 */
-        @JsonProperty("AKCDTE")
-        String akcdte, /* 취소일 */
-        @JsonProperty("AKTCNT")
-        String aktcnt, /* 총지급대상 */
-        @JsonProperty("AKMAMT")
-        String akmamt, /* 누적발생매출 */
-        @JsonProperty("AKIAMT")
-        String akiamt, /* 누적입금 */
-        @JsonProperty("AKISEQ")
-        String akiseq, /* 완납회차 */
-        @JsonProperty("ITM04")
-        String itm04, /* WELLS 고객코드 */
-        @JsonProperty("ITM05")
-        String itm05, /* WELLS 계약일련번호 */
-        @JsonProperty("AKGDYM")
-        String akgdym, /* 수수료지급월 */
-        @JsonProperty("AKHDYM")
-        String akhdym, /* 수수료되물림 */
-        @JsonProperty("AKLOCK")
-        String aklock, /* 마감여부 */
-        @JsonProperty("ITM06")
-        String itm06, /* 데이터삭제여부 */
-        @JsonProperty("AKWDAY")
-        String akwday, /* 생성일자 */
-        @JsonProperty("AKWDSP")
-        String akwdsp, /* 생성자 */
-        @JsonProperty("AKWPGM")
-        String akwpgm, /* 생성PGM */
-        @JsonProperty("ITM07")
-        String itm07, /* 최초등록부서ID */
-        @JsonProperty("AKUDAY")
-        String akuday, /* 수정일자 */
-        @JsonProperty("AKUDSP")
-        String akudsp, /* 수정자 */
-        @JsonProperty("AKUPGM")
-        String akupgm, /* 생성PGM */
-        @JsonProperty("ITM08")
-        String itm08 /* 최종수정부서ID */
+        List<IfRequest> data
     ) {}
 
     @Builder
--- src/main/java/com/kyowon/sms/wells/web/fee/interfaces/service/WfeaLifeSaleCancelFeeInterfaceService.java
+++ src/main/java/com/kyowon/sms/wells/web/fee/interfaces/service/WfeaLifeSaleCancelFeeInterfaceService.java
@@ -21,53 +21,6 @@ public class WfeaLifeSaleCancelFeeInterfaceService {
 
     @Transactional
     public String[] updateLifeFeeSync(WfeaLifeSaleCancelFeeInterfaceDto.SaveReq dto) throws Exception {
-        // 단건형 키값 null 아니면 ㄱㄱ
-        if (StringUtils.isNotEmpty(dto.aksdte()) && StringUtils.isNotEmpty(dto.itm01()) && StringUtils.isNotEmpty(dto.itm02()) && StringUtils.isNotEmpty(dto.akdseq())) {
-            WfeaLifeSaleCancelFeenterfaceDvo saveDvo = new WfeaLifeSaleCancelFeenterfaceDvo();
-            saveDvo.setBaseYm(dto.aksdte());
-            saveDvo.setEtCnfmDvCd(dto.itm01());
-            saveDvo.setLifCntrNo(dto.itm02());
-            saveDvo.setLifCntrOcTn(dto.akdseq());
-            saveDvo.setLifCntrStatCd(dto.aksgub());
-            saveDvo.setSellDvCd(dto.aksale());
-            saveDvo.setPrtnrNo(dto.akdcde());
-            saveDvo.setBrmgrPrtnrNo(dto.akdbon());
-            saveDvo.setLifPdCd(dto.akcode());
-            saveDvo.setLifPdNm(dto.itm03());
-            saveDvo.setRcpdt(dto.akcrte());
-            saveDvo.setCntrDt(dto.akslte());
-            saveDvo.setCanDt(dto.akcdte());
-            saveDvo.setTotDsbOjDvCd(dto.aktcnt());
-            saveDvo.setSlOcAcuAmt(dto.akmamt());
-            saveDvo.setDpAcuAmt(dto.akiamt());
-            saveDvo.setFlpymTn(dto.akiseq());
-            saveDvo.setWelsCntrNo(dto.itm04());
-            saveDvo.setWelsCntrSn(dto.itm05());
-            saveDvo.setFeeDsbYm(dto.akgdym());
-            saveDvo.setFeeRedfYm(dto.akhdym());
-            saveDvo.setCnfmYn(dto.aklock());
-            saveDvo.setDtaDlYn(dto.itm06());
-            saveDvo.setFstRgstDtm(dto.akwday());
-            saveDvo.setFstRgstUsrId(dto.akwdsp());
-            saveDvo.setFstRgstPrgId(dto.akwpgm());
-            saveDvo.setFstRgstDeptId(dto.itm07());
-            saveDvo.setFnlMdfcDtm(dto.akuday());
-            saveDvo.setFnlMdfcUsrId(dto.akudsp());
-            saveDvo.setFnlMdfcPrgId(dto.akupgm());
-            saveDvo.setFnlMdfcDeptId(dto.itm08());
-
-            String cnfmYn = mapper.selectLifeFeeValidKey(saveDvo);
-            // 1.1 마감여부? 체크 후 마감이면 에러 CNFM_YN = 'Y'면 에러
-            if ("Y".equals(cnfmYn)) {
-                throw new BizException("MSG_ALT_CNFM_NO_RENEW_DATA"); // 확정된 DATA는 갱인이 불가능합니다.
-            } else {
-                // 1.2 그외 처리한다.
-                int resultCnt = mapper.updateLifeFeeSync(saveDvo);
-                if (resultCnt <= 0) {
-                    throw new BizException("MSG_ALT_SVE_ERR"); // 응 서버에러
-                }
-            }
-        }
         // 배치형
         if (dto.data().size() > 0) {
             for (IfRequest item : dto.data()) {
