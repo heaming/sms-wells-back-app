182add31fcb5950daf5c99a748541fb7729abcc7
Min.Je <gs.piit191@kyowon.com>
2023-07-03 15:41:25 +0900

    feat: 다시원복함 ㅠ

--- src/main/java/com/kyowon/sms/wells/web/fee/interfaces/service/WfeaLifeSaleCancelFeeInterfaceService.java
+++ src/main/java/com/kyowon/sms/wells/web/fee/interfaces/service/WfeaLifeSaleCancelFeeInterfaceService.java
@@ -22,57 +22,52 @@ public class WfeaLifeSaleCancelFeeInterfaceService {
     @Transactional
     public String[] updateLifeFeeSync(WfeaLifeSaleCancelFeeInterfaceDto.SaveReq dto) throws Exception {
         // 배치형
-        if (dto.list().size() > 0) {
-            for (IfRequest item : dto.list()) {
-                // DTO > DVO
-                WfeaLifeSaleCancelFeenterfaceDvo saveDvo = new WfeaLifeSaleCancelFeenterfaceDvo();
-                saveDvo.setBaseYm(item.aksdte());
-                saveDvo.setEtCnfmDvCd(item.itm01());
-                saveDvo.setLifCntrNo(item.itm02());
-                saveDvo.setLifCntrOcTn(item.akdseq());
-                saveDvo.setLifCntrStatCd(item.aksgub());
-                saveDvo.setSellDvCd(item.aksale());
-                saveDvo.setPrtnrNo(item.akdcde());
-                saveDvo.setBrmgrPrtnrNo(item.akdbon());
-                saveDvo.setLifPdCd(item.akcode());
-                saveDvo.setLifPdNm(item.itm03());
-                saveDvo.setRcpdt(item.akcrte());
-                saveDvo.setCntrDt(item.akslte());
-                saveDvo.setCanDt(item.akcdte());
-                saveDvo.setTotDsbOjDvCd(item.aktcnt());
-                saveDvo.setSlOcAcuAmt(item.akmamt());
-                saveDvo.setDpAcuAmt(item.akiamt());
-                saveDvo.setFlpymTn(item.akiseq());
-                saveDvo.setCntrNo(item.itm04());
-                saveDvo.setCntrSn(Integer.parseInt(item.itm05()));
-                saveDvo.setFeeDsbYm(item.akgdym());
-                saveDvo.setFeeRedfYm(item.akhdym());
-                saveDvo.setCnfmYn(item.aklock());
-                saveDvo.setDtaDlYn(item.itm06());
-                saveDvo.setFstRgstDtm(item.akwday());
-                saveDvo.setFstRgstUsrId(item.akwdsp());
-                saveDvo.setFstRgstPrgId(item.akwpgm());
-                saveDvo.setFstRgstDeptId(item.itm07());
-                saveDvo.setFnlMdfcDeptId(item.itm08());
-                saveDvo.setFnlMdfcDtm(item.akuday());
-                saveDvo.setFnlMdfcUsrId(item.akudsp());
-                saveDvo.setFnlMdfcDtm(null);
-                saveDvo.setFnlMdfcUsrId(null);
-                saveDvo.setFnlMdfcPrgId(null);
-                saveDvo.setFnlMdfcPrgId(item.akupgm());
-                String cnfmYn = mapper.selectLifeFeeValidKey(saveDvo);
-                // 1.1 마감여부? 체크 후 마감이면 에러 CNFM_YN = 'Y'면 에러
-                if ("Y".equals(cnfmYn)) {
-                    throw new BizException("MSG_ALT_CNFM_NO_RENEW_DATA"); // 확정된 DATA는 갱인이 불가능합니다.
-                } else {
-                    // 1.2 그외 처리한다.
-                    int resultCnt = mapper.updateLifeFeeSync(saveDvo);
-                    if (resultCnt <= 0) {
-                        throw new BizException("MSG_ALT_SVE_ERR"); // 응 서버에러
-                    }
+        for (IfRequest item : dto.list()) {
+            // DTO > DVO
+            WfeaLifeSaleCancelFeenterfaceDvo saveDvo = new WfeaLifeSaleCancelFeenterfaceDvo();
+            saveDvo.setBaseYm(item.aksdte());
+            saveDvo.setEtCnfmDvCd(item.itm01());
+            saveDvo.setLifCntrNo(item.itm02());
+            saveDvo.setLifCntrOcTn(item.akdseq());
+            saveDvo.setLifCntrStatCd(item.aksgub());
+            saveDvo.setSellDvCd(item.aksale());
+            saveDvo.setPrtnrNo(item.akdcde());
+            saveDvo.setBrmgrPrtnrNo(item.akdbon());
+            saveDvo.setLifPdCd(item.akcode());
+            saveDvo.setLifPdNm(item.itm03());
+            saveDvo.setRcpdt(item.akcrte());
+            saveDvo.setCntrDt(item.akslte());
+            saveDvo.setCanDt(item.akcdte());
+            saveDvo.setTotDsbOjDvCd(item.aktcnt());
+            saveDvo.setSlOcAcuAmt(item.akmamt());
+            saveDvo.setDpAcuAmt(item.akiamt());
+            saveDvo.setFlpymTn(item.akiseq());
+            saveDvo.setCntrNo(item.itm04());
+            saveDvo.setCntrSn(item.itm05());
+            saveDvo.setFeeDsbYm(item.akgdym());
+            saveDvo.setFeeRedfYm(item.akhdym());
+            saveDvo.setCnfmYn(item.aklock());
+            saveDvo.setDtaDlYn(item.itm06());
+            saveDvo.setFstRgstDtm(item.akwday());
+            saveDvo.setFstRgstUsrId(item.akwdsp());
+            saveDvo.setFstRgstPrgId(item.akwpgm());
+            saveDvo.setFstRgstDeptId(item.itm07());
+            saveDvo.setFnlMdfcDtm(item.akuday());
+            saveDvo.setFnlMdfcUsrId(item.akudsp());
+            saveDvo.setFnlMdfcPrgId(item.akupgm());
+            saveDvo.setFnlMdfcDeptId(item.itm08());
+            String cnfmYn = mapper.selectLifeFeeValidKey(saveDvo);
+            // 1.1 마감여부? 체크 후 마감이면 에러 CNFM_YN = 'Y'면 에러
+            if ("Y".equals(cnfmYn)) {
+                throw new BizException("MSG_ALT_CNFM_NO_RENEW_DATA"); // 확정된 DATA는 갱인이 불가능합니다.
+            } else {
+                // 1.2 그외 처리한다.
+                int resultCnt = mapper.updateLifeFeeSync(saveDvo);
+                if (resultCnt <= 0) {
+                    throw new BizException("MSG_ALT_SVE_ERR"); // 응 서버에러
                 }
             }
-         }
+        }
         return new String[]{"S", "데이터 등록이 성공하였습니다."};
     }
 }
