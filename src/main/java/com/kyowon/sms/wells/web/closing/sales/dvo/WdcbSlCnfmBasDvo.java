4ae0d6d2d881d9340c4197161f4119456910c3d8
우승민 <gs.piit183@kyowon.co.kr>
2023-07-03 14:10:47 +0900

    feat: 매출확정서비스 DVO 수정

---
+++ src/main/java/com/kyowon/sms/wells/web/closing/sales/dvo/WdcbSlCnfmBasDvo.java
@@ -0,0 +1,128 @@
+package com.kyowon.sms.wells.web.closing.sales.dvo;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * <pre>
+ * 매출확정생성 서비스 WELLS매출확정기본 Table DVO
+ * </pre>
+ *
+ * @author WOO SEUNG MIN
+ * @since 2023-06-22
+ */
+@Getter
+@Setter
+@ToString
+public class WdcbSlCnfmBasDvo {
+    private String cntrNo;
+    private int cntrSn;
+    private String slRcogDt;
+    private int slCnfmSn;
+    private String kwGrpCoCd;
+    private String bzHdqDvCd;
+    private String ogTpCd;
+    private String prtnrNo;
+    private String pdHclsfId;
+    private String pdMclsfId;
+    private String pdLclsfId;
+    private String pdCd;
+    private String dgCstId;
+    private String cstNo;
+    private String copnDvCd;
+    private String bzrno;
+    private String sellTpCd;
+    private String sellTpDtlCd;
+    private String sellInflwChnlDtlCd;
+    private String sapPdDvCd;
+    private int sellQty;
+    private int sellAmt;
+    private int sellAmtVat;
+    private int sellSplAmt;
+    private int cntrTam;
+    private int subscAmt;
+    private int rentalRgstCost;
+    private int rentalRgstCostVat;
+    private int rentalAmt;
+    private int rentalDscAmt;
+    private int rentalPtrm;
+    private int rentalTn;
+    private int istmPcamAmt;
+    private int istmFeeLvyAmt;
+    private int istmAmt;
+    private int istmMcn;
+    private int mmIstmAmt;
+    private int dscAmt;
+    private int cntramDpAmt;
+    private int bilDscAmt;
+    private int ovrCtrDpAmt;
+    private int prmTn;
+    private int totPrmAmt;
+    private int prmExpAmt;
+    private String prmStrtY;
+    private String prmStrtMm;
+    private String prmEndY;
+    private String prmEndMm;
+    private int prmMcn;
+    private int prmDscr;
+    private int prmDscAmt;
+    private int prmDpAmt;
+    private int prmRfndAmt;
+    private int prmRplcAmt;
+    private int prmSlAmt;
+    private int nomSlAmt;
+    private int spmtSlAmt;
+    private int nomDscAmt;
+    private int spmtDscAmt;
+    private int slCtrAmt;
+    private int slCanAmt;
+    private String slStpYn;
+    private String cntrStlmFshDtm;
+    private String cntrStrtdt;
+    private String canDt;
+    private int slDc;
+    private int svAmt;
+    private int nomIntAmt;
+    private int intVat;
+    private int mlgSlAmt;
+    private String ostrDtm;
+    private String sppDtm;
+    private String istDtm;
+    private String reqdDtm;
+    private String svDt;
+    private int pvdaOjPcam;
+    private int pvdaAmt;
+    private String lgstSppMthdCd;
+    private String iostWareCd;
+    private String sapMatEvlClssVal;
+    private String sapSlTpCd;
+    private String sapBizDvCd;
+    private String sapBzHdqInfCd;
+    private int slAmt;
+    private int vat;
+    private String iostDt;
+    private String ctrlOrdTpCd;
+    private String sapMatCd;
+    private int slQty;
+    private String rtngdYn;
+    private String frisuYn;
+    private String cscnCd;
+    private String wbsCd;
+    private String sapPurpMatCd;
+    private String fgptYn;
+    private String sapTxnDtfrCd;
+    private String sapTxinvPblBaseCd;
+    private String rvpyYn;
+    private String saveGdsYn;
+    private String sapPlntCd;
+    private String sapSaveLctVal;
+    private String slpAkNo;
+    private String sapTrsDt;
+    private String sapRfdt;
+    private String sapSlpno;
+    private String sapSlipMsg;
+    private String slipSlDt;
+    private String slipPblPrdCd;
+    private String dtaDlYn;
+}
