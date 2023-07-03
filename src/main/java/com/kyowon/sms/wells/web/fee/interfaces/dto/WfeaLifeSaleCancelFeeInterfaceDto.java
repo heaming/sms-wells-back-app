66de92862cc29320a5fe8f8d8bf92716eefbe091
Min.Je <gs.piit191@kyowon.com>
2023-07-03 11:30:13 +0900

    feat: 클라이언트문제 기존코드 원복

--- src/main/java/com/kyowon/sms/wells/web/fee/interfaces/dto/WfeaLifeSaleCancelFeeInterfaceDto.java
+++ src/main/java/com/kyowon/sms/wells/web/fee/interfaces/dto/WfeaLifeSaleCancelFeeInterfaceDto.java
@@ -1,9 +1,6 @@
 package com.kyowon.sms.wells.web.fee.interfaces.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonSetter;
-import com.fasterxml.jackson.annotation.Nulls;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import io.swagger.annotations.ApiModel;
 import lombok.Builder;
 
@@ -28,114 +25,62 @@ public class WfeaLifeSaleCancelFeeInterfaceDto {
         @JsonProperty("AKDSEQ")
         Integer akdseq, /* 회차 */
         @JsonProperty("AKSGUB")
-        @JsonSetter(nulls = Nulls.SKIP)
-        @JsonDeserialize(using = EmptyStringToNullDeserializer.class)
         String aksgub, /* 구분 */
         @JsonProperty("AKSALE")
-        @JsonSetter(nulls = Nulls.SKIP)
-        @JsonDeserialize(using = EmptyStringToNullDeserializer.class)
         String aksale, /* 판매구분 */
         @JsonProperty("AKDCDE")
-        @JsonSetter(nulls = Nulls.SKIP)
-        @JsonDeserialize(using = EmptyStringToNullDeserializer.class)
         String akdcde, /* 사번 */
         @JsonProperty("AKDBON")
-        @JsonSetter(nulls = Nulls.SKIP)
-        @JsonDeserialize(using = EmptyStringToNullDeserializer.class)
         String akdbon, /* 지점장 */
         @JsonProperty("AKCODE")
-        @JsonSetter(nulls = Nulls.SKIP)
-        @JsonDeserialize(using = EmptyStringToNullDeserializer.class)
         String akcode, /* 상조상품코드 */
         @JsonProperty("ITM03")
-        @JsonSetter(nulls = Nulls.SKIP)
-        @JsonDeserialize(using = EmptyStringToNullDeserializer.class)
         String itm03, /* 상조상품명 */
         @JsonProperty("AKCRTE")
-        @JsonSetter(nulls = Nulls.SKIP)
-        @JsonDeserialize(using = EmptyStringToNullDeserializer.class)
         String akcrte, /* 접수일 */
         @JsonProperty("AKSLTE")
-        @JsonSetter(nulls = Nulls.SKIP)
-        @JsonDeserialize(using = EmptyStringToNullDeserializer.class)
         String akslte, /* 계약일 */
         @JsonProperty("AKCDTE")
-        @JsonSetter(nulls = Nulls.SKIP)
-        @JsonDeserialize(using = EmptyStringToNullDeserializer.class)
         String akcdte, /* 취소일 */
         @JsonProperty("AKTCNT")
-        @JsonSetter(nulls = Nulls.SKIP)
-        @JsonDeserialize(using = EmptyStringToNullDeserializer.class)
         String aktcnt, /* 총지급대상 */
         @JsonProperty("AKMAMT")
-        @JsonSetter(nulls = Nulls.SKIP)
-        @JsonDeserialize(using = EmptyStringToNullDeserializer.class)
         Long  akmamt, /* 누적발생매출 */
         @JsonProperty("AKIAMT")
-        @JsonSetter(nulls = Nulls.SKIP)
-        @JsonDeserialize(using = EmptyStringToNullDeserializer.class)
         Long  akiamt, /* 누적입금 */
         @JsonProperty("AKISEQ")
-        @JsonSetter(nulls = Nulls.SKIP)
-        @JsonDeserialize(using = EmptyStringToNullDeserializer.class)
-        Integer  akiseq, /* 완납회차 */
+        Integer akiseq, /* 완납회차 */
         @JsonProperty("ITM04")
-        @JsonSetter(nulls = Nulls.SKIP)
-        @JsonDeserialize(using = EmptyStringToNullDeserializer.class)
         String itm04, /* WELLS 고객코드 */
+        @JsonProperty("ITM05")
+        String itm05, /* WELLS 계약일련번호 */
         @JsonProperty("AKGDYM")
-        @JsonSetter(nulls = Nulls.SKIP)
-        @JsonDeserialize(using = EmptyStringToNullDeserializer.class)
         String akgdym, /* 수수료지급월 */
         @JsonProperty("AKHDYM")
-        @JsonSetter(nulls = Nulls.SKIP)
-        @JsonDeserialize(using = EmptyStringToNullDeserializer.class)
         String akhdym, /* 수수료되물림 */
         @JsonProperty("AKLOCK")
-        @JsonSetter(nulls = Nulls.SKIP)
-        @JsonDeserialize(using = EmptyStringToNullDeserializer.class)
         String aklock, /* 마감여부 */
         @JsonProperty("ITM06")
-        @JsonSetter(nulls = Nulls.SKIP)
-        @JsonDeserialize(using = EmptyStringToNullDeserializer.class)
         String itm06, /* 데이터삭제여부 */
         @JsonProperty("AKWDAY")
-        @JsonSetter(nulls = Nulls.SKIP)
-        @JsonDeserialize(using = EmptyStringToNullDeserializer.class)
         String akwday, /* 생성일자 */
         @JsonProperty("AKWDSP")
-        @JsonSetter(nulls = Nulls.SKIP)
-        @JsonDeserialize(using = EmptyStringToNullDeserializer.class)
         String akwdsp, /* 생성자 */
         @JsonProperty("AKWPGM")
-        @JsonSetter(nulls = Nulls.SKIP)
-        @JsonDeserialize(using = EmptyStringToNullDeserializer.class)
         String akwpgm, /* 생성PGM */
         @JsonProperty("ITM07")
-        @JsonSetter(nulls = Nulls.SKIP)
-        @JsonDeserialize(using = EmptyStringToNullDeserializer.class)
         String itm07, /* 최초등록부서ID */
+        @JsonProperty("AKUDAY")
+        String akuday, /* 수정일자 */
+        @JsonProperty("AKUDSP")
+        String akudsp, /* 수정자 */
+        @JsonProperty("AKUPGM")
+        String akupgm, /* 생성PGM */
         @JsonProperty("ITM08")
-        @JsonSetter(nulls = Nulls.SKIP)
-        @JsonDeserialize(using = EmptyStringToNullDeserializer.class)
         String itm08 /* 최종수정부서ID */
 
-//        @JsonProperty("AKUDAY")
-//        @JsonSetter(nulls = Nulls.SKIP)
-//        @JsonDeserialize(using = EmptyStringToNullDeserializer.class)
-//        String akuday, /* 수정일자 */
-//        @JsonProperty("AKUDSP")
-//        @JsonSetter(nulls = Nulls.SKIP)
-//        @JsonDeserialize(using = EmptyStringToNullDeserializer.class)
-//        String akudsp, /* 수정자 */
-//        @JsonProperty("AKUPGM")
-//        @JsonSetter(nulls = Nulls.SKIP)
-//        @JsonDeserialize(using = EmptyStringToNullDeserializer.class)
-//        String akupgm, /* 생성PGM */
-//        @JsonProperty("ITM05")
-//        @JsonSetter(nulls = Nulls.SKIP)
-//        @JsonDeserialize(using = EmptyStringToNullDeserializer.class)
-//        String itm05, /* WELLS 계약일련번호 */
+
+
     ) {}
     @Builder
     @ApiModel("WfeaLifeSaleCancelFeeInterfaceDto-SaveReq")
--- src/main/java/com/kyowon/sms/wells/web/fee/interfaces/service/WfeaLifeSaleCancelFeeInterfaceService.java
+++ src/main/java/com/kyowon/sms/wells/web/fee/interfaces/service/WfeaLifeSaleCancelFeeInterfaceService.java
@@ -44,6 +44,7 @@ public class WfeaLifeSaleCancelFeeInterfaceService {
                 saveDvo.setDpAcuAmt(item.akiamt());
                 saveDvo.setFlpymTn(item.akiseq());
                 saveDvo.setCntrNo(item.itm04());
+                saveDvo.setCntrSn(Integer.parseInt(item.itm05()));
                 saveDvo.setFeeDsbYm(item.akgdym());
                 saveDvo.setFeeRedfYm(item.akhdym());
                 saveDvo.setCnfmYn(item.aklock());
@@ -53,15 +54,12 @@ public class WfeaLifeSaleCancelFeeInterfaceService {
                 saveDvo.setFstRgstPrgId(item.akwpgm());
                 saveDvo.setFstRgstDeptId(item.itm07());
                 saveDvo.setFnlMdfcDeptId(item.itm08());
-//                saveDvo.setFnlMdfcDtm(item.akuday());
-//                saveDvo.setFnlMdfcUsrId(item.akudsp());
-//                saveDvo.setFnlMdfcPrgId(item.akupgm());
-//                saveDvo.setCntrSn(item.itm05());
-                saveDvo.setCntrSn(null);
+                saveDvo.setFnlMdfcDtm(item.akuday());
+                saveDvo.setFnlMdfcUsrId(item.akudsp());
                 saveDvo.setFnlMdfcDtm(null);
                 saveDvo.setFnlMdfcUsrId(null);
                 saveDvo.setFnlMdfcPrgId(null);
-
+                saveDvo.setFnlMdfcPrgId(item.akupgm());
                 String cnfmYn = mapper.selectLifeFeeValidKey(saveDvo);
                 // 1.1 마감여부? 체크 후 마감이면 에러 CNFM_YN = 'Y'면 에러
                 if ("Y".equals(cnfmYn)) {
