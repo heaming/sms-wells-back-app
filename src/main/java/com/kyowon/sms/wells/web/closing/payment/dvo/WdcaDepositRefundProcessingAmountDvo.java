bbd02e164aa97f5e3e1fb1a4775a709cf29cb06d
우승민 <gs.piit183@kyowon.co.kr>
2023-07-10 17:17:38 +0900

    fix:단위테스트 결함 수정

---
+++ src/main/java/com/kyowon/sms/wells/web/closing/payment/dvo/WdcaDepositRefundProcessingAmountDvo.java
@@ -0,0 +1,28 @@
+package com.kyowon.sms.wells.web.closing.payment.dvo;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * <pre>
+ * 연체 입금/환불 반영 서비스 DVO
+ * </pre>
+ *
+ * @author WOO SEUNGMIN
+ * @since 2023-05-24
+ */
+@Getter
+@Setter
+@ToString
+public class WdcaDepositRefundProcessingAmountDvo {
+    private String cntrNo;
+    private String cntrSn;
+    private int thmDlqDpSumAmt; /*당월연체입금합계금액*/
+    private int thmDlqAddDpSumAmt; /*당월연체가산입금합계금액*/
+    private int thmDlqRfndSumAmt; /*당월연체환불합계금액*/
+    private int thmDlqAddRfndSumAmt; /*당월연체가산환불합계금액*/
+    private int eotDlqAmt; /*기말연체금액*/
+    private int eotDlqAddAmt; /*기말연체가산금액*/
+    private String sellTpCd;
+}
