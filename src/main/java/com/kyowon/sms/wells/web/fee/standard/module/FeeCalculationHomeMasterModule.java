a32ed89e36a31306a594fc44ef7503a163674fb8
JungJingun <gs.piit36@kyowon.com>
2023-07-10 11:30:57 +0900

    fix : 수수료기준정보 수정

--- src/main/java/com/kyowon/sms/wells/web/fee/standard/module/FeeCalculationHomeMasterModule.java
+++ src/main/java/com/kyowon/sms/wells/web/fee/standard/module/FeeCalculationHomeMasterModule.java
@@ -7,6 +7,7 @@ import com.kyowon.sms.common.web.fee.standard.context.ApplicationContextHolder;
 import com.kyowon.sms.common.web.fee.standard.module.FeeCalculationCommonModule;
 import com.kyowon.sms.wells.web.fee.standard.mapper.WfeyHomeMasterCalculationMapper;
 
+import static com.kyowon.sms.common.web.fee.standard.constant.FeFeeConst.FeeCalculationTypeCode.FEE_CALCULATION;
 import static com.kyowon.sms.common.web.fee.standard.constant.FeFeeConst.SYSTEM_PACKAGE_WELLS;
 
 @FeeModuleInfo(systemType = SYSTEM_PACKAGE_WELLS, moduleName = "홈마스터모듈", moduleExplanation = "홈마스터 특화수수료계산 모듈")
@@ -74,5 +75,33 @@ public class FeeCalculationHomeMasterModule extends FeeCalculationCommonModule {
     @FeeModuleMethodInfo(methodName = "서비스현장수수료(W030007) 후처리", methodExplanation = "서비스실적내역 테이블에 데이터 업데이트")
     public void runSvSiteFeePostProcess() {
         homeMasterCalculationMapper.updateSvSiteFeePostProcess(baseYm, feeStandard.basic().ogTpCd(), feeCd, feeTcntDvCd);
+    }
+
+    /**
+     * 교육수수료(W030011)계산 1단계
+     *
+     * 2회차 지급월에 퇴직여부에 따라 미지급(삭제)으로 상태 변경
+     *
+     */
+    @FeeModuleMethodInfo(methodName = "교육수수료(W030011)계산 1단계", methodExplanation = "2회차 지급월에 퇴직여부에 따라 미지급(삭제)으로 상태 변경")
+    public void runEducationFeeStep1() {
+        homeMasterCalculationMapper.updateResignPartnerEducationFees(baseYm, feeStandard.basic().ogTpCd(), feeTcntDvCd);
+        homeMasterCalculationMapper.insertResignPartnerEducationFeeHistories(baseYm, feeStandard.basic().ogTpCd(), feeTcntDvCd);
+        homeMasterCalculationMapper.updateResignPartnerEducationFeePlan(baseYm, feeStandard.basic().ogTpCd(), feeCd, feeTcntDvCd);
+        homeMasterCalculationMapper.insertResignPartnerEducationFeePlanHistories(baseYm, feeStandard.basic().ogTpCd(), feeCd, feeTcntDvCd);
+    }
+
+    /**
+     * 교육수수료(W030011)계산 2단계
+     *
+     * 기준년월 수수료 데이터 생성 (수수료기본 + 수수료분할지급계획)
+     *
+     */
+    @FeeModuleMethodInfo(methodName = "교육수수료(W030011)계산 2단계", methodExplanation = "기준년월 수수료 데이터 생성 (수수료기본 + 수수료분할지급계획)")
+    public void runEducationFeeStep2() {
+        homeMasterCalculationMapper.insertEducationFees(baseYm, feeStandard.basic().ogTpCd(), feeTcntDvCd, feeCd, feeStandard.basic().dtaCrtFeeCd(), feeStandard.basic().coCd(), feeStandard.basic().fnlFeeYn());
+        feeCalculationMapper.insertCalcBasHistories(baseYm, feeCd, feeTcntDvCd, FEE_CALCULATION.getCode(), null, null);
+        homeMasterCalculationMapper.insertEducationFeePlan(baseYm, feeStandard.basic().ogTpCd(), feeTcntDvCd, feeCd, feeStandard.basic().dtaCrtFeeCd(), feeStandard.basic().coCd(), feeStandard.basic().fnlFeeYn());
+        feeCalculationMapper.insertPatDsbPlanHitories(baseYm, feeCd, feeTcntDvCd, FEE_CALCULATION.getCode(), null, null);
     }
 }
