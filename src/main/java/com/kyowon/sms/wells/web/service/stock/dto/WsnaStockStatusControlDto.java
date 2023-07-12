693352aeef747e3dae0ff50b4dce566658c2cfe8
송태성 <gs.piit58@kyowon.com>
2023-07-12 16:35:00 +0900

    feat : 재고상태조정 관리 신규개발

---
+++ src/main/java/com/kyowon/sms/wells/web/service/stock/dto/WsnaStockStatusControlDto.java
@@ -0,0 +1,77 @@
+package com.kyowon.sms.wells.web.service.stock.dto;
+
+import javax.validation.constraints.NotBlank;
+
+import com.sds.sflex.system.config.validation.validator.ValidDate;
+
+import io.swagger.annotations.ApiModel;
+import lombok.Builder;
+
+public class WsnaStockStatusControlDto {
+    // *********************************************************
+    // Request Dto
+    // *********************************************************
+    // 재고상태조정 관리 Search Request Dto
+    @Builder
+    @ApiModel("WsnaStockStatusControlDto-SearchReq")
+    public record SearchReq(
+        String stFromYmd,
+        String edToYmd,
+        String wareNo,
+        String itmKnd,
+        String itmGdCtrTpCd,
+        String itmGdCtrRsonCd
+    ) {}
+
+    // *********************************************************
+    // Result Dto
+    // *********************************************************
+    // 재고상태조정 관리 Search Result Dto
+    @ApiModel("WsnaStockStatusControlDto-SearchRes")
+    public record SearchRes(
+        String wareNo,
+        String wareNm,
+        String wareMngtPrtnrNo,
+        String wareDvCd,
+        String ogNm,
+        String itemKnd,
+        String ctrWkDt,
+        String statCtrApyDt,
+        String itmPdCd,
+        String itmGdCtrTpNm,
+        String itmPdNm,
+        String mgtUnit,
+        int bfctNomStocAGdQty,
+        int bfctNomStocEGdQty,
+        int bfctNomStocRGdQty,
+        String bfctItmGdCd,
+        String afctItmGdCd,
+        int ctrQty,
+        String itmGdCtrRsonNm,
+        String ctrSn
+    ) {}
+
+    @ApiModel("WsnaStockStatusControlDto-SearchWarehouseReq")
+    public record SearchWarehouseReq(
+        String wareDvCd
+    ) {}
+
+    @ApiModel("WsnaStockStatusControlDto-SearchWarehouseRes")
+    public record SearchWarehouseRes(
+        String codeId,
+        String codeName,
+        String wareDvCd
+    ) {}
+
+    @ApiModel("WsnaStockStatusControlDto-FindOgNmRes")
+    public record FindOgNmRes(
+        String ogCd,
+        String ogNm
+    ) {}
+
+    @ApiModel("WsnaStockStatusControlDto-FinditmPdCdRes")
+    public record FinditmPdCdRes(
+        String codeId,
+        String codeName
+    ) {}
+}
---
+++ src/main/java/com/kyowon/sms/wells/web/service/stock/rest/WsnaStockStatusControlController.java
@@ -0,0 +1,79 @@
+package com.kyowon.sms.wells.web.service.stock.rest;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import com.kyowon.sms.wells.web.service.stock.service.WsnaStockStatusControlService;
+import com.kyowon.sms.wells.web.service.zcommon.constants.SnServiceConst;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import static com.kyowon.sms.wells.web.service.stock.dto.WsnaStockStatusControlDto.*;
+
+import com.sds.sflex.system.config.datasource.PageInfo;
+import com.sds.sflex.system.config.datasource.PagingResult;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+
+@Api(tags = "[WSNA] 재고상태조정 관리")
+@Validated
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(SnServiceConst.REST_URL_V1 + "/stock-status-control")
+public class WsnaStockStatusControlController {
+
+    private final WsnaStockStatusControlService service;
+
+    @ApiOperation(value = "재고상태조정 관리 페이징 조회", notes = "재고상태조정 관리 페이지를 조회한다.")
+    @GetMapping("/paging")
+    public PagingResult<SearchRes> getStockStatusControlPages(
+        SearchReq dto,
+        @Valid
+        PageInfo pageInfo
+    ) {
+        return service.getStockStatusControlPages(dto, pageInfo);
+    }
+
+    @ApiOperation(value = "재고상태조정 관리 엑셀 다운로드", notes = "")
+    @ApiImplicitParams(value = {
+        @ApiImplicitParam(name = "", value = "", paramType = "query", required = true),
+    })
+    @GetMapping("/excel-download")
+    public List<SearchRes> getStockStatusControlsForExcelDownload(
+        @Valid
+        SearchReq dto
+    ) {
+        return service.getStockStatusControlsForExcelDownload(dto);
+    }
+
+    @ApiOperation(value = "재고상태조정 관리 창고조회", notes = "재고상태조정 관리에 조회조건에 있는 조직창고를 조회한다")
+    @GetMapping("/stocks")
+    public List<SearchWarehouseRes> getStockStatusControlsWarehouse(
+        SearchWarehouseReq dto
+    ) {
+        return service.getStockStatusControlsWarehouse(dto);
+    }
+
+    @ApiOperation(value = "재고상태조정 관리 관리부서조회", notes = "재고상태조정 관리에 조회조건에 있는 관리부서를 조회한다")
+    @GetMapping("/organization/{wareNo}")
+    public FindOgNmRes getOrganizationDeptName(
+        @PathVariable
+        String wareNo
+    ) {
+        return service.getOrganizationDeptName(wareNo);
+    }
+
+    @ApiOperation(value = "재고상태조정 관리 상품조회.", notes = "재고상태조정 관리 품목종류에 해당하는 상품을 조회한다.")
+    @GetMapping("/product/{itmKnd}")
+    public List<FinditmPdCdRes> getStockStatusItmPdCd(
+        @PathVariable
+        String itmKnd
+    ) {
+        return service.getStockStatusItmPdCd(itmKnd);
+    }
+}
