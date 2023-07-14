165defe1e7245fc007a10fa9f62d6a52d8c6e120
sojeong.shin <gs.piit202@kyowon.com>
2023-07-14 13:56:04 +0900

    fix: 상조제휴 입금등록 관리, 어음입금 등록 수정
    Signed-off-by: sojeong.shin <gs.piit202@kyowon.com>

--- src/main/java/com/kyowon/sms/wells/web/withdrawal/idvrve/dto/WwdbBillDepositMgtDto.java
+++ src/main/java/com/kyowon/sms/wells/web/withdrawal/idvrve/dto/WwdbBillDepositMgtDto.java
@@ -16,9 +16,9 @@ public class WwdbBillDepositMgtDto {
     }
 
     public record SearchRes(
-        String cntrNo, /* 계약번호 */
-        String cntrSn, /* 계약일련번호 */
-        String cntrDtlNo, /* 계약상세번호 */
+        String cntrNo, /*계약번호*/
+        String cntrSn, /*계약일련번호*/
+        String cntrDtlNo, /*계약상세번호*/
         String mconBzsNm, /*거래처명*/
         String billRmkCn, /*어음구분*/
         String billDpAmt, /*입금액*/
