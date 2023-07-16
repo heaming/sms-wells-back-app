875860ba6804cc0975b5189b68190bf3306a9782
gs.piit2 <gs.piit02@kyowon.co.kr>
2023-07-16 15:45:56 +0900

    fix: PostMapping 으로 변경

--- src/main/java/com/kyowon/sms/wells/web/service/interfaces/rest/WsniSeasonFiltersInterfaceController.java
+++ src/main/java/com/kyowon/sms/wells/web/service/interfaces/rest/WsniSeasonFiltersInterfaceController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -30,7 +31,7 @@ public class WsniSeasonFiltersInterfaceController {
     private final WsniSeasonFiltersInterfaceService service;
 
     @ApiOperation(value = "W-SV-I-0007 웰스홈페이지 필터정보 조회", notes = "W-SV-I-0007 웰스홈페이지 필터정보 조회")
-    @GetMapping
+    @PostMapping
     public EaiWrapper getBsServiceHistories(
         @Valid
         @RequestBody
