91b2393c82e32ec44e98c4029aed00d9614bc2b3
jaehaYeon <gs.piit167@kyowon.com>
2023-07-17 18:52:03 +0900

    chore : IF 수정
    Signed-off-by: jaehaYeon <gs.piit167@kyowon.com>

--- src/main/java/com/kyowon/sms/wells/web/withdrawal/interfaces/dto/WwdbIntegrationDepositInterfaceDto.java
+++ src/main/java/com/kyowon/sms/wells/web/withdrawal/interfaces/dto/WwdbIntegrationDepositInterfaceDto.java
@@ -13,7 +13,6 @@ public class WwdbIntegrationDepositInterfaceDto {
     /* 공통 통합입금 내역 회사선택 코드 조회 Request Dto */
     @ApiModel("WwdbIntegrationDepositDto-SearchCompanyCodeReq")
     public record SearchCompanyCodeReq(
-        @NotBlank
         @JsonProperty("TENANT_ID")
         String tenantId
     ) {}
@@ -30,7 +29,6 @@ public class WwdbIntegrationDepositInterfaceDto {
     /* 공통 통합입금 내역 입금유형 코드 조회 Request Dto */
     @ApiModel("WwdbIntegrationDepositDto-SearchDepoCodeReq")
     public record SearchDepoCodeReq(
-        @NotBlank
         @JsonProperty("TENANT_ID")
         String tenantId
     ) {}
@@ -56,52 +54,52 @@ public class WwdbIntegrationDepositInterfaceDto {
     @ApiModel("WwdbIntegrationDepositDto-SearchDetailTradeRes")
     public record SearchDetailTradeRes(
         @JsonProperty("DP_DT")
-        String dpDt,           // 거래일자
+        String dpDt, // 거래일자
         @JsonProperty("DP_HMS")
-        String dpHms,          // 거래시간
+        String dpHms, // 거래시간
         @JsonProperty("DP_DV_CD")
-        String dpDvCd,        // 입금구분코드
+        String dpDvCd, // 입금구분코드
         @JsonProperty("DP_DV_NM")
-        String dpDvNm,        // 입금구분명
+        String dpDvNm, // 입금구분명
         @JsonProperty("ISTM_MCN")
-        String istmMcn,        // 할부개월
+        String istmMcn, // 할부개월
         @JsonProperty("CNTR_NO")
-        String cntrNo,         // 계약번호
+        String cntrNo, // 계약번호
         @JsonProperty("CNTR_DTL_NO")
-        String cntrDtlNo,     // 계약상세번호
+        String cntrDtlNo, // 계약상세번호
         @JsonProperty("CST_NM")
-        String cstNm,          // 계약자명
+        String cstNm, // 계약자명
         @JsonProperty("DP_AMT")
-        String dpAmt,          // 입금금액
+        String dpAmt, // 입금금액
         @JsonProperty("DP_CPRCNF_AMT")
-        String dpCprcnfAmt,   // 대사금액
+        String dpCprcnfAmt, // 대사금액
         @JsonProperty("CAN_AMT")
-        String canAmt,         // 취소금액
+        String canAmt, // 취소금액
         @JsonProperty("RFND_AMT")
-        String rfndAmt         // 환불금액
+        String rfndAmt // 환불금액
     ) {}
 
     /* 공통 통합입금 내역 조회 Request Dto */
     @ApiModel("WwdbIntegrationDepositDto-SearchItemizationReq")
     public record SearchItemizationReq(
         @JsonProperty("CNO_TP")
-        String cnoTp,          // 유형(1계좌/2카드)
+        String cnoTp, // 유형(1계좌/2카드)
         @JsonProperty("CNO")
-        String cno,             // 카드/계좌번호(유형이 1이면 계좌번호, 2면 카드번호)
+        String cno, // 카드/계좌번호(유형이 1이면 계좌번호, 2면 카드번호)
         @JsonProperty("CARD_APRNO")
-        String cardAprno,      // 승인번호
+        String cardAprno, // 승인번호
         @JsonProperty("RVE_CD")
-        String rveCd,          // 수납코드
+        String rveCd, // 수납코드
         @JsonProperty("KW_GRP_CO_CD")
-        String kwGrpCoCd,    // 회사코드
+        String kwGrpCoCd, // 회사코드
         @JsonProperty("DP_BLAM_YN")
-        String dpBlamYn,      // 잔액유무
+        String dpBlamYn, // 잔액유무
         @JsonProperty("DP_DT_ST")
-        String dpDtSt,        // 입금일자 from
+        String dpDtSt, // 입금일자 from
         @JsonProperty("DP_DT_ED")
-        String dpDtEd,        // 입금일자 to
+        String dpDtEd, // 입금일자 to
         @JsonProperty("DP_TP_CD")
-        String dpTpCd         // 입금유형(신용카드/가상계좌/회사통장/계좌이체/카드이체/어음/카드-PG/이체-PG/수수료공제/K머니/W머니/W포인트/K멤버스포인트 등)
+        String dpTpCd // 입금유형(신용카드/가상계좌/회사통장/계좌이체/카드이체/어음/카드-PG/이체-PG/수수료공제/K머니/W머니/W포인트/K멤버스포인트 등)
     ) {
         public SearchItemizationReq {
             cno = StringUtils.isNotEmpty(cno) ? DbEncUtil.enc(cno) : cno; // 암호화
@@ -112,49 +110,49 @@ public class WwdbIntegrationDepositInterfaceDto {
     @ApiModel("WwdbIntegrationDepositDto-SearchItemizationRes")
     public record SearchItemizationRes(
         @JsonProperty("DP_DT")
-        String dpDt,           // 입금일자
+        String dpDt, // 입금일자
         @JsonProperty("DP_HMS")
-        String dpHms,          // 일금시간
+        String dpHms, // 일금시간
         @JsonProperty("DP_TP_CD")
-        String dpTpCd,        // 입금유형
+        String dpTpCd, // 입금유형
         @JsonProperty("CNO")
-        String cno,             // 카드/계좌번호
+        String cno, // 카드/계좌번호
         @JsonProperty("FNIT_CD")
-        String fnitCd,         // 금융기관코드
+        String fnitCd, // 금융기관코드
         @JsonProperty("FNIT_NM")
-        String fnitNm,         // 카드/은행명
+        String fnitNm, // 카드/은행명
         @JsonProperty("CARD_APRNO")
-        String cardAprno,      // 승인번호
+        String cardAprno, // 승인번호
         @JsonProperty("ISTM_MCN")
-        String istmMcn,        // 할부개월
+        String istmMcn, // 할부개월
         @JsonProperty("DPR_NM")
-        String dprNm,          // 예금주/카드주명
+        String dprNm, // 예금주/카드주명
         @JsonProperty("DP_AMT")
-        String dpAmt,          // 입금금액
+        String dpAmt, // 입금금액
         @JsonProperty("DP_CPRCNF_AMT")
-        String dpCprcnfAmt,   // 대사금액
+        String dpCprcnfAmt, // 대사금액
         @JsonProperty("CAN_AMT")
-        String canAmt,         // 취소금액
+        String canAmt, // 취소금액
         @JsonProperty("RFND_AMT")
-        String rfndAmt,        // 환불금액
+        String rfndAmt, // 환불금액
         @JsonProperty("DP_BLAM")
-        String dpBlam,         // 잔액
+        String dpBlam, // 잔액
         @JsonProperty("OVR_AMT")
-        String ovrAmt,         // 초과금액
+        String ovrAmt, // 초과금액
         @JsonProperty("ITG_DP_NO")
-        String itgDpNo,       // 통합입금번호
+        String itgDpNo, // 통합입금번호
         @JsonProperty("RVE_CD")
-        String rveCd,          // 수납코드
+        String rveCd, // 수납코드
         @JsonProperty("RVE_NM")
-        String rveNm,          // 수납처(채권-웰스...)
+        String rveNm, // 수납처(채권-웰스...)
         @JsonProperty("PRTNR_NO")
-        String prtnrNo,        // 판매인ID
+        String prtnrNo, // 판매인ID
         @JsonProperty("PRTNR_NM")
-        String prtnrNm,        // 판매인명
+        String prtnrNm, // 판매인명
         @JsonProperty("KW_GRP_CO_CD")
-        String kwGrpCoCd,    // 회사코드
+        String kwGrpCoCd, // 회사코드
         @JsonProperty("KW_GRP_CO_NM")
-        String kwGrpCoNm     // 회사명
+        String kwGrpCoNm // 회사명
     ) {
         public SearchItemizationRes {
             cno = StringUtils.isNotEmpty(cno) ? DbEncUtil.dec(cno) : cno; //복호화
