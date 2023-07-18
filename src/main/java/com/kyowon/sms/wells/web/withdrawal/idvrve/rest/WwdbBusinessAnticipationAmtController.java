bd0f8290d45210a4cf49a0585be2bc27269d7777
heungjun.lee <gs.piit168@kyowon.com>
2023-07-18 18:15:19 +0900

    단위테스트 개별수납 소스수정
    Signed-off-by: heungjun.lee <gs.piit168@kyowon.com>

---
+++ src/main/java/com/kyowon/sms/wells/web/withdrawal/idvrve/rest/WwdbBusinessAnticipationAmtController.java
@@ -0,0 +1,36 @@
+package com.kyowon.sms.wells.web.withdrawal.idvrve.rest;
+
+import com.kyowon.sms.common.web.withdrawal.idvrve.dto.ZwdbEtcAnticipationDpProcsDto;
+import com.kyowon.sms.wells.web.withdrawal.idvrve.service.WwdcaBusinessAnticipationAmtService;
+import com.kyowon.sms.wells.web.withdrawal.zcommon.constants.WdWithdrawalConst;
+import com.sds.sflex.system.config.response.SaveResponse;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@Api(tags = "[WDB] 기타선수금 입금처리")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = WdWithdrawalConst.REST_URL_IDVRVE + "/etc-atam-deposits")
+public class WwdcaBusinessAnticipationAmtController {
+
+    private final WwdcaBusinessAnticipationAmtService service;
+
+    @ApiOperation(value = "기타선수금 입금처리를 한다.")
+    @PostMapping
+    public SaveResponse saveDepositProcs(
+        @RequestBody
+        @Valid
+        ZwdbEtcAnticipationDpProcsDto.SaveDepositProcessingReq dto
+    ) throws Exception {
+        return SaveResponse.builder()
+            .processCount(service.saveDepositProcs(dto))
+            .build();
+    }
+}
