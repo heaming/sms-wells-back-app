ba49b032d2d16ac02edba6911ed9f6f4010dba45
younuk.choi <gs.piit186@kyowon.co.kr>
2023-07-18 15:49:23 +0900

    feat : 취소등록 신규개발

---
+++ src/main/java/com/kyowon/sms/wells/web/contract/changeorder/converter/WctbCancelBaseConverter.java
@@ -0,0 +1,23 @@
+package com.kyowon.sms.wells.web.contract.changeorder.converter;
+
+import org.mapstruct.BeanMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+import com.kyowon.sms.wells.web.contract.changeorder.dto.WctbCancelBaseDto.FindCancelRes;
+import com.kyowon.sms.wells.web.contract.changeorder.dvo.WctbCancelBaseDvo;
+
+@Mapper(componentModel = "spring")
+public interface WctbCancelBaseConverter {
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    WctbCancelBaseDvo convertCancelBaseDvo(
+        WctbCancelBaseDvo source,
+        @MappingTarget
+        WctbCancelBaseDvo target
+    );
+
+    FindCancelRes mapCancelBaseDvoToFindCancelReslRes(WctbCancelBaseDvo dvo);
+
+    WctbCancelBaseDvo mapFindCancelReslResToCancelBaseDvo(FindCancelRes dto);
+}
