ab2fe2e1c3f087fe3d84fc42f37df4789272dc3d
park yesol <gs.piit239@kyowon.co.kr>
2023-07-28 15:38:42 +0900

    feat: 온라인 연계 과정 조회 수정 * 교육코드별 쿼리 분기처리 추가

--- src/main/java/com/kyowon/sms/wells/web/competence/education/converter/WpsbOnlineLinkCourseInqrConverter.java
+++ src/main/java/com/kyowon/sms/wells/web/competence/education/converter/WpsbOnlineLinkCourseInqrConverter.java
@@ -1,10 +1,15 @@
 package com.kyowon.sms.wells.web.competence.education.converter;
 
-import com.kyowon.sms.wells.web.competence.education.dto.WpsbOnlineLinkCourseInqrDto.*;
+import com.kyowon.sms.wells.web.competence.education.dto.WpsbOnlineLinkCourseInqrDto.SearchReq;
+import com.kyowon.sms.wells.web.competence.education.dto.WpsbOnlineLinkCourseInqrDto.SearchRes;
 import com.kyowon.sms.wells.web.competence.education.dvo.WpsbOnlineLinkCourseDvo;
 import org.mapstruct.Mapper;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface WpsbOnlineLinkCourseInqrConverter {
     WpsbOnlineLinkCourseDvo mapToDvo(SearchReq req);
+
+    List<SearchRes> dvoToSearchRes(List<WpsbOnlineLinkCourseDvo> dvos);
 }
--- src/main/java/com/kyowon/sms/wells/web/competence/education/service/WpsbOnlineLinkCourseInqrService.java
+++ src/main/java/com/kyowon/sms/wells/web/competence/education/service/WpsbOnlineLinkCourseInqrService.java
@@ -3,12 +3,14 @@ package com.kyowon.sms.wells.web.competence.education.service;
 import com.kyowon.sms.wells.web.competence.education.converter.WpsbOnlineLinkCourseInqrConverter;
 import com.kyowon.sms.wells.web.competence.education.dto.WpsbOnlineLinkCourseInqrDto.SearchReq;
 import com.kyowon.sms.wells.web.competence.education.dto.WpsbOnlineLinkCourseInqrDto.SearchRes;
+import com.kyowon.sms.wells.web.competence.education.dvo.WpsbOnlineLinkCourseDvo;
 import com.kyowon.sms.wells.web.competence.education.mapper.WpsbOnlineLinkCourseInqrMapper;
 import com.sds.sflex.system.config.datasource.PageInfo;
 import com.sds.sflex.system.config.datasource.PagingResult;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -20,13 +22,30 @@ public class WpsbOnlineLinkCourseInqrService {
 
     public PagingResult<SearchRes> getOnlineLinkCourseInqrPages(SearchReq dto, PageInfo pageInfo) {
         PagingResult<SearchRes> res = new PagingResult<>();
-        List<SearchRes> list =  mapper.selectOnlineLinkCourseInqrPages(dto, pageInfo);
-        res.setList(list);
+        WpsbOnlineLinkCourseDvo dvo = converter.mapToDvo(dto);
+        List<WpsbOnlineLinkCourseDvo> list = new ArrayList<>();
+        if("127".equals(dvo.getEducCrseNo())){
+            list = mapper.selectOnlineLinkCourseInqr127Pages(dvo, pageInfo);
+        }else if("128".equals(dvo.getEducCrseNo())){
+            list = mapper.selectOnlineLinkCourseInqr128Pages(dvo, pageInfo);
+        }else if("135".equals(dvo.getEducCrseNo())){
+            list = mapper.selectOnlineLinkCourseInqr135Pages(dvo, pageInfo);
+        }
+        res.setList(converter.dvoToSearchRes(list));
         res.setPageInfo(pageInfo);
-        return mapper.selectOnlineLinkCourseInqrPages(dto, pageInfo);
+        return res;
     }
 
     public List<SearchRes> getOnlineLinkCourseInqrsForExcelDownload(SearchReq dto) {
-        return mapper.selectOnlineLinkCourseInqrPages(dto);
+        WpsbOnlineLinkCourseDvo dvo = converter.mapToDvo(dto);
+        List<WpsbOnlineLinkCourseDvo> list = new ArrayList<>();
+         if("127".equals(dvo.getEducCrseNo())){
+            list = mapper.selectOnlineLinkCourseInqr127Pages(dvo);
+        }else if("128".equals(dvo.getEducCrseNo())){
+            list = mapper.selectOnlineLinkCourseInqr128Pages(dvo);
+        }else if("135".equals(dvo.getEducCrseNo())){
+            list = mapper.selectOnlineLinkCourseInqr135Pages(dvo);
+        }
+        return converter.dvoToSearchRes(list);
     }
 }
