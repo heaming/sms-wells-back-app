a671d0863c2df20e6d084a05b4d4f23c962369d5
gugyeongu <gs.piit163@kyowon.co.kr>
2023-08-02 09:12:18 +0900

    chore : 배치 호출 부분 불필요한 소스 삭제

--- src/main/java/com/kyowon/sms/wells/web/bond/transfer/service/WbnaCollectorAssignService.java
+++ src/main/java/com/kyowon/sms/wells/web/bond/transfer/service/WbnaCollectorAssignService.java
@@ -4,9 +4,7 @@ import com.kyowon.sflex.common.common.dvo.BatchCallReqDvo;
 import com.kyowon.sflex.common.common.service.BatchCallService;
 import com.kyowon.sms.common.web.bond.standard.dto.ZbnyBondAssignRuleMgtDto;
 import com.kyowon.sms.common.web.bond.standard.service.ZbnyBondAssignRuleMgtService;
-import com.kyowon.sms.common.web.bond.transfer.dvo.ZbnaBondBatchStatusDetailsDvo;
 import com.kyowon.sms.common.web.bond.transfer.dvo.ZbnaBondTransferAssignDvo;
-import com.kyowon.sms.common.web.bond.transfer.service.ZbnaBondBatchStatusDetailsService;
 import com.kyowon.sms.common.web.bond.zcommon.constants.BnBondConst;
 import com.kyowon.sms.wells.web.bond.transfer.converter.WbnaCollectorAssingConverter;
 import com.kyowon.sms.wells.web.bond.transfer.dto.WbnaCollectorAssignDto.*;
@@ -41,7 +39,6 @@ public class WbnaCollectorAssignService {
     private final ZbnaBondTransferAssignMgtService bondTransferAssignMgtService;
     private final ZbnyBondAssignRuleMgtService bondAssignRuleMgtService;
     private final BatchCallService batchCallService;
-    private final ZbnaBondBatchStatusDetailsService bondBatchStatusDetailsService;
 
     public List<SearchRes> getCollectorAssigns(
         SearchReq dto
@@ -74,13 +71,6 @@ public class WbnaCollectorAssignService {
         // 배정 가능 여부 확인
         BizAssert.isTrue(mapper.selectCanCollectorAssign(dto) == 1, "MSG_ALT_ASN_DTA_EXST");
 
-        // 기존 배치 동작 여부 확인 (true:배치 동작 가능, false:배치 동작 불가)
-        BizAssert.isTrue(
-            bondBatchStatusDetailsService
-                .checkBondBatchOperationStatus(dto.baseYm(), dto.bzHdqDvCd(), dto.clctamDvCd()),
-            "MSG_ALT_RUN_ASN_BAT_EXST"
-        ); // TODO 메시지 변경 필요한 경우 해당 id의 내용만 변경
-
         /*
         * 1.균등배정 -> 배정 타입1(균등배정 -> 지역우선배정) 0103
         * 2.전월담당자배정 -> 배정 타입2(균등배정 -> 기존배정고려균등배정) 0104
@@ -112,17 +102,6 @@ public class WbnaCollectorAssignService {
         //배치호출(try-catch대신, throw사용)
         String oldBondBatchJobRunId = batchCallService.runJob(dvo); //결과값으로 Control-M 에서 run-id를 받는다.
         BizAssert.isTrue(StringUtils.isNotEmpty(oldBondBatchJobRunId), "MSG_ALT_SVE_ERR"); // TODO 메시지 변경 필요(설계 혹은 공통 메시지 나오면 수정)
-
-        //배치 호출 후 호출 정보 저장(여기서 부터는 배치 호출 후기 때문에 에러가 나면 안됨)
-        ZbnaBondBatchStatusDetailsDvo bondBatchStatusDetailsDvo = new ZbnaBondBatchStatusDetailsDvo();
-        bondBatchStatusDetailsDvo.setBaseYm(dto.baseYm());
-        bondBatchStatusDetailsDvo.setBzHdqDvCd(dto.bzHdqDvCd());
-        bondBatchStatusDetailsDvo.setClctamDvCd(dto.clctamDvCd());
-        bondBatchStatusDetailsDvo.setBndBatWkId(oldBondBatchJobRunId);
-        bondBatchStatusDetailsDvo.setTfBizDvCd(BnBondConst.TfBizDvCd.COLLECTOR_ASSIGNMENT.getValue());
-        bondBatchStatusDetailsDvo.setBndNwDvCd(BnBondConst.BndNwDvCd.OLD.getValue());
-        bondBatchStatusDetailsDvo.setBndBatStatCd(BnBondConst.BndBatStatCd.START.getValue());
-        bondBatchStatusDetailsService.createBondBatchStatusDetails(bondBatchStatusDetailsDvo);
 
         return oldBondBatchJobRunId;
     }
