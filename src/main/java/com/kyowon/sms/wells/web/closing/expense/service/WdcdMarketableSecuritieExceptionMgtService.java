902abbe43f6208bb5e455c158e8534e4e52ef20c
gs.piit256 <gs.piit256@kyowon.co.kr>
2023-08-06 22:11:31 +0900

    fix : 운영비 관련 오류수정 및 개선작업 DEF_208	[시내교통비] 코드정보 조회 및 신청기능 오류	개발자조치 - 완 DEF_219	[시내교통비] 삭제기능 오류 -완 DEF_220	[시내교통비 승인 관리] 로그인 사용자별 조회기능 오류 -완 DEF_222	[경조금] 정보조회 및 저장 오류 -완 DEF_224	[경조금] 경조유형, 경조관계 오류 -완 DEF_225	[경조금] 조직레벨 초기셋팅 오류 -완 DEF_226	[경조금] 승인오류 및 권한에 따른 제어필요 -완

--- src/main/java/com/kyowon/sms/wells/web/closing/expense/service/WdcdMarketableSecuritieExceptionMgtService.java
+++ src/main/java/com/kyowon/sms/wells/web/closing/expense/service/WdcdMarketableSecuritieExceptionMgtService.java
@@ -1,15 +1,18 @@
 package com.kyowon.sms.wells.web.closing.expense.service;
 
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.kyowon.sms.wells.web.closing.expense.converter.WdcdMarketableSecuritieExceptionMgtConverter;
 import com.kyowon.sms.wells.web.closing.expense.dto.WdcdMarketableSecuritieExceptionMgttDto.*;
 import com.kyowon.sms.wells.web.closing.expense.dvo.WdcdMarketableSecuritieExceptionDvo;
 import com.kyowon.sms.wells.web.closing.expense.mapper.WdcdMarketableSecuritieExceptionMgtMapper;
 import com.sds.sflex.system.config.validation.BizAssert;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
@@ -32,41 +35,46 @@ public class WdcdMarketableSecuritieExceptionMgtService {
 
     @Transactional
     public int saveSettlementWithholdingTax(List<SaveReq> reqs) {
-
         int count = 0;
-        String opcsAdjNo = null;
+        //1. 해당월 확정완료 여부 체크
+        String cnfmYn = mapper.selectCheckWhetherMonthFinalized(reqs.get(0));
 
-        String checkMonth = mapper.selectCheckWhetherMonthFinalized(reqs.get(0));
-
-        if ("Y".equals(checkMonth)) {
-            BizAssert.isTrue(false, "등록수정 불가합니다.");
+        //2. 1번에서 CNFM_YN 결과값이 'Y'이면 alert 띄운다.
+        if ("Y".equals(cnfmYn)) {
+            BizAssert.isTrue(false, "해당 월은 등록/수정 불가합니다."); // TODO 메세지 처리
         }
 
-        List<AccCardInfoDetailRes> detailRess = mapper.selectAccCardInfoDetail(reqs.get(0));
+        //3. 카드 정보/금액 조회 (ASIS : getAccCardInfoDetail)
+        AccCardInfoDetailRes res = mapper.selectAccCardInfoDetail(reqs.get(0));
+        WdcdMarketableSecuritieExceptionDvo masterDvo = converter.mapAccCardInfoDetailResToWdcdMarketableSecuritieExceptionDvo(res);
+        masterDvo.setAdjOgId(res.ogId());
+        masterDvo.setOgTpCd(reqs.get(0).ogTpCd());
+        masterDvo.setPdstOpt(reqs.get(0).pdstOpt() != null ? reqs.get(0).pdstOpt() : "03");
+        Integer dstAmtTotal = 0;
+        for (SaveReq saveReq : reqs) {
+            dstAmtTotal += Integer.valueOf(saveReq.dstAmt());
+        }
+        masterDvo.setDstAmt(dstAmtTotal.toString());
 
-        for (AccCardInfoDetailRes detailRes : detailRess) {
-
-            WdcdMarketableSecuritieExceptionDvo dvo = converter.mapAccCardInfoDetailResToWdcdMarketableSecuritieExceptionDvo(detailRes);
-
-            if (!"null".equals(String.valueOf(detailRes))) {
-                count += mapper.updateAccMst(dvo);
-                count += mapper.deleteAccDetail(dvo);
-            }
+        if (!StringUtils.isEmpty(res.opcsAdjNo())) { //4. 3번에서 OPCS_ADJ_NO(운영비정산번호)가 값이 있으면 원천세 정산 수정 진행
+            count += mapper.deleteAccDetail(masterDvo);
+            count += mapper.updateAccMst(masterDvo);
+        } else { // 5. 3번에서 OPCS_ADJ_NO(운영비정산번호)가 값이 없으면 원천세 정산 신규 등록 진행
+            masterDvo.setOpcsAdjNo(mapper.selectOpcsAdjNo(masterDvo));
+            mapper.insertAccMst(masterDvo);
         }
 
         for (SaveReq req : reqs) {
-
             WdcdMarketableSecuritieExceptionDvo dvo = converter.mapSaveReqToWdcdMarketableSecuritieExceptionDvo(req);
-
-            dvo.setOpcsAdjNo(mapper.selectOpcsAdjNo(dvo));
-
-            count += mapper.insertAccMst(dvo);
-            count += mapper.insertAccDetail(dvo);
-            count += mapper.updateOpcsCard(dvo);
-            count += mapper.insertAccMap(dvo);
-
+            dvo.setOpcsAdjNo(masterDvo.getOpcsAdjNo());
+            if (Integer.valueOf(dvo.getDstAmt()) > 0) {
+                mapper.insertAccDetail(dvo);
+            }
         }
 
+        mapper.updateOpcsCard(masterDvo); // 6. 카드정보 업데이트 (ASIS : updateOpcsCard). 운영비 정산카드내역
+        masterDvo.setOpcsCardUseIzId(reqs.get(0).opcsCardUseIzId());
+        mapper.insertAccMap(masterDvo); //7. 매핑정보 등록 (ASIS : insertAccMap). 정산번호와 운영비사용카드내역 아이디를 매핑
         return count;
     }
 }
