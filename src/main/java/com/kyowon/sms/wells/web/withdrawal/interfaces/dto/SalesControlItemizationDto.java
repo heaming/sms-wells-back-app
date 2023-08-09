0dad273ad04d968921708b3621227a2d82595592
eunsun.ha <gs.piit241@kyowon.com>
2023-08-09 16:02:47 +0900

    chore : wells 매출조정 내역 조회 인터페이스 수정
    -일시에서 일자로 변경(쿼리)
    -주석 추가

--- src/main/java/com/kyowon/sms/wells/web/withdrawal/interfaces/dto/SalesControlItemizationDto.java
+++ src/main/java/com/kyowon/sms/wells/web/withdrawal/interfaces/dto/SalesControlItemizationDto.java
@@ -10,35 +10,35 @@ public class SalesControlItemizationDto {
     public record SearchReq(
         @NotBlank
         @JsonProperty("CNTR_NO")
-        String cntrNo,
+        String cntrNo, //계약번호
         @NotBlank
         @JsonProperty("CNTR_SN")
-        String cntrSn,
+        String cntrSn, //계약일련번호
         @NotBlank
         @JsonProperty("INQR_YM")
-        String inqrYm
+        String inqrYm //조회년월
     ) {}
 
     @ApiModel("SalesControlItemizationDto-SearchRes")
     public record SearchRes(
 
         @JsonProperty("END_NM")
-        String endNm,
+        String endNm, //시작년월
         @JsonProperty("RS_CD_NM")
-        String rsCdNm,
+        String rsCdNm, //종료년월
         @JsonProperty("SL_CTR_AMT")
-        String slCtrAmt,
+        String slCtrAmt, //매출조정금액
         @JsonProperty("SL_CTR_DV_CD_NM")
-        String slCtrDvCdNm,
+        String slCtrDvCdNm, //매출조정구분코드명
         @JsonProperty("SL_CTR_TP_CD_NM")
-        String slCtrTpCdNm,
+        String slCtrTpCdNm, //매출조정유형명
         @JsonProperty("SL_CTR_RSON_NM")
-        String slCtrRsonNm,
+        String slCtrRsonNm, //매출조정사유명
         @JsonProperty("RGST_DT")
-        String rgstDt,
+        String rgstDt, //등록일자
         @JsonProperty("RGR_NM")
-        String rgrNm,
+        String rgrNm, //등록자명
         @JsonProperty("RGR_ID")
-        String rgrId
+        String rgrId //등록자ID
     ) {}
 }
