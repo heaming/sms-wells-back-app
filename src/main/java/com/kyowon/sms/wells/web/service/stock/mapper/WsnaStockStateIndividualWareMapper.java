6eea01c0e1907d4ad682a062be0d55b5df577714
leeminwoo <gs.piitxx@kyowon.com>
2023-08-21 09:30:25 +0900

    feat : 재고현황개인창고(K-W-SV-U-0113M01) 신규 개발

---
+++ src/main/java/com/kyowon/sms/wells/web/service/stock/mapper/WsnaStockStateIndividualWareMapper.java
@@ -0,0 +1,19 @@
+package com.kyowon.sms.wells.web.service.stock.mapper;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.kyowon.sms.wells.web.service.stock.dto.WsnaStockStateIndividualWareDto.SearchReq;
+import com.kyowon.sms.wells.web.service.stock.dvo.WsnaStockStateIndividualWareDvo;
+import com.kyowon.sms.wells.web.service.stock.dvo.WsnaStockStateIndividualWareWareDvo;
+
+@Mapper
+public interface WsnaStockStateIndividualWareMapper {
+
+    List<WsnaStockStateIndividualWareWareDvo> selectMcByWares(SearchReq dto);
+
+    List<HashMap<String, String>> selectStockStateIndividualWare(WsnaStockStateIndividualWareDvo dvo);
+
+}
