195005cd8a75dd419b4e99c9443194f57688629d
jungheejin <gs.piit227@kyowon.co.kr>
2023-08-25 21:48:48 +0900

    feat : 월별출고집계현황 (K-W-SV-U-0255M01) 신규 개발

---
+++ src/main/java/com/kyowon/sms/wells/web/service/stock/dto/WsnaMonthlyOutOfStorageAgrgDto.java
@@ -0,0 +1,25 @@
+package com.kyowon.sms.wells.web.service.stock.dto;
+
+import io.swagger.annotations.ApiModel;
+
+/**
+ * <pre>
+ *  월별 출고 집계 현황
+ * </pre>
+ *
+ * @author junggheejin
+ * @since 2023.08.24
+ */
+public class WsnaMonthlyOutOfStorageAgrgDto {
+
+    @ApiModel(value = "WsnaMonthlyOutOfStorageAgrgDto-SearchReq")
+    public record SearchReq(
+        String startDt,
+        String endDt,
+        String wareDvCd,
+        String itmGdCd,
+        String itmKndCd,
+        String itmPdCd,
+        String useYn
+    ) {}
+}
---
+++ src/main/java/com/kyowon/sms/wells/web/service/stock/dvo/WsnaMonthlyOutOfStorageAgrgDvo.java
@@ -0,0 +1,31 @@
+package com.kyowon.sms.wells.web.service.stock.dvo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * <pre>
+ *  월별 출고 집계 현황 dvo
+ * </pre>
+ *
+ * @author junggheejin
+ * @since 2023.08.25
+ */
+@Getter
+@Setter
+public class WsnaMonthlyOutOfStorageAgrgDvo {
+
+    // PIVOT (FNL_VST_FSH_YM) 조건
+    String fnlVstFshYmStr;
+    // PIVOT (FNL_VST_FSH_YM) 필드
+    String fnlVstFshYmStrFields;
+
+    //DTO 매핑
+    String startDt;
+    String endDt;
+    String wareDvCd;
+    String itmGdCd;
+    String itmKndCd;
+    String itmPdCd;
+    String useYn;
+}
