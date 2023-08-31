473dbfd41d7a4f6e27b010e4d018eb3160f4a8c8
jungheejin <gs.piit227@kyowon.co.kr>
2023-08-31 18:50:23 +0900

    feat: 필터출고현황(세부) (K-W-SV-U-0263M01) 신규개발

---
+++ src/main/java/com/kyowon/sms/wells/web/service/stock/dto/WsnaFilterOutOfStorageDetailDto.java
@@ -0,0 +1,91 @@
+package com.kyowon.sms.wells.web.service.stock.dto;
+
+import io.swagger.annotations.ApiModel;
+
+public class WsnaFilterOutOfStorageDetailDto {
+
+    @ApiModel(value = "WsnaFilterOutOfStorageDetailDto-SearchReq")
+    public record SearchReq(
+        String startDt,
+
+        String endDt,
+
+        String wareDvCd,
+
+        String wareNoM,
+
+        String wareNoD,
+
+        String itmKndCd,
+
+        String mngrDvCd,
+
+        String refriDvCd,
+
+        String fnlSellTpCd,
+
+        String svBizHclsfCd,
+
+        String ogId,
+
+        String prtnrNo,
+
+        String dgr1LevlOgId,
+
+        String dgr2LevlOgId,
+
+        String dgr3LevlOgId,
+
+        String pdGrpCd,
+
+        String rgsnYn
+    ) {}
+    @ApiModel(value = "WsnaFilterOutOfStorageDetailDto-SearchRes")
+    public record SearchRes(
+        String fnlVstFshDt,
+
+        String cntrNo,
+
+        String cntrSn,
+
+        String cstNm,
+
+        String fnlSellTpCd,
+
+        String fnlSellTpNm,
+
+        String fnlPdCd,
+
+        String pdNm,
+
+        String svBizHclsfCd,
+
+        String svBizHclsfNm,
+
+        String svBizDclsfCd,
+
+        String svBizDclsfNm,
+
+        String sapMatCd,
+
+        String itmPdCd,
+
+        String itmPdNm,
+
+        String useQty,
+
+        String refriDvCd,
+
+        String refriDvNm,
+
+        String prtnrNo,
+
+        String prtnrKnm,
+
+        String ogNm,
+
+        String mngrDvCd,
+
+        String wkWareNo
+    ) {}
+}
