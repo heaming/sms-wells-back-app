34a539d205214f0650f9429008c4d61a91e82824
JungJingun <gs.piit36@kyowon.com>
2023-09-04 16:55:01 +0900

    fix : 수수료기준정보 수정

--- src/main/java/com/kyowon/sms/wells/web/fee/standard/mapper/WfeyFeeCalculationSqlFor201Mapper.java
+++ src/main/java/com/kyowon/sms/wells/web/fee/standard/mapper/WfeyFeeCalculationSqlFor201Mapper.java
@@ -9,6 +9,19 @@ import org.apache.ibatis.annotations.Mapper;
 public interface WfeyFeeCalculationSqlFor201Mapper {
 
     /**
+     * M조직 판매자 정착수수료 SQL Mapper (W020081, 1)
+     *
+     * @param baseYm      기준년월
+     * @param ogTpCd      조직유형코드
+     * @param feeCd       수수료코드
+     * @param feeTcntDvCd 수수료차수구분코드
+     * @return 처리개수
+     */
+    @SqlMethodInfo(methodName = "M조직 판매자 정착수수료계산", methodExplanation = "M조직 판매자 정착수수료 계산 SQL 매퍼")
+    Integer insertSettlementFeesForSeller(String baseYm, String ogTpCd, String coCd, String feeCd, String dtaCrtFeeCd, String feeTcntDvCd, String fnlFeeYn);
+
+
+    /**
      * M조직 판매자 유니폼수수료 SQL Mapper (W020098, 1)
      *
      * @param baseYm      기준년월
