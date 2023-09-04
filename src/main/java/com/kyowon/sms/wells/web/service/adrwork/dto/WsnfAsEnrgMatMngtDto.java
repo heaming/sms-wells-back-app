1afa53aa639054f542a7af791020de5f7741e394
gs.piit122 <kdycorp@kyowon.co.kr>
2023-09-04 09:30:12 +0900

    fix: AS 유형별 필요자재관리

--- src/main/java/com/kyowon/sms/wells/web/service/adrwork/dto/WsnfAsEnrgMatMngtDto.java
+++ src/main/java/com/kyowon/sms/wells/web/service/adrwork/dto/WsnfAsEnrgMatMngtDto.java
@@ -1,6 +1,5 @@
 package com.kyowon.sms.wells.web.service.adrwork.dto;
 
-import com.sds.sflex.common.utils.DbEncUtil;
 import io.swagger.annotations.ApiModel;
 
 import javax.validation.constraints.NotBlank;
@@ -9,55 +8,27 @@ public class WsnfAsEnrgMatMngtDto {
 
     @ApiModel(value = "WsnfAsEnrgMatMngtDto-SearchReq")
     public record SearchReq(
-        @NotBlank
+        String pdGrpCd,
         String pdCd,
-        String dateType,
-        String dateValueFromDt,
-        String dateValueToDt,
-        String svTpCd,
-        String prtnrNo,
-        String ogId
+        String classA,
+        String classB,
+        String classC
     ) {}
 
     @ApiModel(value = "WsnfAsEnrgMatMngtDto-SearchRes")
     public record SearchRes(
-        String ichrPrtnrNo,
-        String cntrCstNo,
-        String cntrNo,
-        String istllKnm,
-        String cralLocaraTno,
-        String mexnoEncr,
-        String cralIdvTno,
-        String adrId,
-        String zip,
-        String adr,
-        String cnslMoCn,
-        String cnslTpHclsfCd,
-        String cnslTpMclsfNm,
-        String svCnrOgId,
-        String ichrOgTpCd,
-        String rcpdt,
-        String vstDuedt,
-        String arvDt,
-        String vstCnfmdt,
-        String adrDvCd,
-        String itmPdCd,
-        String pdAbbrNm,
-        String svBizHclsfNm,
-        String prtnrNm,
-        String ogNm,
-        String cstSvAsnNo,
-        String sapMatCd,
         String pdCd,
         String pdNm,
+        String itmPdCd,
         String itmPdNm,
+        String sapMatCd,
         String cnslTpLcsfCdNm,
         String cnslCn,
         String itmRcmdRnk,
         String itmRcmdQty
     ) {
-//        public SearchRes {
-//            tel = cralLocaraTno + "-" + DbEncUtil.dec(mexnoEncr) + "-" + cralIdvTno;
-//        }
+        //        public SearchRes {
+        //            tel = cralLocaraTno + "-" + DbEncUtil.dec(mexnoEncr) + "-" + cralIdvTno;
+        //        }
     }
 }
--- src/main/java/com/kyowon/sms/wells/web/service/adrwork/dvo/WsnfAsEnrgMatMngtDvo.java
+++ src/main/java/com/kyowon/sms/wells/web/service/adrwork/dvo/WsnfAsEnrgMatMngtDvo.java
@@ -6,37 +6,11 @@ import lombok.Setter;
 @Getter
 @Setter
 public class WsnfAsEnrgMatMngtDvo {
-    String ichrPrtnrNo;
-    String cntrCstNo;
-    String cntrNo;
-    String istllKnm;
-    String cralLocaraTno;
-    String mexnoEncr;
-    String cralIdvTno;
-    String adrId;
-    String zip;
-    String adr;
-    String cnslMoCn;
-    String cnslTpHclsfCd;
-    String cnslTpMclsfNm;
-    String svCnrOgId;
-    String ichrOgTpCd;
-    String rcpdt;
-    String vstDuedt;
-    String arvDt;
-    String vstCnfmdt;
-    String adrDvCd;
-    String itmPdCd;
-    String pdAbbrNm;
-    String svBizHclsfNm;
-    String prtnrNm;
-    String ogNm;
-
-    String cstSvAsnNo;
-    String sapMatCd;
     String pdCd;
     String pdNm;
+    String itmPdCd;
     String itmPdNm;
+    String sapMatCd;
     String cnslTpLcsfCdNm;
     String cnslCn;
     String itmRcmdRnk;
