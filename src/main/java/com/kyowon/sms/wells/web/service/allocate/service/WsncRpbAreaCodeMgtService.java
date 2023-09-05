deca4ae6004b0eb0c3c85f7a7bf9327b4a4ab01a
gs.piit129 <gs.piit129@kyowon.co.kr>
2023-09-05 17:14:43 +0900

    chore : 수정

--- src/main/java/com/kyowon/sms/wells/web/service/allocate/service/WsncRpbAreaCodeMgtService.java
+++ src/main/java/com/kyowon/sms/wells/web/service/allocate/service/WsncRpbAreaCodeMgtService.java
@@ -1,6 +1,6 @@
 package com.kyowon.sms.wells.web.service.allocate.service;
 
-import static com.kyowon.sms.wells.web.service.allocate.dto.WsncRpbAreaCodeMgtDto.*;
+import static com.kyowon.sms.wells.web.service.allocate.dto.WsncRpbAreaCodeMgtDto.SaveReq;
 
 import java.util.List;
 
@@ -8,10 +8,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.kyowon.sms.wells.web.service.allocate.converter.WsncRpbAreaCodeMgtConverter;
+import com.kyowon.sms.wells.web.service.allocate.dto.WsncRpbAreaCodeMgtDto;
 import com.kyowon.sms.wells.web.service.allocate.dvo.WsncRpbAreaCodeDvo;
 import com.kyowon.sms.wells.web.service.allocate.mapper.WsncRpbAreaCodeMgtMapper;
-import com.sds.sflex.system.config.datasource.PageInfo;
-import com.sds.sflex.system.config.datasource.PagingResult;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -32,30 +31,41 @@ public class WsncRpbAreaCodeMgtService {
     private final WsncRpbAreaCodeMgtMapper mapper;
     private final WsncRpbAreaCodeMgtConverter converter;
 
-    /**
-     * 책임지역 지역코드 관리 - 조회 (페이징)
-     * @param dto : { zipForm : 우편번호 From, zipTo : 우편번호 To, fr2pLgldCd : 법정동코드 앞 2자리, ctpvNm : 시도명, ctctyNm : 시군구명,
-     *           ogId :서비스센터 ID, wrkGrpCd : 작업그룹코드, applyDate : 적용일자, locaraCdFrom : 지역코드 From, locaraCdTo : 지역코드 To }
-     * @param pageInfo : 페이징정보
-     * @return 조회결과
-     */
-    public PagingResult<SearchRes> getAreaCodePages(
-        SearchReq dto, PageInfo pageInfo
-    ) {
-        return new PagingResult<>(
-            this.converter.mapWsncRpbAreaCodeDvoToSearchRes(this.mapper.selectAreaCodePages(dto, pageInfo)), pageInfo
-        );
-    }
+    //    /**
+    //     * 책임지역 지역코드 관리 - 조회 (페이징)
+    //     * @param dto : { zipForm : 우편번호 From, zipTo : 우편번호 To, fr2pLgldCd : 법정동코드 앞 2자리, ctpvNm : 시도명, ctctyNm : 시군구명,
+    //     *           ogId :서비스센터 ID, wrkGrpCd : 작업그룹코드, applyDate : 적용일자, locaraCdFrom : 지역코드 From, locaraCdTo : 지역코드 To }
+    //     * @param pageInfo : 페이징정보
+    //     * @return 조회결과
+    //     */
+    //    public PagingResult<SearchRes> getAreaCodePages(
+    //        SearchReq dto, PageInfo pageInfo
+    //    ) {
+    //        return new PagingResult<>(
+    //            this.converter.mapWsncRpbAreaCodeDvoToSearchRes(this.mapper.selectAreaCodePages(dto, pageInfo)), pageInfo
+    //        );
+    //    }
+    //
+    //    /**
+    //     * 책임지역 지역코드 관리 - 조회 (엑셀다운로드)
+    //     * @param dto : { zipForm : 우편번호 From, zipTo : 우편번호 To, fr2pLgldCd : 법정동코드 앞 2자리, ctpvNm : 시도명, ctctyNm : 시군구명,
+    //     *           ogId :서비스센터 ID, wrkGrpCd : 작업그룹코드, applyDate : 적용일자, locaraCdFrom : 지역코드 From, locaraCdTo : 지역코드 To }
+    //     * @return 조회결과
+    //     */
+    //    public List<SearchRes> getAreaCodePagesExcelDownload(SearchReq dto) {
+    //        List<SearchRes> res = converter.mapWsncRpbAreaCodeDvoToSearchRes(mapper.selectAreaCodePages(dto));
+    //        return res;
+    //    }
 
     /**
-     * 책임지역 지역코드 관리 - 조회 (엑셀다운로드)
+     * 책임지역 지역코드 관리 - 조회
      * @param dto : { zipForm : 우편번호 From, zipTo : 우편번호 To, fr2pLgldCd : 법정동코드 앞 2자리, ctpvNm : 시도명, ctctyNm : 시군구명,
      *           ogId :서비스센터 ID, wrkGrpCd : 작업그룹코드, applyDate : 적용일자, locaraCdFrom : 지역코드 From, locaraCdTo : 지역코드 To }
      * @return 조회결과
      */
-    public List<SearchRes> getAreaCodePagesExcelDownload(SearchReq dto) {
-        List<SearchRes> res = converter.mapWsncRpbAreaCodeDvoToSearchRes(mapper.selectAreaCodePages(dto));
-        return res;
+    public List<WsncRpbAreaCodeMgtDto.SearchRes> getAreaCodes(WsncRpbAreaCodeMgtDto.SearchReq dto) {
+        List<WsncRpbAreaCodeDvo> res1 = mapper.selectAreaCodes(dto);
+        return converter.mapWsncRpbAreaCodeDvoToSearchRes(res1);
     }
 
     /**
