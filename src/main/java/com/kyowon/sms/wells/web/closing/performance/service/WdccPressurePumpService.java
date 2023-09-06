91f201e93b3e1ea606cf4bf572ab7db1bc7e2386
choi sung hun <gs.piit151@kyowon.com>
2023-09-06 14:37:33 +0900

    feat: 가압펌프 설치/철거 관리 신규 화면

---
+++ src/main/java/com/kyowon/sms/wells/web/closing/performance/service/WdccPressurePumpService.java
@@ -0,0 +1,84 @@
+package com.kyowon.sms.wells.web.closing.performance.service;
+
+import java.util.List;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.kyowon.sms.wells.web.closing.performance.converter.WdccPressurePumpConverter;
+import com.kyowon.sms.wells.web.closing.performance.dto.WdccPressurePumpDto.RemoveReq;
+import com.kyowon.sms.wells.web.closing.performance.dto.WdccPressurePumpDto.SaveReq;
+import com.kyowon.sms.wells.web.closing.performance.dto.WdccPressurePumpDto.SearchConfirmManagementReq;
+import com.kyowon.sms.wells.web.closing.performance.dto.WdccPressurePumpDto.SearchConfirmManagementRes;
+import com.kyowon.sms.wells.web.closing.performance.dto.WdccPressurePumpDto.SearchSalesBaseReq;
+import com.kyowon.sms.wells.web.closing.performance.dto.WdccPressurePumpDto.SearchSalesBaseRes;
+import com.kyowon.sms.wells.web.closing.performance.dvo.WdccPressurePumpDvo;
+import com.kyowon.sms.wells.web.closing.performance.mapper.WdccPressurePumpMapper;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class WdccPressurePumpService {
+
+    private final WdccPressurePumpMapper mapper;
+    private final WdccPressurePumpConverter converter;
+
+    public List<SearchConfirmManagementRes> getConfirmManagement(SearchConfirmManagementReq dto) {
+        return this.mapper.selectConfirmManagement(dto);
+    }
+
+    public List<SearchSalesBaseRes> getSalesBase(SearchSalesBaseReq dto) {
+        return this.mapper.selectSalesBase(dto);
+    }
+
+    /**
+     * 가압펌프 설치/철거 관리 - 가압펌프 확정관리 대상 저장
+     * @param dto
+     * @return processCount
+     */
+    @Transactional
+    public int saveConfirmManagement(List<SaveReq> dtos) throws Exception {
+        int processCount = 0;
+        int result = 0;
+        System.out.println(CollectionUtils.isNotEmpty(dtos));
+        if (CollectionUtils.isNotEmpty(dtos)) {
+            for (SaveReq dto : dtos) {
+                WdccPressurePumpDvo dvo = this.converter.mapSaveReqToWdccPressurePumpDvo(dto);
+                System.out.println(dto.gubn());
+                if ("사용".equals(dto.gubn())) {
+                    result = this.mapper.insertConfirmManagement(dvo);
+
+                } else if ("회수".equals(dto.gubn())) {
+                    result = this.mapper.updateConfirmManagement(dvo);
+                }
+
+                processCount = result;
+            }
+        }
+
+        if (processCount <= 0) {
+            return processCount;
+        }
+        return processCount;
+
+    }
+
+    /**
+     * 가압펌프 설치/철거 관리 - 가압펌프 확정관리 대상 삭제
+     * @param dto
+     * @return processCount
+     */
+    @Transactional
+    public int removeConfirmManagement(List<RemoveReq> dtos) {
+        int processCount = 0;
+
+        for (RemoveReq dto : dtos) {
+            WdccPressurePumpDvo dvo = converter.mapRemoveReqToWdccPressurePumpDvo(dto);
+            processCount += mapper.deleteConfirmManagement(dvo);
+        }
+
+        return processCount;
+    }
+}
