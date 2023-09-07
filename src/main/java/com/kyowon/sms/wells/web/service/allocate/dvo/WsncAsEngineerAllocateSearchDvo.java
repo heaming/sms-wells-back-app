e3e274c20fc0ab9468d299b81603267f0c3f7c9f
37758 <hunee95@kyowon.co.kr>
2023-09-07 17:46:59 +0900

    feat: 엔지니어 배정현황 개발(OZ 뷰어 및 처리결과입력 기능 제외)

---
+++ src/main/java/com/kyowon/sms/wells/web/service/allocate/dvo/WsncAsEngineerAllocateSearchDvo.java
@@ -0,0 +1,70 @@
+package com.kyowon.sms.wells.web.service.allocate.dvo;
+
+import com.sds.sflex.system.config.annotation.DBDecField;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ *
+ * <pre>
+ * K-W-SV-U-0021M01 엔지니어 배정현황
+ * </pre>
+ *
+ * @author 37758 이재훈
+ * @since 2023.09.06
+ */
+@Setter
+@Getter
+public class WsncAsEngineerAllocateSearchDvo {
+
+    String ogId;
+    String engId;
+    String rgsnYn;
+    String svDvCd;
+    String prgsDvCd;
+    String baseDateFrom;
+    String baseDateTo;
+    String vstCfrmDt;
+    String cfrmOnlyYn;
+    String sellTpCd;
+    String istConfCd;
+
+    String cntrNo;
+    String cntrSn;
+    String sellTp;
+    String istllKnm;
+    String cstGd;
+    String svTp;
+    String locaraTno;
+    @DBDecField
+    String exnoEncr;
+    String idvTno;
+    String cralLocaraTno;
+    @DBDecField
+    String mexnoEncr;
+    String cralIdvTno;
+    String cntrDt;
+    String sapMatCd;
+    String pdctPdCd;
+    String pdNm;
+    String fgptNm;
+    String alncmpCd;
+    String pdUswyCd;
+    String newAdrZip;
+    String adrDtl;
+    String cnslMoCn;
+    String svBizDclsfCd;
+    String asRefriDvCd;
+    String bsRefriDvCd;
+    String istDt;
+    String expireDt;
+    String egerAsnDt;
+    String vstDt;
+    String dtChange;
+    String cfrmDt;
+    String dtmChRsonCd;
+    String dtmChRsonDtlCn;
+    String prtnrNm;
+    String wkPrgsStatCd;
+}
