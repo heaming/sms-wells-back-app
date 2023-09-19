acdd6df36cf64749416d9e619987208b886f5afa
jungheejin <gs.piit227@kyowon.co.kr>
2023-09-19 17:17:32 +0900

    feat: 택배 배송업체 송장관리 신규 화면 개발

--- src/main/java/com/kyowon/sms/wells/web/service/stock/service/WsnaPcsvSppBzsIvcMgtService.java
+++ src/main/java/com/kyowon/sms/wells/web/service/stock/service/WsnaPcsvSppBzsIvcMgtService.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.collections4.CollectionUtils;
-import org.eclipse.jetty.util.StringUtil;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -35,6 +35,7 @@ public class WsnaPcsvSppBzsIvcMgtService {
     public UploadRes savePcsvSppBzsOutOfStorageExcelUpload(MultipartFile file) throws Exception {
 
         // TODO FIX 된 양식 기준으로 헤더 정의 필요 (현재는 ASIS 기준)
+
         // 1. Excel Header 정보 설정
         Map<String, String> headerTitle = getHeaderTitle();
 
@@ -88,55 +89,96 @@ public class WsnaPcsvSppBzsIvcMgtService {
         List<ExcelUploadErrorDvo> excelUploadErrorDvos = new ArrayList<>();
 
         if (CollectionUtils.isNotEmpty(list)) {
-            int row = 2;
+            int row = 1;
 
             for (WsnaPcsvSppBzsIvcExcelUploadDvo dvo : list) {
+                row++;
+
                 List<String> emptyColumnNames = new ArrayList();
 
                 // TODO FIX 된 양식 유효성 정의 필요 (현재는 ASIS 기준)
+                String reqDv = dvo.getReqDv();
+                String cntrNo = dvo.getCntrNo();
+                String cntrSn = dvo.getCntrSn();
+                String sppIvcNo = dvo.getSppIvcNo();
+                String sppBzsPdId = dvo.getSppBzsPdId();
+                String pcsvCompDv = dvo.getPcsvCompDv();
                 // 구분
-                if (StringUtil.isEmpty(dvo.getReqDv())) {
+                if (StringUtils.isEmpty(reqDv)) {
                     emptyColumnNames.add("reqDv");
                 }
                 // 계약번호
-                if (StringUtil.isEmpty(dvo.getCntrNo())) {
+                if (StringUtils.isEmpty(cntrNo)) {
                     emptyColumnNames.add("cntrNo");
                 }
                 // 계약일련번호
-                if (StringUtil.isEmpty(dvo.getCntrSn())) {
+                if (StringUtils.isEmpty(cntrSn)) {
                     emptyColumnNames.add("cntrSn");
                 }
                 // 송장번호
-                if (StringUtil.isEmpty(dvo.getSppIvcNo())) {
+                if (StringUtils.isEmpty(sppIvcNo)) {
                     emptyColumnNames.add("sppIvcNo");
                 }
                 // SR번호
-                if (StringUtil.isEmpty(dvo.getSppBzsPdId())) {
+                if (StringUtils.isEmpty(sppBzsPdId)) {
                     emptyColumnNames.add("sppBzsPdId");
                 }
                 // 택배사 구분
-                if (StringUtil.isEmpty(dvo.getPcsvCompDv())) {
+                if (StringUtils.isEmpty(pcsvCompDv)) {
                     emptyColumnNames.add("pcsvCompDv");
                 }
 
-                //  데이터 없을시 [유효하지 않은 항목이 있습니다.]
-                for (String column : emptyColumnNames) {
+                if (CollectionUtils.isNotEmpty(emptyColumnNames)) {
+                    //  데이터 존재 체크 [유효하지 않은 항목이 있습니다.]
+                    for (String column : emptyColumnNames) {
+                        excelUploadErrorDvos.add(
+                            getErrorDvo(
+                                row, headerTitle.get(column),
+                                messageResourceService.getMessage("MSG_ALT_EXIST_INVALID_ITEM")
+                            )
+                        );
+                    }
+                    continue;
+                }
+
+                // 데이터 존재하는경우에만 유효성 체크
+
+                // 송장번호 숫자 체크 [숫자만 입력 가능합니다.]
+                if (StringUtils.isNotEmpty(sppIvcNo) && !isNumeric(sppIvcNo)) {
+                    String header = headerTitle.get("sppIvcNo");
                     excelUploadErrorDvos.add(
                         getErrorDvo(
-                            row, headerTitle.get(column),
-                            messageResourceService.getMessage("MSG_ALT_EXIST_INVALID_ITEM", headerTitle.get(column))
+                            row, header,
+                            messageResourceService.getMessage("MSG_ALT_ONLY_NUMBER", header)
+                        )
+                    );
+                }
+                // 계약일련번호 숫자 체크 [숫자만 입력 가능합니다.]
+                if (StringUtils.isNotEmpty(cntrSn) && !isNumeric(cntrSn)) {
+                    String header = headerTitle.get("cntrSn");
+                    excelUploadErrorDvos.add(
+                        getErrorDvo(
+                            row, header,
+                            messageResourceService.getMessage("MSG_ALT_ONLY_NUMBER", header)
                         )
                     );
                 }
 
-                // 추가 유효성체크
-
-                row++;
             }
         }
         return excelUploadErrorDvos;
     }
 
+    /**
+     * 숫자 체크 (정규식): 소수점까지 체크
+     */
+    private boolean isNumeric(String s) {
+        return s.matches("[0-9]+(.[0-9])");
+    }
+
+    /**
+     * 에러 정보 Dvo
+     */
     private ExcelUploadErrorDvo getErrorDvo(int row, String header, String errorData) {
         ExcelUploadErrorDvo errorDvo = new ExcelUploadErrorDvo();
         errorDvo.setErrorRow(row);
