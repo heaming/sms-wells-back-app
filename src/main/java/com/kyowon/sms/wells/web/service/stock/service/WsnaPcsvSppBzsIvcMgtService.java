26c8f1c5048a83079eb33806fab6e56095e8f588
jungheejin <gs.piit227@kyowon.co.kr>
2023-09-19 11:39:37 +0900

    feat: 택배 배송업체 송장관리 신규 화면 개발

--- src/main/java/com/kyowon/sms/wells/web/service/stock/service/WsnaPcsvSppBzsIvcMgtService.java
+++ src/main/java/com/kyowon/sms/wells/web/service/stock/service/WsnaPcsvSppBzsIvcMgtService.java
@@ -12,7 +12,6 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.kyowon.sms.wells.web.service.stock.dvo.WsnaPcsvSppBzsIvcExcelUploadDvo;
-import com.kyowon.sms.wells.web.service.stock.dvo.WsnaPcsvSppBzsIvcExcelUploadValidDvo;
 import com.sds.sflex.common.common.dto.ExcelUploadDto;
 import com.sds.sflex.common.common.dto.ExcelUploadDto.UploadRes;
 import com.sds.sflex.common.common.dvo.ExcelMetaDvo;
@@ -35,51 +34,29 @@ public class WsnaPcsvSppBzsIvcMgtService {
     @Transactional
     public UploadRes savePcsvSppBzsOutOfStorageExcelUpload(MultipartFile file) throws Exception {
 
-        // 업로드 엑셀 헤더 설정
         // TODO FIX 된 양식 기준으로 헤더 정의 필요 (현재는 ASIS 기준)
-        Map<String, String> headerTitle = new LinkedHashMap<>();
-        headerTitle.put("reqDv", "구분");
-        headerTitle.put("cntrNo", "계약번호");
-        headerTitle.put("pcsvCompDv", "택배사구분");
-        headerTitle.put("sppIvcNo", "송장번호");
-        headerTitle.put("sppBzsPdId", "SR번호");
+        // 1. Excel Header 정보 설정
+        Map<String, String> headerTitle = getHeaderTitle();
 
-        // file 복호화
-        List<WsnaPcsvSppBzsIvcExcelUploadDvo> list = excelReadService
-            .readExcel(file, new ExcelMetaDvo(1, headerTitle), WsnaPcsvSppBzsIvcExcelUploadDvo.class, true);
+        // 2. File 데이터 Read
+        List<WsnaPcsvSppBzsIvcExcelUploadDvo> list = excelReadService.readExcel(
+            file, new ExcelMetaDvo(1, headerTitle), WsnaPcsvSppBzsIvcExcelUploadDvo.class, true
+        );
 
-        // 업로드 에러
-        List<ExcelUploadErrorDvo> excelUploadErrorDvos = new ArrayList<>();
+        // 3. 유효성 체크
+        List<ExcelUploadErrorDvo> excelUploadErrorDvos = validateExcelDatas(headerTitle, list);
 
-        if (CollectionUtils.isNotEmpty(list)) {
-            int row = 2;
-
-            WsnaPcsvSppBzsIvcExcelUploadValidDvo validDvo = new WsnaPcsvSppBzsIvcExcelUploadValidDvo();
-            validDvo.setHeaderTitle(headerTitle);
-            validDvo.setErrorDvos(excelUploadErrorDvos);
-
-            for (WsnaPcsvSppBzsIvcExcelUploadDvo dvo : list) {
-                validDvo.setDvo(dvo);
-                validDvo.setRow(row);
-                //유효성체크
-                this.checkExcelUploadValid(validDvo);
-                row++;
+        // 4. Data 저장 (에러메세지 없는경우)
+        if (CollectionUtils.isEmpty(excelUploadErrorDvos)) {
+            for (WsnaPcsvSppBzsIvcExcelUploadDvo insertData : list) {
+                log.info("insertData: {}", insertData);
+                // TODO 저장할 테이블 지정
+                int result = 1;
+                BizAssert.isTrue(result == 1, "MSG_ALT_SVE_ERR");
             }
-
-            excelUploadErrorDvos = validDvo.getErrorDvos();
-
-            // 에러메세지 없는경우 (정상 데이터) 인경우 테이블 저장
-            if (CollectionUtils.isEmpty(excelUploadErrorDvos)) {
-                for (WsnaPcsvSppBzsIvcExcelUploadDvo insertData : list) {
-                    log.info("insertData: {}", insertData);
-                    // TODO 저장할 테이블 지정
-                    int result = 1;
-                    BizAssert.isTrue(result == 1, "MSG_ALT_SVE_ERR");
-                }
-            }
-
         }
 
+        // 5. Upload 결과 리턴
         return ExcelUploadDto.UploadRes.builder()
             .status(CollectionUtils.isNotEmpty(list) && CollectionUtils.isEmpty(excelUploadErrorDvos) ? "S" : "E")
             .errorInfo(excelUploadErrorDvos)
@@ -87,53 +64,84 @@ public class WsnaPcsvSppBzsIvcMgtService {
     }
 
     /**
-     * 엑셀업로드 유효성 체크
-     * @param validDvo
+     * 엑셀업로드 헤더 설정
      */
-    private void checkExcelUploadValid(WsnaPcsvSppBzsIvcExcelUploadValidDvo validDvo) {
+    private Map<String, String> getHeaderTitle() {
 
-        WsnaPcsvSppBzsIvcExcelUploadDvo dvo = validDvo.getDvo();
-        Map<String, String> headerTitle = validDvo.getHeaderTitle();
-        List<ExcelUploadErrorDvo> excelUploadErrorDvos = validDvo.getErrorDvos();
+        Map<String, String> headerTitle = new LinkedHashMap<>();
+        headerTitle.put("reqDv", "구분");
+        headerTitle.put("cntrNo", "계약번호");
+        headerTitle.put("cntrSn", "계약일련번호");
+        headerTitle.put("pcsvCompDv", "택배사구분");
+        headerTitle.put("sppIvcNo", "송장번호");
+        headerTitle.put("sppBzsPdId", "SR번호");
 
-        List<String> emptyColumnNames = new ArrayList();
-        // 유효성체크 (headerTitle 기준으로 정의해야함)
+        return headerTitle;
+    }
 
-        // TODO FIX 된 양식 유효성 정의 필요 (현재는 ASIS 기준)
-        // 구분
-        if (StringUtil.isBlank(dvo.getReqDv())) {
-            emptyColumnNames.add("reqDv");
-        }
-        // 계약번호
-        if (StringUtil.isBlank(dvo.getCntrNo())) {
-            emptyColumnNames.add("cntrNo");
-        }
-        // 송장번호
-        if (StringUtil.isBlank(dvo.getSppIvcNo())) {
-            emptyColumnNames.add("sppIvcNo");
-        }
-        // SR번호
-        if (StringUtil.isBlank(dvo.getSppBzsPdId())) {
-            emptyColumnNames.add("sppBzsPdId");
-        }
-        // 택배사 구분
-        if (StringUtil.isBlank(dvo.getPcsvCompDv())) {
-            emptyColumnNames.add("pcsvCompDv");
-        }
+    /**
+     * 엑셀업로드 유효성 체크 설정
+     */
+    private List<ExcelUploadErrorDvo> validateExcelDatas(
+        Map<String, String> headerTitle, List<WsnaPcsvSppBzsIvcExcelUploadDvo> list
+    ) {
+        List<ExcelUploadErrorDvo> excelUploadErrorDvos = new ArrayList<>();
 
-        // 화면에 각각 에러표시를 위한 저장
-        int row = validDvo.getRow();
-        for (String column : emptyColumnNames) {
-            ExcelUploadErrorDvo errorDvo = new ExcelUploadErrorDvo();
-            errorDvo.setErrorRow(row);
-            errorDvo.setHeaderName(headerTitle.get(column));
-            errorDvo.setErrorData(
-                messageResourceService.getMessage(
-                    "MSG_ALT_EXIST_INVALID_ITEM" // 유효하지 않은 항목이 있습니다.
-                )
-            );
-            excelUploadErrorDvos.add(errorDvo);
-        }
+        if (CollectionUtils.isNotEmpty(list)) {
+            int row = 2;
 
+            for (WsnaPcsvSppBzsIvcExcelUploadDvo dvo : list) {
+                List<String> emptyColumnNames = new ArrayList();
+
+                // TODO FIX 된 양식 유효성 정의 필요 (현재는 ASIS 기준)
+                // 구분
+                if (StringUtil.isEmpty(dvo.getReqDv())) {
+                    emptyColumnNames.add("reqDv");
+                }
+                // 계약번호
+                if (StringUtil.isEmpty(dvo.getCntrNo())) {
+                    emptyColumnNames.add("cntrNo");
+                }
+                // 계약일련번호
+                if (StringUtil.isEmpty(dvo.getCntrSn())) {
+                    emptyColumnNames.add("cntrSn");
+                }
+                // 송장번호
+                if (StringUtil.isEmpty(dvo.getSppIvcNo())) {
+                    emptyColumnNames.add("sppIvcNo");
+                }
+                // SR번호
+                if (StringUtil.isEmpty(dvo.getSppBzsPdId())) {
+                    emptyColumnNames.add("sppBzsPdId");
+                }
+                // 택배사 구분
+                if (StringUtil.isEmpty(dvo.getPcsvCompDv())) {
+                    emptyColumnNames.add("pcsvCompDv");
+                }
+
+                //  데이터 없을시 [유효하지 않은 항목이 있습니다.]
+                for (String column : emptyColumnNames) {
+                    excelUploadErrorDvos.add(
+                        getErrorDvo(
+                            row, headerTitle.get(column),
+                            messageResourceService.getMessage("MSG_ALT_EXIST_INVALID_ITEM", headerTitle.get(column))
+                        )
+                    );
+                }
+
+                // 추가 유효성체크
+
+                row++;
+            }
+        }
+        return excelUploadErrorDvos;
+    }
+
+    private ExcelUploadErrorDvo getErrorDvo(int row, String header, String errorData) {
+        ExcelUploadErrorDvo errorDvo = new ExcelUploadErrorDvo();
+        errorDvo.setErrorRow(row);
+        errorDvo.setHeaderName(header);
+        errorDvo.setErrorData(errorData);
+        return errorDvo;
     }
 }
