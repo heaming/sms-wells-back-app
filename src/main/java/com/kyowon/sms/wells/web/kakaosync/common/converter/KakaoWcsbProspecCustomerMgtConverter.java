4bf4c19035cf702d87734bfba7322d6aae2335b2
hohwanm <hohwanm@kyowon.com>
2023-09-20 18:05:11 +0900

    fix : 카카오 싱크 완료/에러 페이지 추가 * * 카카오싱크 완료 페이지 추가 * 카카오싱크 에러 페이지 추가 *

--- src/main/java/com/kyowon/sms/wells/web/kakaosync/common/converter/KakaoWcsbProspecCustomerMgtConverter.java
+++ src/main/java/com/kyowon/sms/wells/web/kakaosync/common/converter/KakaoWcsbProspecCustomerMgtConverter.java
@@ -1,8 +1,5 @@
 package com.kyowon.sms.wells.web.kakaosync.common.converter;
 
-import com.kyowon.sms.wells.web.customer.prospective.dto.WcsbProspecCustomerMgtDto;
-import com.kyowon.sms.wells.web.customer.prospective.dvo.WcsbProspecCustomerDvo;
-import com.kyowon.sms.wells.web.customer.prospective.dvo.WcsbTbSsopPspcCstDdlvHistDvo;
 import com.kyowon.sms.wells.web.kakaosync.common.dto.KakaoWcsbProspecCustomerMgtDto;
 import com.kyowon.sms.wells.web.kakaosync.common.dvo.KakaoWcsbSyncDefaultDvo;
 import org.mapstruct.Mapper;
@@ -10,15 +7,8 @@ import org.mapstruct.Mapper;
 @Mapper(componentModel = "spring")
 public interface KakaoWcsbProspecCustomerMgtConverter {
 
-    //    @Mapping(source = "fnlMdfcDtm", target = "orglFnlMdfcDtm")
-    WcsbProspecCustomerDvo mapSaveReqToWcsbProspecCustomerDvo(WcsbProspecCustomerMgtDto.TbSsopPspcCstBas dto);
-
     KakaoWcsbSyncDefaultDvo mapSaveReqToKakaoWcsbSyncDefaultDvo(
         KakaoWcsbProspecCustomerMgtDto.SaveReq dto
     );
 
-    // 교사정보
-    WcsbTbSsopPspcCstDdlvHistDvo mapSaveReqToWcsbTbSsopPspcCstDdlvHistDvo(
-        WcsbProspecCustomerMgtDto.TbSsopPspcCstDdlvHist dto
-    );
 }
--- src/main/java/com/kyowon/sms/wells/web/kakaosync/common/service/KakaoWcsbProspecCustomerMgtService.java
+++ src/main/java/com/kyowon/sms/wells/web/kakaosync/common/service/KakaoWcsbProspecCustomerMgtService.java
@@ -61,33 +61,13 @@ public class KakaoWcsbProspecCustomerMgtService {
             BizAssert.isFalse(dto.phoneNumber().equals(akdhpno), "담당EP 휴대전화 번호와  동일한 번호는 등록할 수 없습니다.");
         }
 
-        WcsbTbSsopPspcCstDdlvHistDvo teacherVo = converter
-            .mapSaveReqToWcsbTbSsopPspcCstDdlvHistDvo(
-                new WcsbProspecCustomerMgtDto.TbSsopPspcCstDdlvHist(
-                    "",
-                    "", "", "", "", "",
-                    "", "", "", ""
-                )
-            );
+        WcsbTbSsopPspcCstDdlvHistDvo teacherVo = new WcsbTbSsopPspcCstDdlvHistDvo();
 
         //  카카오싱크 + default dvo
         KakaoWcsbSyncDefaultDvo defaultdvo = converter.mapSaveReqToKakaoWcsbSyncDefaultDvo(dto);
 
         //  가망고객기본 dvo
-        WcsbProspecCustomerDvo dvo = converter.mapSaveReqToWcsbProspecCustomerDvo(
-            new WcsbProspecCustomerMgtDto.TbSsopPspcCstBas(
-                "",
-                "", "", "", "", "", "", "", "", "",
-                "", "", "", "", "", "", "", "", "", "",
-                "", "", "", "", "", "", "", "",
-                "", "", "", "", "", "", "", "",
-                0, "", "", "", "", "", "",
-                "", "", "", "", "", "", "",
-                "", "", "", "", "", "", "",
-                "", "", "", "", "", "", "",
-                "", "", "", "", ""
-            )
-        );
+        WcsbProspecCustomerDvo dvo = new WcsbProspecCustomerDvo();
 
         /**
          * #0. 카카오싱크 기본 ()
