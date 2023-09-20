5eeaf2411782b61c25a8c87d4cc51de59794d60a
gs.piit182 <gs.piit182@kyowon.com>
2023-09-20 09:26:27 +0900

    chore: 소스정리

--- src/main/java/com/kyowon/sms/wells/web/service/common/mapper/WsnyInstallSeparationMgtMapper.java
+++ src/main/java/com/kyowon/sms/wells/web/service/common/mapper/WsnyInstallSeparationMgtMapper.java
@@ -1,16 +1,17 @@
 package com.kyowon.sms.wells.web.service.common.mapper;
 
-import com.kyowon.sms.wells.web.service.common.dto.WsnyInstallSeparationMgtDto.*;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.kyowon.sms.wells.web.service.common.dto.WsnyInstallSeparationMgtDto.SearchReq;
+import com.kyowon.sms.wells.web.service.common.dto.WsnyInstallSeparationMgtDto.SearchRes;
 import com.kyowon.sms.wells.web.service.common.dvo.WsnyInstallSeparationMgtDvo;
 import com.sds.sflex.system.config.datasource.PageInfo;
 import com.sds.sflex.system.config.datasource.PagingResult;
-import org.apache.ibatis.annotations.Mapper;
-
-import java.util.List;
 
 @Mapper
 public interface WsnyInstallSeparationMgtMapper {
-    List<FindProductsRes> selectPdCd(String pdGr);
     PagingResult<SearchRes> selectInstallSeparationCosts(SearchReq dto, PageInfo pageInfo);
     List<SearchRes> selectInstallSeparationCosts(SearchReq dto);
     int insertInstallSeparationCosts(WsnyInstallSeparationMgtDvo dvo);
