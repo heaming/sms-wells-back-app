376adfa9ebf9240775d918bbd70aeebde47b85ff
jungheejin <gs.piit227@kyowon.co.kr>
2023-09-25 10:52:56 +0900

    feat : MD 상품 출고관리 소스 추가

--- src/main/java/com/kyowon/sms/wells/web/service/stock/mapper/WsnaMdProductOutOfStorageExcelUploadMapper.java
+++ src/main/java/com/kyowon/sms/wells/web/service/stock/mapper/WsnaMdProductOutOfStorageExcelUploadMapper.java
@@ -2,9 +2,5 @@ package com.kyowon.sms.wells.web.service.stock.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 
-import com.kyowon.sms.wells.web.service.stock.dvo.WsnaMdProdcutOutOfStorageSaveDvo;
-
 @Mapper
-public interface WsnaMdProductOutOfStorageExcelUploadMapper {
-    void updateSvpdCstSvExcnIz(WsnaMdProdcutOutOfStorageSaveDvo vo);
-}
+public interface WsnaMdProductOutOfStorageExcelUploadMapper {}
