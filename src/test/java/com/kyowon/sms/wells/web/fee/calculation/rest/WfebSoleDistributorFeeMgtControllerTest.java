6d733c797cfbc52b1f813daaa5a077a35429d3d8
Jaewoo Jung <gs.piit15@kyowon.co.kr>
2023-10-05 21:54:52 +0900

    fix: 총판 실적 생성 로직 수수료기준정보 참조로 변경

---
+++ src/test/java/com/kyowon/sms/wells/web/fee/calculation/rest/WfebSoleDistributorFeeMgtControllerTest.java
@@ -0,0 +1,33 @@
+package com.kyowon.sms.wells.web.fee.calculation.rest;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.Commit;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import com.kyowon.sms.wells.web.fee.calculation.dto.WfebSoleDistributorFeeMgtDto.CreateReq;
+import com.kyowon.sms.wells.web.fee.zcommon.constants.CtFeeConst;
+import com.sds.sflex.system.config.test.SpringTestSupport;
+
+class WfebSoleDistributorFeeMgtControllerTest extends SpringTestSupport {
+
+    private static final String BASE_URL = CtFeeConst.REST_URL_V1 + "/sole-distributor";
+
+    @Commit
+    @Test
+    @DisplayName("[총판] 총판 실적생성")
+    void aggregateDistributorPerformance() throws Exception {
+        CreateReq dto = new CreateReq("202308", "02");
+
+        MockHttpServletRequestBuilder request = post(String.format("%s/aggregate", BASE_URL))
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(dto));
+
+        mockMvc.perform(request)
+            .andExpect(status().isOk());
+    }
+}
