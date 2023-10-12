35fe1df4b083c72e0e2fa1eb42507775ebcbae23
gs.piit238 <gs.piit238@kyowon.co.kr>
2023-10-12 16:41:11 +0900

    chore : dto 주석 추가

--- src/main/java/com/kyowon/sms/wells/web/service/stock/dto/WsnaQomAsnDto.java
+++ src/main/java/com/kyowon/sms/wells/web/service/stock/dto/WsnaQomAsnDto.java
@@ -23,12 +23,16 @@ public class WsnaQomAsnDto {
     @Builder
     @ApiModel("WsnaQomAsnDto-SearchWareReq")
     public record SearchWareReq(
+        // 기준년월
         @NotBlank
         String apyYm,
+        // 배정년월
         @NotBlank
         String asnOjYm,
+        // 창고구분
         @NotBlank
         String wareDvCd,
+        // 창고상세구분
         @NotBlank
         String wareDtlDvCd
     ) {}
@@ -36,64 +40,101 @@ public class WsnaQomAsnDto {
     @Builder
     @ApiModel("WsnaQomAsnDto-SearchReq")
     public record SearchReq(
+        // 기준년월
         @NotBlank
         String apyYm,
+        // 배정년월
         @NotBlank
         String asnOjYm,
-
+        // 회차
         @Positive
         @Max(999999999999L)
         BigDecimal cnt,
+        // 출고창고
         @NotBlank
         String ostrWareNo,
+        // 품목종류
         String itmKndCd,
+        // 품목코드
         String itmPdCd,
+        // 창고구분
         @NotBlank
         String wareDvCd,
+        // 창고상세구분
         @NotBlank
         String wareDtlDvCd,
+        // 입고창고
         String strWareNo
     ) {}
 
     @Builder
     @ApiModel("WsnaQomAsnDto-SearchRes")
     public record SearchRes(
+        // SAP코드
         String sapCd,
+        // 품목코드
         String itmPdCd,
+        // 품목명
         String itmPdNm,
+        // 관리단위
         String mngtUnit,
+        // 자재등급
         String matGdCd,
-
+        // 입고창고번호
         String wareNo,
+        // 사번
         String prtnrNo,
+        // 파트너명
         String prtnrNm,
+        // 입고창고
         String wareNm,
-
+        // 센터창고수량
         BigDecimal centerQty,
+        // 일반배정수량
         BigDecimal geQty,
+        // 법인배정수량
         BigDecimal crpQty,
+        // 총배정수량
         BigDecimal totalQty,
+        // 가중치적용수량
         BigDecimal apyQty,
+        // 누적출고수량
         BigDecimal ostrQty,
+        // BS완료수량
         BigDecimal bsQty,
+        // 현재재고수량
         BigDecimal stocQty,
+        // 금주예정수량
         BigDecimal thwkQty,
+        // 차주예정수량
         BigDecimal borrQty,
+        // 확정수량
         BigDecimal cnfmQty,
+        // 박스수량
         BigDecimal boxQty,
-
+        // 당월BS FULL수량
         BigDecimal bsFullQty,
+        // 당월BS배정수량
         BigDecimal bsAsnQty,
+        // 재고(조직)수량
         BigDecimal stockOgQty,
+        // 재고(개인)수량
         BigDecimal stockIndvQty,
+        // 소요수량
         BigDecimal nedQty,
+        // 박스단위수량
         BigDecimal boxUnitQty,
-
+        // 빌딩코드
         String bldCd,
+        // 빌딩명
         String bldNm,
+        // 전화번호
         String telNo,
+        // 우편번호
         String adrZip,
+        // 주소1
         String rnadr,
+        // 주소2
         String rdadr
 
     ) {}
@@ -101,17 +142,23 @@ public class WsnaQomAsnDto {
     @Builder
     @ApiModel("WsnaQomAsnDto-CreateReq")
     public record CreateReq(
+        // 기준년월
         @NotBlank
         String apyYm,
+        // 배정년월
         @NotBlank
         String asnOjYm,
+        // 회차
         @Positive
         @Max(999999999999L)
         BigDecimal cnt,
+        // 출고창고
         @NotBlank
         String ostrWareNo,
+        // 창고구분
         @NotBlank
         String wareDvCd,
+        // 창고상세구분
         @NotBlank
         String wareDtlDvCd
 
@@ -120,16 +167,20 @@ public class WsnaQomAsnDto {
     @Builder
     @ApiModel("WsnaQomAsnDto-RemoveReq")
     public record RemoveReq(
-
+        // 배정년월
         @NotBlank
         String asnOjYm,
+        // 회차
         @Positive
         @Max(999999999999L)
         BigDecimal cnt,
+        // 출고창고
         @NotBlank
         String ostrWareNo,
+        // 창고구분
         @NotBlank
         String wareDvCd,
+        // 창고상세구분
         @NotBlank
         String wareDtlDvCd
     ) {}
@@ -137,10 +188,13 @@ public class WsnaQomAsnDto {
     @Builder
     @ApiModel("WsnaQomAsnDto-EditReq")
     public record EditReq(
+        // 기준년월
         @NotBlank
         String apyYm,
+        // 배정년월
         @NotBlank
         String asnOjYm,
+        // 출고창고
         @NotBlank
         String ostrWareNo
 
--- src/main/java/com/kyowon/sms/wells/web/service/stock/dto/WsnaShippingByFilterTypeDto.java
+++ src/main/java/com/kyowon/sms/wells/web/service/stock/dto/WsnaShippingByFilterTypeDto.java
@@ -23,9 +23,11 @@ public class WsnaShippingByFilterTypeDto {
     @Builder
     @ApiModel("WsnaShippingByFilterTypeDto-SearchPdRes")
     public record SearchPdRes(
-
+        // 품목코드
         String pdCd,
+        // 품목명
         String pdNm,
+        // 품목그룹
         String itmGrCd
 
     ) {}
@@ -33,22 +35,30 @@ public class WsnaShippingByFilterTypeDto {
     @Builder
     @ApiModel("WsnaShippingByFilterTypeDto-SearchReq")
     public record SearchReq(
-
+        // 방문시작일자
         @NotBlank
         @ValidDate
         String strtDt,
+        // 방문종료일자
         @NotBlank
         @ValidDate
         String endDt,
+        // 창고구분
         @NotBlank
         String wareDvCd,
+        // 반납여부
         String gbYn,
+        // 업무유형
         String svBizHclsfCd,
+        // 상위창고
         String hgrWareNo,
+        // 창고번호
         String wareNo,
+        // 품목그룹
         String itmGrCd,
+        // 품목코드
         String itmPdCd,
-
+        // 수거일자
         @ValidDate
         String ostrConfDt
 
@@ -57,28 +67,49 @@ public class WsnaShippingByFilterTypeDto {
     @Builder
     @ApiModel("WsnaShippingByFilterTypeDto-SearchRes")
     public record SearchRes(
-
+        // 고객서비스배정번호
         String cstSvAsnNo,
+        // 작업출고일련번호
         int wkOstrSn,
+        // 소속
         String ogNm,
+        // 사번
         String prtnrNo,
+        // 성명
         String prtnrKnm,
+        // 반납여부
         String stkrPrntYn,
+        // 특이사항
         String rmkCn,
+        // 계약번호
         String cntrNo,
+        // 계약일련번호
         int cntrSn,
+        // 계약상세번호
         String cntrDtlNo,
+        // 고객명
         String cstKnm,
+        // 상품명
         String basePdNm,
+        // 수거일자
         String ostrConfDt,
+        // 방문일자
         String fnlVstFshDt,
+        // 품목코드
         String itmPdCd,
+        // 필터종류
         String itmPdNm,
+        // 고객유형
         String sellTpNm,
+        // 업무유형
         String svBizHclsfNm,
+        // 유형상세
         String svBizDclsfNm,
+        // 유무상
         String refriDvNm,
+        // 우편번호
         String adrZip,
+        // 고객주소
         String cstAdr
 
     ) {}
@@ -86,14 +117,18 @@ public class WsnaShippingByFilterTypeDto {
     @Builder
     @ApiModel("WsnaShippingByFilterTypeDto-EditReq")
     public record EditReq(
-
+        // 고객서비스배정번호
         @NotBlank
         String cstSvAsnNo,
+        // 작업출고일련번호
         @Positive
         int wkOstrSn,
+        // 반납여부
         String stkrPrntYn,
+        // 특이사항
         @Size(max = 4000)
         String rmkCn,
+        // 수거일자
         @ValidDate
         String ostrConfDt
     ) {}
