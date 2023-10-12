4612f66f58d282bfb11f554ad447a40e9115c4cf
인송미 <gs.piit54@kyowon.com>
2023-10-12 17:20:55 +0900

    chore : 결함 조치

--- src/main/java/com/kyowon/sms/wells/web/closing/sales/service/WdcbMonthlySalesVolumeTableService.java
+++ src/main/java/com/kyowon/sms/wells/web/closing/sales/service/WdcbMonthlySalesVolumeTableService.java
@@ -1,25 +1,45 @@
 package com.kyowon.sms.wells.web.closing.sales.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
 import com.kyowon.sms.wells.web.closing.sales.dto.WdcbMonthlySalesVolumeTableDto.SearchPaymentRes;
 import com.kyowon.sms.wells.web.closing.sales.dto.WdcbMonthlySalesVolumeTableDto.SearchRentalRes;
 import com.kyowon.sms.wells.web.closing.sales.dto.WdcbMonthlySalesVolumeTableDto.SearchReq;
 import com.kyowon.sms.wells.web.closing.sales.mapper.WdcbMonthlySalesVolumeTableMapper;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
 
-import java.util.List;
-
+/**
+ * <pre>
+ * 월 매출수량 집계표
+ * </pre>
+ *
+ * @author songmi.in
+ * @since 2023-03-24
+ */
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class WdcbMonthlySalesVolumeTableService {
     private final WdcbMonthlySalesVolumeTableMapper mapper;
 
+    /**
+      * 월 매출수량 집계표 - 렌탈 조회
+      * @param req
+      * @return 조회결과
+      */
     public List<SearchRentalRes> getRentalMonthSalesQuantity(SearchReq req) {
         return mapper.selectRentalMonthSalesQuantity(req);
     }
 
+    /**
+      * 월 매출수량 집계표 - 일시불/할부 조회
+      * @param req
+      * @return 조회결과
+      */
     public List<SearchPaymentRes> getPaymentMonthSalesQuantity(SearchReq req) {
         return mapper.selectPaymentMonthSalesQuantity(req);
     }
