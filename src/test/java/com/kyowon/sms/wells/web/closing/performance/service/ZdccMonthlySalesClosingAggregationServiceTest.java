c7d1d01027a4174cb2f5aceb4d3a85127b2ed59b
gugyeongu <gs.piit163@kyowon.co.kr>
2023-10-13 18:04:34 +0900

    chore: 사용하지 않는 서비스 삭제

---
+++ src/test/java/com/kyowon/sms/wells/web/closing/performance/service/ZdccMonthlySalesClosingAggregationServiceTest.java
@@ -0,0 +1,20 @@
+package com.kyowon.sms.wells.web.closing.performance.service;
+
+import com.kyowon.sms.common.web.closing.performance.service.ZdccMonthlySalesClosingAggregationService;
+import com.sds.sflex.system.config.test.SpringTestSupport;
+import lombok.RequiredArgsConstructor;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@RequiredArgsConstructor
+class ZdccMonthlySalesClosingAggregationServiceTest extends SpringTestSupport {
+    private final ZdccMonthlySalesClosingAggregationService service;
+
+    @Test
+    @DisplayName("WELLS 매출 월마감 내역 집계 테스트")
+    void createSalesMonthlyClosingDetailsTally() {
+        int result = service.createSalesMonthlyClosingDetailsTallyForWells("20231009", "W20093924043", "1");
+        Assertions.assertThat(result).isEqualTo(1);
+    }
+}
