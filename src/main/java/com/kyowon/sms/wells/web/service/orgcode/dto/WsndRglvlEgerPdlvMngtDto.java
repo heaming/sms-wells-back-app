d5bc5ebdd35f021cdde3ace3a907256923b0f2cd
gs.piit231 <gs.piit231@kyowon.co.kr>
2023-10-16 17:49:21 +0900

    feat: 방문고객 급지집계 화면, 출고지 조회 관련 추가

--- src/main/java/com/kyowon/sms/wells/web/service/orgcode/dto/WsndRglvlEgerPdlvMngtDto.java
+++ src/main/java/com/kyowon/sms/wells/web/service/orgcode/dto/WsndRglvlEgerPdlvMngtDto.java
@@ -12,12 +12,36 @@ public class WsndRglvlEgerPdlvMngtDto {
 
     @ApiModel(value = "WsndRglvlEgerPdlvMngtDto-SearchRes")
     public record SearchRes(
-        String ogId,        // 서비스센터
+        String cnrOgId,     // 서비스센터
         String prtnrNo,     // 사번
         String prtnrKnm,    // 성명
-        String basPdlv,     // 기본출고지
-        String basPdlvAdr,  // 기본출고지 주소
-        String bstrPdlv,    // 기본출고지
-        String bstrPdlvAdr  // 기본출고지 주소
+        String pdlvNo,      // 출고지번호
+        String apyStrtdt,   // 적용시작일
+        String apyEnddt,    // 적용종료일
+        String zip,         // 우편번호
+        String pdlvAdr,     // 기본출고지 주소
+        String pdlvNm,      // 출고지명
+        String bstrZip,     // 출장출고지 우편번호
+        String bstrPdlvAdr, // 출장출고지 주소
+        String bstrPdlvNm,  // 출장 출고지명
+        String apr,         // 승인
+        String aprTeamNm,   // 승인팀명
+        String aprPrtnrNo,  // 승인자 사번
+        String aprPrtnrKnm, // 승인자 성명
+        String bstrRson     // 출장사유
+    ){}
+
+    @ApiModel(value = "WsndRglvlEgerPdlvMngtDto-SaveEgerReq")
+    public record SaveEgerReq(
+        String pdlvNm,          // 기본출고지
+        String bstrPdlvNm,      // 출장출고지
+        String apyStrtdt,       // 적용시작일
+        String apyEnddt         // 적용종료일
+    ){}
+
+    @ApiModel(value = "WsndRglvlEgerPdlvMngtDto-ApporovalEgerReq")
+    public record ApporovalEgerReq(
+        String cnrOgId,  // 서비스센터
+        String prtnrNo   // 사번
     ){}
 }
