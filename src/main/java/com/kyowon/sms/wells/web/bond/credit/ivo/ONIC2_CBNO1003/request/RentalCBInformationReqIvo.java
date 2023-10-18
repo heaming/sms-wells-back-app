da6824d6d34c6583a7c4b02f9d06f9e830c3723e
gilyong.han <gs.piit128@kyowon.co.kr>
2023-10-18 21:09:18 +0900

    refactor: 렌탈CB정보 조회 리펙토링

---
+++ src/main/java/com/kyowon/sms/wells/web/bond/credit/ivo/ONIC2_CBNO1003/request/RentalCBInformationReqIvo.java
@@ -0,0 +1,40 @@
+package com.kyowon.sms.wells.web.bond.credit.ivo.ONIC2_CBNO1003.request;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * <pre>
+ * ONIC2_CBNO1003 CB렌탈정보 조회 요청 Request Interface Dvo
+ * </pre>
+ *
+ * @author gilyong.han
+ * @since 2023-10-18
+ * */
+
+@Getter
+@Setter
+public class RentalCBInformationReqIvo {
+    @JsonProperty("COMM")
+    private CommReq commReq;
+    @JsonProperty("ROWDATA_5")
+    private RowData5Req rowData5Req;
+    @JsonProperty("ROWDATA_4")
+    private RowData4Req rowData4Req;
+    @JsonProperty("SUMMARYITEM_6")
+    private SummaryItem6Req summaryItem6Req;
+    @JsonProperty("SUMMARYITEM_6_REPAET")
+    private List<SummaryItem6RepaetItemReq> summaryItem6RepaetItemReqs;
+    @JsonProperty("DATA")
+    private DataReq dataReq;
+    @JsonProperty("SUMMARYITEM_7")
+    private SummaryItem7Req summaryItem7Req;
+    @JsonProperty("SUMMARYITEM_7_REPAET")
+    private List<SummaryItem7RepaetItemReq> summaryItem7RepaetItemReqs;
+    @JsonProperty("ROWDATA_1")
+    private RowData1Req rowData1Req;
+}
