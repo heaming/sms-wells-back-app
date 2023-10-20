417352f401b2f41878f5a33d66fce5f102e6ad9d
gs.piit231 <gs.piit231@kyowon.co.kr>
2023-10-20 11:06:06 +0900

    fit: 방문고객 급지 집계, 츨고지 조회, 급지 엔지니어 출장관리 수정

--- src/main/java/com/kyowon/sms/wells/web/service/orgcode/service/WsndRglvlEgerPdlvMngtService.java
+++ src/main/java/com/kyowon/sms/wells/web/service/orgcode/service/WsndRglvlEgerPdlvMngtService.java
@@ -11,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import com.kyowon.sms.wells.web.service.orgcode.dto.WsndRglvlEgerPdlvMngtDto.*;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -32,5 +33,31 @@ public class WsndRglvlEgerPdlvMngtService {
 
     public List<SearchRes> getRglvlEgerPdlvMngtExcelDownload(SearchReq dto){
         return mapper.selectRglvlEgerPdlvMngtPages(dto);
+    }
+
+    @Transactional
+    public int saveRglvlEgerPdlvMngt(List<SaveEgerReq> dtos){
+        int processCount = 0;
+
+        for(SaveEgerReq dto : dtos){
+            int result = 0;
+            result += mapper.insertRglvlEgerPdlvMngts(dto);
+            processCount += result;
+        }
+
+        return processCount;
+    }
+
+    @Transactional
+    public int approvalRglvlEgerPdlvMngt(List<SaveEgerReq> dtos){
+        int processCount = 0;
+
+        for(SaveEgerReq dto : dtos){
+            int result = 0;
+            result += mapper.updateRglvlEgerPdlvMngts(dto);
+            processCount += result;
+        }
+
+        return processCount;
     }
 }
