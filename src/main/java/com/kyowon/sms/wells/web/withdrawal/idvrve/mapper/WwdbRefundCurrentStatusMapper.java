8e36b82a8b57e89b3af4812113fa861b3ec5d13a
heungjun.lee <gs.piit168@kyowon.com>
2023-10-20 19:33:09 +0900

    단위테스트 개별수납 소스수정
    Signed-off-by: heungjun.lee <gs.piit168@kyowon.com>

--- src/main/java/com/kyowon/sms/wells/web/withdrawal/idvrve/mapper/WwdbRefundCurrentStatusMapper.java
+++ src/main/java/com/kyowon/sms/wells/web/withdrawal/idvrve/mapper/WwdbRefundCurrentStatusMapper.java
@@ -16,29 +16,68 @@ import com.sds.sflex.system.config.datasource.PagingResult;
 @Mapper
 public interface WwdbRefundCurrentStatusMapper {
 
+    /**
+     * 환불내역 목록
+     *
+     * @param req
+     * @param pageInfo
+     * @return PagingResult<SearchRefundHistoryRes>
+     */
     PagingResult<SearchRefundHistoryRes> selectRefundHistory(
         SearchRefundHistoryReq req,
         PageInfo pageInfo
     );
 
+    /**
+     * 환불내역 엑셀다운로드
+     *
+     * @param req
+     * @return List<SearchRefundHistoryRes>
+     */
     List<SearchRefundHistoryRes> selectRefundHistory(
         SearchRefundHistoryReq req
     );
 
+    /**
+     * 카드사별 환불내역 목록
+     *
+     * @param req
+     * @param pageInfo
+     * @return PagingResult<SearchCardRefundHistoryRes>
+     */
     PagingResult<SearchCardRefundHistoryRes> selectCardRefundHistory(
         SearchCardRefundHistoryReq req,
         PageInfo pageInfo
     );
 
+    /**
+     * 카드사별 환불내역 엑셀다운로드
+     *
+     * @param req
+     * @return List<SearchCardRefundHistoryRes>
+     */
     List<SearchCardRefundHistoryRes> selectCardRefundHistory(
         SearchCardRefundHistoryReq req
     );
 
+    /**
+     * 전금내역 목록
+     *
+     * @param req
+     * @param pageInfo
+     * @return PagingResult<SearchBalanceTransferRefundHistoryRes>
+     */
     PagingResult<SearchBalanceTransferRefundHistoryRes> selectBalanceTransferRefundHistory(
         SearchBalanceTransferRefundHistoryReq req,
         PageInfo pageInfo
     );
 
+    /**
+     * 전금내역 목록 엑셀다운로드
+     *
+     * @param req
+     * @return List<SearchBalanceTransferRefundHistoryRes>
+     */
     List<SearchBalanceTransferRefundHistoryRes> selectBalanceTransferRefundHistory(
         SearchBalanceTransferRefundHistoryReq req
     );
