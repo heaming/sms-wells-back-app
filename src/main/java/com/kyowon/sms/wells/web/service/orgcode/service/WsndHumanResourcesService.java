483bb094b10ceb6291aad8368c941a2e5f1027db
Kim June <gs.piit34@kyowon.com>
2023-10-27 13:40:23 +0900

    fix: 주석 추가

--- src/main/java/com/kyowon/sms/wells/web/service/orgcode/service/WsndHumanResourcesService.java
+++ src/main/java/com/kyowon/sms/wells/web/service/orgcode/service/WsndHumanResourcesService.java
@@ -11,11 +11,21 @@ import com.sds.sflex.system.config.datasource.PagingResult;
 
 import lombok.RequiredArgsConstructor;
 
+/**
+ * @author Anonymous
+ * @since 2023-12-31
+ */
 @Service
 @RequiredArgsConstructor
 public class WsndHumanResourcesService {
     private final WsndHumanResourcesMapper mapper;
 
+    /**
+     * 인사기본정보 조회
+     * @param dto
+     * @param pageInfo
+     * @return
+     */
     public PagingResult<SearchRes> getHumanResourcesPages(
         SearchReq dto, PageInfo pageInfo
     ) {
