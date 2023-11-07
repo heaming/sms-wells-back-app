7c3b8068dc43c44ae72ca4b1d2012deeb520c990
gs.piit161 <gs.piit161@kyowon.com>
2023-11-07 21:17:37 +0900

    fix: 공제대체 테스트 수정

---
+++ src/main/java/com/kyowon/sms/wells/web/deduction/adsb/dto/WdebAwAdsbMgtDto.java
@@ -0,0 +1,15 @@
+package com.kyowon.sms.wells.web.deduction.adsb.dto;
+
+import io.swagger.annotations.ApiModel;
+import lombok.Builder;
+
+public class WdebAwAdsbMgtDto {
+
+    @Builder
+    @ApiModel("WdebAwAdsbMgtDto-CreateReq")
+    public record CreateReq(
+        String baseYm, // 반영년월
+        String ogTpCd // 조직유형
+    ) {}
+
+}
