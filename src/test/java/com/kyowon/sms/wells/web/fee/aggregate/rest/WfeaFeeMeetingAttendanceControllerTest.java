a464ac44100b174aa8603b2d2311d9469e635e8c
Jaewoo Jung <gs.piit15@kyowon.co.kr>
2023-11-11 20:39:24 +0900

    fix: 지점장 실전교육수료여부 집계 로직 오류 수정

--- src/test/java/com/kyowon/sms/wells/web/fee/aggregate/rest/WfeaFeeMeetingAttendanceControllerTest.java
+++ src/test/java/com/kyowon/sms/wells/web/fee/aggregate/rest/WfeaFeeMeetingAttendanceControllerTest.java
@@ -22,7 +22,7 @@ class WfeaFeeMeetingAttendanceControllerTest extends SpringTestSupport {
     @DisplayName("[P조직] 미팅참석자 실적 생성")
     void saveWellsFeeMeetingAttendances() throws Exception {
         // given
-        SaveReq dto1 = new SaveReq("W01", "202308", "W0104", "02");
+        SaveReq dto1 = new SaveReq("W01", "202301", "W0104", "02");
 
         // when & then
         MockHttpServletRequestBuilder request1 = post(BASE_URL)
@@ -33,7 +33,7 @@ class WfeaFeeMeetingAttendanceControllerTest extends SpringTestSupport {
             .andExpect(status().isOk());
 
         // given
-        SaveReq dto2 = new SaveReq("W01", "202308", "W0105", "02");
+        SaveReq dto2 = new SaveReq("W01", "202301", "W0105", "02");
 
         // when & then
         MockHttpServletRequestBuilder request2 = post(BASE_URL)
@@ -49,7 +49,7 @@ class WfeaFeeMeetingAttendanceControllerTest extends SpringTestSupport {
     @DisplayName("[M조직] 미팅참석자 실적 생성")
     void saveWellsFeeMeetingAttendancesForManager() throws Exception {
         // given
-        SaveReq dto1 = new SaveReq("W02", "202308", "W0204", "02");
+        SaveReq dto1 = new SaveReq("W02", "202301", "W0204", "02");
 
         // when & then
         MockHttpServletRequestBuilder request1 = post(BASE_URL)
@@ -60,7 +60,7 @@ class WfeaFeeMeetingAttendanceControllerTest extends SpringTestSupport {
             .andExpect(status().isOk());
 
         // given
-        SaveReq dto2 = new SaveReq("W02", "202308", "W0205", "02");
+        SaveReq dto2 = new SaveReq("W02", "202301", "W0205", "02");
 
         // when & then
         MockHttpServletRequestBuilder request2 = post(BASE_URL)
@@ -76,7 +76,7 @@ class WfeaFeeMeetingAttendanceControllerTest extends SpringTestSupport {
     @DisplayName("[홈마스터] 미팅참석자 실적 생성")
     void saveWellsFeeMeetingAttendancesForHomemaster() throws Exception {
         // given
-        SaveReq dto1 = new SaveReq("W03", "202308", "W0302", "02");
+        SaveReq dto1 = new SaveReq("W03", "202301", "W0302", "02");
 
         // when & then
         MockHttpServletRequestBuilder request1 = post(BASE_URL)
@@ -87,7 +87,7 @@ class WfeaFeeMeetingAttendanceControllerTest extends SpringTestSupport {
             .andExpect(status().isOk());
 
         // given
-        SaveReq dto2 = new SaveReq("W03", "202308", "W0301", "02");
+        SaveReq dto2 = new SaveReq("W03", "202301", "W0301", "02");
 
         // when & then
         MockHttpServletRequestBuilder request2 = post(BASE_URL)
