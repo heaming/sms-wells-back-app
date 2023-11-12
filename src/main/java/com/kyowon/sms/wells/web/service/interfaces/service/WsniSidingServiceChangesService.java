2484f6818bad95f03aa49d5b804e284c07eddad5
gs.piit37 <gs.piit37@kyowon.com>
2023-11-12 14:28:53 +0900

    fix: 모종 패키지 변경 - 주기표 및 배정 관련 불필요 로그 삭제

--- src/main/java/com/kyowon/sms/wells/web/service/interfaces/service/WsniSidingServiceChangesService.java
+++ src/main/java/com/kyowon/sms/wells/web/service/interfaces/service/WsniSidingServiceChangesService.java
@@ -101,9 +101,6 @@ public class WsniSidingServiceChangesService {
         /*요청 구분에 따라 처리 - 1: 패키지변경, 4:다음회차 방문 중지*/
         //IF(P_REQ_GB = '1' AND P_DATA_STUS != '3') THEN
         if ("1".equals(req.asAkDvCd()) && !"3".equals(req.mtrProcsStatCd())) {
-            log.debug("req.akChdt 1 : " + req.akChdt());
-            log.debug("req.akChdt.subString 1 : " + req.akChdt().substring(0, 6));
-
             /*방문주기 재생성(SP_LC_SERVICEVISIT_482_LST_I06)*/
             service1.processVisitPeriodRegen(
                 new WsnbIndividualVisitPrdDto.SearchProcessReq(
@@ -126,31 +123,15 @@ public class WsniSidingServiceChangesService {
             );
             if (bsTargetDvo != null) {
                 /*고객 정기BS 삭제(SP_LC_SERVICEVISIT_482_LST_I07)*/
-
-                log.debug("req.akChdt 2 : " + req.akChdt());
-                log.debug("req.akChdt.subString 2 : " + req.akChdt().substring(0, 6));
-
                 service2.removeRglrBfsvcDl(
-                    //                    new WsnbCustomerRglrBfsvcDlDto.SaveReq(
-                    //                        bsTargetDvo.getCstSvAsnNo(), //row.getCstSvAsnNo(),
-                    //                        bsTargetDvo.getAsnOjYm()
-                    //                    )
                     new WsnbCustomerRglrBfsvcDlDto.SaveReq(
                         bsTargetDvo.getCstSvAsnNo(), //row.getCstSvAsnNo(),
                         bsTargetDvo.getAsnOjYm()
                     )
                 );
                 log.debug("고객 정기BS 배정(SP_LC_SERVICEVISIT_482_LST_I03)");
-                log.debug("req.akChdt 3 : " + req.akChdt());
-                log.debug("req.akChdt.subString 3 : " + req.akChdt().substring(0, 6));
                 /*고객 정기BS 배정(SP_LC_SERVICEVISIT_482_LST_I03)*/
                 service3.processRegularBfsvcAsn(
-                    //                    new WsncRegularBfsvcAsnDto.SaveProcessReq(
-                    //                        bsTargetDvo.getAsnOjYm(), //row.getAsnOjYm(),
-                    //                        SFLEXContextHolder.getContext().getUserSession().getUserId(),
-                    //                        req.cntrNo(),
-                    //                        req.cntrSn()
-                    //                    )
                     new WsncRegularBfsvcAsnDto.SaveProcessReq(
                         req.akChdt().substring(0, 6),
                         SFLEXContextHolder.getContext().getUserSession().getUserId(),
