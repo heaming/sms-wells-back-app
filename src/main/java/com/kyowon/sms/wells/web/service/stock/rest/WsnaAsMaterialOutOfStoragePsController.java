6fd125257e8f16e800cfa245533311084165e087
jungheejin <gs.piit227@kyowon.co.kr>
2023-11-13 17:32:55 +0900

    fix: MD/AS자재/필터출고  쿼리 수정 (페이징 제거)

--- src/main/java/com/kyowon/sms/wells/web/service/stock/rest/WsnaAsMaterialOutOfStoragePsController.java
+++ src/main/java/com/kyowon/sms/wells/web/service/stock/rest/WsnaAsMaterialOutOfStoragePsController.java
@@ -13,8 +13,6 @@ import com.kyowon.sms.wells.web.service.stock.dto.WsnaAsMaterialOutOfStoragePsDt
 import com.kyowon.sms.wells.web.service.stock.dto.WsnaAsMaterialOutOfStoragePsDto.SearchRes;
 import com.kyowon.sms.wells.web.service.stock.service.WsnaAsMaterialOutOfStoragePsService;
 import com.kyowon.sms.wells.web.service.zcommon.constants.SnServiceConst;
-import com.sds.sflex.system.config.datasource.PageInfo;
-import com.sds.sflex.system.config.datasource.PagingResult;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
@@ -43,33 +41,11 @@ public class WsnaAsMaterialOutOfStoragePsController {
         @ApiImplicitParam(name = "installBase", value = "설치기준", paramType = "query"),
         @ApiImplicitParam(name = "svBizDclsfCd", value = "업무유형(서비스업무세분류코드)", paramType = "query"),
     })
-    @GetMapping("/paging")
-    public PagingResult<SearchRes> getAsMaterialOutOfStoragePsPages(
-        @Valid
-        SearchReq dto,
-        PageInfo pageInfo
-    ) {
-        return service.getAsMaterialOutOfStoragePsPages(dto, pageInfo);
-    }
-
-    @ApiOperation(value = "A/S 자재 출고 현황 엑셀 다운로드", notes = "A/S 자재 출고 현황 엑셀다운로드를 한다.")
-    @ApiImplicitParams(value = {
-        @ApiImplicitParam(name = "startDt", value = "처리시작일자", paramType = "query", required = true),
-        @ApiImplicitParam(name = "endDt", value = "처리종료일자", paramType = "query", required = true),
-        @ApiImplicitParam(name = "refriType", value = "유무상구분", paramType = "query", required = true),
-        @ApiImplicitParam(name = "serviceType", value = "서비스유형", paramType = "query"),
-        @ApiImplicitParam(name = "ogCd", value = "서비스센터", paramType = "query"),
-        @ApiImplicitParam(name = "prtnrNo", value = "엔지니어", paramType = "query"),
-        @ApiImplicitParam(name = "itmKndCd", value = "품목구분", paramType = "query"),
-        @ApiImplicitParam(name = "pdGrpCd", value = "상품그룹코드", paramType = "query"),
-        @ApiImplicitParam(name = "installBase", value = "설치기준", paramType = "query"),
-        @ApiImplicitParam(name = "svBizDclsfCd", value = "업무유형(서비스업무세분류코드)", paramType = "query"),
-    })
-    @GetMapping("/excel-download")
-    public List<SearchRes> getAsMaterialOutOfStoragePsForExcelDownload(
+    @GetMapping
+    public List<SearchRes> getAsMaterialOutOfStoragePss(
         @Valid
         SearchReq dto
     ) {
-        return service.getAsMaterialOutOfStoragePsForExcelDownload(dto);
+        return service.getAsMaterialOutOfStoragePss(dto);
     }
 }
