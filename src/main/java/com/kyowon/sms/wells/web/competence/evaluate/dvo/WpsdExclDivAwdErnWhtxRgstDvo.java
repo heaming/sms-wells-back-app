30dd6122bb8791f3c10134b788d6056cf838a7ef
gs.piit269 <gs.piit269@kyowon.co.kr>
2023-11-13 16:52:58 +0900

    feat: 시상평가관리 / 우수사업부 시상소득원천세 등록 개발

---
+++ src/main/java/com/kyowon/sms/wells/web/competence/evaluate/dvo/WpsdExclDivAwdErnWhtxRgstDvo.java
@@ -0,0 +1,17 @@
+package com.kyowon.sms.wells.web.competence.evaluate.dvo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class WpsdExclDivAwdErnWhtxRgstDvo {
+    private String perfYm;
+    private String ogTpCd;
+    private String prtnrNo;
+    private String awdDvCd;
+    private String awdPerfSn;
+    private String awdNm;
+    private String awdIntbsAmt;
+    private String awdErnWhtx;
+}
