aec6f0645ba170530cca3c728e9ffdae04bdcbb7
gs.piit231 <gs.piit231@kyowon.co.kr>
2023-11-14 09:37:16 +0900

    fix: 급지 엔지니어 출고지 관리 승인자 정보 출력 수정

--- src/main/java/com/kyowon/sms/wells/web/service/visit/mapper/WsnbTotAsRtSdingPsMapper.java
+++ src/main/java/com/kyowon/sms/wells/web/service/visit/mapper/WsnbTotAsRtSdingPsMapper.java
@@ -9,4 +9,6 @@ import java.util.List;
 @Mapper
 public interface WsnbTotAsRtSdingPsMapper {
     List<SearchRes> selectTotAsRtSdingPss(SearchReq dto);
+    List<SdingPackageRes> selectSdingPackages();
+    List<SdingDtlRes> selectSdingDtlInfos(SdingDtlReq dtlDto);
 }
