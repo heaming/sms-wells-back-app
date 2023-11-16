02080ee0609f0ffebe9a7a9695ec4c6b6f6c6f29
조혜미 <hey._.mi@kyowon.co.kr>
2023-11-16 09:33:48 +0900

    feat: 일자별 설치재고 현황 추가 페이지 * wsna-installation-stock-ps-by-day.xml * WsnaInstallationStockPsByDayCenterDvo.java * WsnaInstallationStockPsByDayController.java * WsnaInstallationStockPsByDayDto.java * WsnaInstallationStockPsByDayMapper.java * WsnaInstallationStockPsByDayPdDvo.java * WsnaInstallationStockPsByDayService.java

--- src/main/java/com/kyowon/sms/wells/web/service/stock/dto/WsnaInstallationStockPsByDayDto.java
+++ src/main/java/com/kyowon/sms/wells/web/service/stock/dto/WsnaInstallationStockPsByDayDto.java
@@ -20,11 +20,12 @@ public class WsnaInstallationStockPsByDayDto {
         String baseDt,
         String pdGdCd,
         String itmKndCd,
-        String pdCd
+        String pdCd,
+        String svCnr
     ) {}
 
-    @ApiModel(value = "WsnaInstallationStockPsByDayDto-SearchRes")
-    public record SearchRes(
+    @ApiModel(value = "WsnaInstallationStockPsByDayDto-SearchResCenter")
+    public record SearchResCenter(
         String ogId,
         String ogNm,
         String pdCd,
@@ -32,6 +33,46 @@ public class WsnaInstallationStockPsByDayDto {
         Integer prvMng,
         Integer engQty,
         Integer stockTotal,
+        Integer stockdate1,
+        Integer stockdate2,
+        Integer stockdate3,
+        Integer stockdate4,
+        Integer stockdate5,
+        Integer stockdate6,
+        Integer stockdate7,
+        Integer stockdate8,
+        Integer stockdate9,
+        Integer stockdate10,
+        Integer stockdate11,
+        Integer stockdate12,
+        Integer stockdate13,
+        Integer stockdate14,
+        Integer installdate1,
+        Integer installdate2,
+        Integer installdate3,
+        Integer installdate4,
+        Integer installdate5,
+        Integer installdate6,
+        Integer installdate7,
+        Integer installdate8,
+        Integer installdate9,
+        Integer installdate10,
+        Integer installdate11,
+        Integer installdate12,
+        Integer installdate13,
+        Integer installdate14,
+        Integer istTotal
+    ) {}
+
+    @ApiModel(value = "WsnaInstallationStockPsByDayDto-SearchResPd")
+    public record SearchResPd(
+        String pdCd,
+        String pdNm,
+        Integer pajuQty,
+        Integer centerQty,
+        Integer engQty,
+        Integer eotQty,
+        Integer stocQty,
         Integer stockdate1,
         Integer stockdate2,
         Integer stockdate3,
--- src/main/java/com/kyowon/sms/wells/web/service/stock/rest/WsnaInstallationStockPsByDayController.java
+++ src/main/java/com/kyowon/sms/wells/web/service/stock/rest/WsnaInstallationStockPsByDayController.java
@@ -10,7 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kyowon.sms.wells.web.service.stock.dto.WsnaInstallationStockPsByDayDto.SearchReq;
-import com.kyowon.sms.wells.web.service.stock.dto.WsnaInstallationStockPsByDayDto.SearchRes;
+import com.kyowon.sms.wells.web.service.stock.dto.WsnaInstallationStockPsByDayDto.SearchResCenter;
+import com.kyowon.sms.wells.web.service.stock.dto.WsnaInstallationStockPsByDayDto.SearchResPd;
 import com.kyowon.sms.wells.web.service.stock.service.WsnaInstallationStockPsByDayService;
 import com.kyowon.sms.wells.web.service.zcommon.constants.SnServiceConst;
 import com.sds.sflex.system.config.datasource.PageInfo;
@@ -29,21 +30,39 @@ public class WsnaInstallationStockPsByDayController {
 
     private final WsnaInstallationStockPsByDayService service;
 
-    @ApiOperation(value = "일자별 설치재고 현황 조회", notes = "조회일자를 기준으로 14일 범위의 일자별 설치재고 집계 현황을 조회한다.")
-    @GetMapping("/paging")
-    public PagingResult<SearchRes> getReturningGoodsOstrAgrg(
+    @ApiOperation(value = "일자별 설치재고 현황 조회", notes = "조회일자, 서비스센터를 기준으로 14일 범위의 일자별 설치재고 집계 현황을 조회한다.")
+    @GetMapping("/center/paging")
+    public PagingResult<SearchResCenter> getInstallationStockPsByDayCenter(
         @Valid
         SearchReq dto, PageInfo pageInfo
     ) {
-        return service.getInstallationStockPsByDay(dto, pageInfo);
+        return service.getInstallationStockPsByDayCenter(dto, pageInfo);
     }
 
-    @ApiOperation(value = "일자별 설치재고 현황 엑셀다운로드", notes = "조회일자를 기준으로 14일 범위의 일자별 설치재고 집계 현황을 엑셀 다운로드한다.")
-    @GetMapping("/excel-download")
-    public List<SearchRes> getReturningGoodsOstrAgrg(
+    @ApiOperation(value = "일자별 설치재고 현황 엑셀다운로드", notes = "조회일자, 서비스센터를 기준으로 14일 범위의 일자별 설치재고 집계 현황을 엑셀 다운로드한다.")
+    @GetMapping("/center/excel-download")
+    public List<SearchResCenter> getInstallationStockPsByDayCenter(
         @Valid
         SearchReq dto
     ) {
-        return service.getInstallationStockPsByDay(dto);
+        return service.getInstallationStockPsByDayCenter(dto);
+    }
+
+    @ApiOperation(value = "일자별 설치재고 현황 조회", notes = "조회일자, 품목 기준으로 14일 범위의 일자별 설치재고 집계 현황을 조회한다.")
+    @GetMapping("/product/paging")
+    public PagingResult<SearchResPd> getInstallationStockPsByDayPd(
+        @Valid
+        SearchReq dto, PageInfo pageInfo
+    ) {
+        return service.getInstallationStockPsByDayPd(dto, pageInfo);
+    }
+
+    @ApiOperation(value = "일자별 설치재고 현황 엑셀다운로드", notes = "조회일자, 품목 기준으로 14일 범위의 일자별 설치재고 집계 현황을 엑셀 다운로드한다.")
+    @GetMapping("/product/excel-download")
+    public List<SearchResPd> getInstallationStockPsByDayPd(
+        @Valid
+        SearchReq dto
+    ) {
+        return service.getInstallationStockPsByDayPd(dto);
     }
 }
