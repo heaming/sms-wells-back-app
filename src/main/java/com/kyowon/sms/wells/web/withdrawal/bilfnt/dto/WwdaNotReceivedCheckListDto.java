77fb86e1960bd833fadc01bc7a481fb19c326c8e
gs.piit160 <gs.piit160@kyowon.co.kr>
2023-11-17 11:18:30 +0900

    fix: wells 결함 조치

--- src/main/java/com/kyowon/sms/wells/web/withdrawal/bilfnt/dto/WwdaNotReceivedCheckListDto.java
+++ src/main/java/com/kyowon/sms/wells/web/withdrawal/bilfnt/dto/WwdaNotReceivedCheckListDto.java
@@ -9,83 +9,45 @@ import io.swagger.annotations.ApiModel;
 
 public class WwdaNotReceivedCheckListDto {
 
-    @ApiModel("WwdaNotReceivedCheckListDto-SearchAftnBilNrcvListReq")
-    public record SearchAftnBilNrcvListReq(
-        String bilDt,
-        String fntDvCd
-    ) {}
+	@ApiModel("WwdaNotReceivedCheckListDto-SearchAftnBilNrcvListReq")
+	public record SearchAftnBilNrcvListReq(String bilDt, String fntDvCd) {
+	}
 
-    @ApiModel("WwdaNotReceivedCheckListDto-SearchAftnBilNrcvListRes")
-    public record SearchAftnBilNrcvListRes(
-        String autoFntClsf,
-        String bnkCd,
-        String bnkNm,
-        String ct
-    ) {}
+	@ApiModel("WwdaNotReceivedCheckListDto-SearchAftnBilNrcvListRes")
+	public record SearchAftnBilNrcvListRes(String autoFntClsf, String bnkCd, String bnkNm, String ct) {
+	}
 
-    @ApiModel("WwdaNotReceivedCheckListDto-SearchAftnDpNcrtCheckListReq")
-    public record SearchAftnDpNcrtCheckListReq(
-        String bilDt,
-        String fntDvCd
-    ) {}
+	@ApiModel("WwdaNotReceivedCheckListDto-SearchAftnDpNcrtCheckListReq")
+	public record SearchAftnDpNcrtCheckListReq(String bilDt, String fntDvCd) {
+	}
 
-    @ApiModel("WwdaNotReceivedCheckListDto-SearchAftnDpNcrtCheckListRes")
-    public record SearchAftnDpNcrtCheckListRes(
-        // 자동이체 입금 미생성 체크 목록
-        String aftnClsf,
-        String cntrNo,
-        String cntrSn,
-        String bilDt,
-        String sellTpCd,
-        String dpAmt
-    ) {}
+	@ApiModel("WwdaNotReceivedCheckListDto-SearchAftnDpNcrtCheckListRes")
+	public record SearchAftnDpNcrtCheckListRes(
+			// 자동이체 입금 미생성 체크 목록
+			String aftnClsf, String cntrNo, String cntrSn, String bilDt, String sellTpCd, String rveAmt) {
+	}
 
-    @ApiModel("WwdaNotReceivedCheckListDto-SearchAftnSlPerfCheckInqrRes")
-    public record SearchAftnSlPerfCheckInqrRes(
-        // 매출실적 입금 전용 누락건
-        String sellTpNm,
-        String baseYm,
-        String cntrNo,
-        String cntrSn,
-        String cntr,
-        String rentalNmn,
-        String thmIntamDpAmt,
-        String thmIstmRfndAmt,
-        String rveAmt,
-        String dpMesCd
-    ) {
-//        public SearchAftnSlPerfCheckInqrRes {
-//            if (!StringUtil.isEmpty(cntrNo) && !StringUtil.isEmpty(cntrSn)) {
-//                cntr = cntrNo + "-" + cntrSn;
-//            }
-//        }
-    }
+	@ApiModel("WwdaNotReceivedCheckListDto-SearchAftnSlPerfCheckInqrRes")
+	public record SearchAftnSlPerfCheckInqrRes(
+			// 매출실적 입금 전용 누락건
+			String sellTpNm, String baseYm, String cntrNo, String cntrSn, String cntr, String rentalNmn,
+			String thmIntamDpAmt, String thmIstmRfndAmt, String rveAmt, String dpMesCd) {
+	}
 
-    @ApiModel("WwdaNotReceivedCheckListDto-SearchResultBundleErrorRes")
-    public record SearchResultBundleErrorRes(
-        // 묶음 출금 오등록
-        String mstrSellTpCd,
-        String dgCntrNo,
-        String mstrDpTpCd,
-        String subSellTpCd,
-        String subodCntrNo,
-        String subDpTpCd,
-        String errTyp,
-        @MaskRequired(type = MaskingType.ACCOUNT)
-        String mAcnoEncr,
-        @MaskRequired(type = MaskingType.ACCOUNT)
-        String sAcnoEncr,
-        @MaskRequired(type = MaskingType.CARD)
-        String mCrcdnoEncr,
-        @MaskRequired(type = MaskingType.CARD)
-        String sCrcdnoEncr
-    ) {
-        public SearchResultBundleErrorRes {
-            mAcnoEncr = StringUtil.isNotEmpty(mAcnoEncr) ? DbEncUtil.dec(mAcnoEncr) : mAcnoEncr;
-            sAcnoEncr = StringUtil.isNotEmpty(sAcnoEncr) ? DbEncUtil.dec(sAcnoEncr) : sAcnoEncr;
-            mCrcdnoEncr = StringUtil.isNotEmpty(mCrcdnoEncr) ? DbEncUtil.dec(mCrcdnoEncr) : mCrcdnoEncr;
-            sCrcdnoEncr = StringUtil.isNotEmpty(sCrcdnoEncr) ? DbEncUtil.dec(sCrcdnoEncr) : sCrcdnoEncr;
-        }
-    }
+	@ApiModel("WwdaNotReceivedCheckListDto-SearchResultBundleErrorRes")
+	public record SearchResultBundleErrorRes(
+			// 묶음 출금 오등록
+			String mstrSellTpCd, String dgCntrNo, String mstrDpTpCd, String subSellTpCd, String subodCntrNo,
+			String subDpTpCd, String errTyp, @MaskRequired(type = MaskingType.ACCOUNT) String mAcnoEncr,
+			@MaskRequired(type = MaskingType.ACCOUNT) String sAcnoEncr,
+			@MaskRequired(type = MaskingType.CARD) String mCrcdnoEncr,
+			@MaskRequired(type = MaskingType.CARD) String sCrcdnoEncr) {
+		public SearchResultBundleErrorRes {
+			mAcnoEncr = StringUtil.isNotEmpty(mAcnoEncr) ? DbEncUtil.dec(mAcnoEncr) : mAcnoEncr;
+			sAcnoEncr = StringUtil.isNotEmpty(sAcnoEncr) ? DbEncUtil.dec(sAcnoEncr) : sAcnoEncr;
+			mCrcdnoEncr = StringUtil.isNotEmpty(mCrcdnoEncr) ? DbEncUtil.dec(mCrcdnoEncr) : mCrcdnoEncr;
+			sCrcdnoEncr = StringUtil.isNotEmpty(sCrcdnoEncr) ? DbEncUtil.dec(sCrcdnoEncr) : sCrcdnoEncr;
+		}
+	}
 
 }
