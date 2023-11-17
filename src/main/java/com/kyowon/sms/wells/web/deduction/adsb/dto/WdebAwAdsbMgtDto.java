be0536057fb2d5f14473c4d09ae45c2a69d86dfd
gs.piit161 <gs.piit161@kyowon.com>
2023-11-17 16:57:22 +0900

    fix: 공제대체 수정

--- src/main/java/com/kyowon/sms/wells/web/deduction/adsb/dto/WdebAwAdsbMgtDto.java
+++ src/main/java/com/kyowon/sms/wells/web/deduction/adsb/dto/WdebAwAdsbMgtDto.java
@@ -9,7 +9,8 @@ public class WdebAwAdsbMgtDto {
     @ApiModel("WdebAwAdsbMgtDto-CreateReq")
     public record CreateReq(
         String baseYm, // 반영년월
-        String ogTpCd // 조직유형
+        String ogTpCd, // 조직유형
+        String lifeYn // 상조 여부
     ) {}
 
 }
