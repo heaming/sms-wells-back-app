20b7f7d5a88bf273c158dba94fe4cbef93891e66
gs.piit122 <kdycorp@kyowon.co.kr>
2023-11-17 17:28:22 +0900

    feat: 홈카드 처리현황 파일 추가

---
+++ src/main/java/com/kyowon/sms/wells/web/service/common/mapper/WsnyProcPsMapper.java
@@ -0,0 +1,23 @@
+package com.kyowon.sms.wells.web.service.common.mapper;
+
+import com.kyowon.sms.wells.web.service.common.dto.WsnyProcPsDto.*;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * <pre>
+ * [W-SV-U-1301M01] 홈카드 - 처리 현황 Mapper
+ * <pre>
+ *
+ * @author 김동엽
+ * @since 2023.11.16
+ */
+@Mapper
+public interface WsnyProcPsMapper {
+    /**
+     * 홈카드 처리 현황 조회
+     * @param dto
+     * @return
+     */
+    SearchRes selectProcPs(SearchReq dto);
+}
---
+++ src/main/java/com/kyowon/sms/wells/web/service/common/service/WsnyProcPsService.java
@@ -0,0 +1,30 @@
+package com.kyowon.sms.wells.web.service.common.service;
+
+import com.kyowon.sms.wells.web.service.common.dto.WsnyProcPsDto.*;
+import com.kyowon.sms.wells.web.service.common.mapper.WsnyProcPsMapper;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * <pre>
+ * [W-SV-U-1301M01] 홈카드 - 처리 현황 Service
+ * <pre>
+ *
+ * @author 김동엽
+ * @since 2023.11.16
+ */
+@Service
+@RequiredArgsConstructor
+public class WsnyProcPsService {
+    private final WsnyProcPsMapper mapper;
+
+    /**
+     * 홈카드 - 처리 현황 조회
+     * @param dto
+     * @return
+     */
+    public SearchRes selectProcPs(SearchReq dto) {
+        return mapper.selectProcPs(dto);
+    }
+}
