f9e9dc1a6a6a7beec88730f51d1b0cf9ca9e8ea8
gilyong.han <gs.piit128@kyowon.co.kr>
2023-11-21 18:08:50 +0900

    fix: 위탁이관 설계 반영

--- src/main/java/com/kyowon/sms/wells/web/bond/transfer/dto/WbnaFosterTransferMgtDto.java
+++ src/main/java/com/kyowon/sms/wells/web/bond/transfer/dto/WbnaFosterTransferMgtDto.java
@@ -42,6 +42,7 @@ public class WbnaFosterTransferMgtDto {
         String clctamPrtnrNo, /* 집금담당자번호 */
         Double woCstCt, /* 전체 - 고객수 */
         Double woCntrCt, /* 전체 - 계약수 */
+        Double woUcAmt, /* 전체 - 미수금 */
         Double woObjAmt, /* 전체 - 당월대상 */
         Double woDlqAmt, /* 전체 - 연체금액 */
         Double woThmChramAmt, /* 전체 - 당월요금 */
@@ -49,6 +50,7 @@ public class WbnaFosterTransferMgtDto {
         Double woRsgBorAmt, /* 전체 - 위약금액 */
         Double rentalCstCt, /* 렌탈 - 고객수 */
         Double rentalCntrCt, /* 렌탈 - 계약수 */
+        Double rentalUcAmt, /* 렌탈 - 미수금 */
         Double rentalObjAmt, /* 렌탈 - 당월대상 */
         Double rentalDlqAmt, /* 렌탈 - 연체금액 */
         Double rentalThmChramAmt, /* 렌탈 - 당월요금 */
@@ -56,6 +58,7 @@ public class WbnaFosterTransferMgtDto {
         Double rentalRsgBorAmt, /* 렌탈 - 위약금액 */
         Double leaseCstCt, /* 금융리스 - 고객수 */
         Double leaseCntrCt, /* 금융리스 - 계약수 */
+        Double leaseUcAmt, /* 금융리스 - 미수금 */
         Double leaseObjAmt, /* 금융리스 - 당월대상 */
         Double leaseDlqAmt, /* 금융리스 - 연체금액 */
         Double leaseThmChramAmt, /* 금융리스 - 당월요금 */
@@ -63,6 +66,7 @@ public class WbnaFosterTransferMgtDto {
         Double leaseRsgBorAmt, /* 금융리스 - 위약금액 */
         Double mshCstCt, /* 일반멤버십 - 고객수 */
         Double mshCntrCt, /* 일반멤버십 - 계약수 */
+        Double mshUcAmt, /* 일반멤버십 - 미수금 */
         Double mshObjAmt, /* 일반멤버십 - 당월대상 */
         Double mshDlqAmt, /* 일반멤버십 - 연체금액 */
         Double mshThmChramAmt, /* 일반멤버십 - 당월요금 */
@@ -70,6 +74,7 @@ public class WbnaFosterTransferMgtDto {
         Double mshRsgBorAmt, /* 일반멤버십 - 위약금액 */
         Double rglrSppCstCt, /* 홈케어멤버십 - 고객수 */
         Double rglrSppCntrCt, /* 홈케어멤버십 - 계약수 */
+        Double rglrSppUcAmt, /* 홈케어멤버십 - 미수금 */
         Double rglrSppObjAmt, /* 홈케어멤버십 - 당월대상 */
         Double rglrSppDlqAmt, /* 홈케어멤버십 - 연체금액 */
         Double rglrSppThmChramAmt, /* 홈케어멤버십 - 당월요금 */
@@ -77,6 +82,7 @@ public class WbnaFosterTransferMgtDto {
         Double rglrSppRsgBorAmt, /* 홈케어멤버십 - 위약금액 */
         Double hcrCstCt, /* 일시불 - 고객수 */
         Double hcrCntrCt, /* 일시불 - 계약수 */
+        Double hcrUcAmt, /* 일시불 - 미수금 */
         Double hcrObjAmt, /* 일시불 - 당월대상 */
         Double hcrDlqAmt, /* 일시불 - 연체금액 */
         Double hcrThmChramAmt, /* 일시불 - 당월요금 */
@@ -84,6 +90,7 @@ public class WbnaFosterTransferMgtDto {
         Double hcrRsgBorAmt, /* 일시불 - 위약금액 */
         Double spayCstCt, /* 정기배송 - 고객수 */
         Double spayCntrCt, /* 정기배송 - 계약수 */
+        Double spayUcAmt, /* 정기배송 - 미수금 */
         Double spayObjAmt, /* 정기배송 - 당월대상 */
         Double spayDlqAmt, /* 정기배송 - 연체금액 */
         Double spayThmChramAmt, /* 정기배송 - 당월요금 */
@@ -104,11 +111,14 @@ public class WbnaFosterTransferMgtDto {
         String cstNo, /* 고객번호  */
         String pdNm, /* 상품구분 -- 계약번호 + 계약상세번호 로 상품정보 조회 */
         String dlqMcn, /* 연체개월 */
+        Double ucAmt, /* 미수금 */
         Double trgAmt, /* 대상금액 */
         Double dlqAmt, /* 연체금액 */
         Double thmChramAmt, /* 당월요금 */
         Double dlqAddAmt, /* 연체가산금 */
         Double rsgBorAmt, /* 위약금액 */
+        String bndClctnPrpDvNm, /* 속성 */
+        String bndClctnPrpRsonNm, /* 사유 */
         String lwmTp, /* 법조치유형 */
         String lwmDtlTpCd, /* 법조치상세 */
         String lwmDt, /* 법조치일자 */
@@ -121,6 +131,7 @@ public class WbnaFosterTransferMgtDto {
 
     @ApiModel("WbnaFosterTransferMgtDto-SearchDetailSummaryRes")
     public record SearchDetailSummaryRes(
+        String ucAmt, /*총 미수금*/
         String trgAmt, /*총 대상금액*/
         String dlqAmt, /*총 연체금액*/
         String thmChramAmt, /*총 당월요금*/
