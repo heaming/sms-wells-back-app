d0afb130ff48b2a39e6b71ba335b60ced44344bd
dagyeong.hong <gs.piit268@kyowon.co.kr>
2023-11-22 16:22:05 +0900

    fix: 입금내역관리 수정

--- src/main/java/com/kyowon/sms/wells/web/withdrawal/idvrve/mapper/WwdbDepositDetailMapper.java
+++ src/main/java/com/kyowon/sms/wells/web/withdrawal/idvrve/mapper/WwdbDepositDetailMapper.java
@@ -5,16 +5,15 @@ import java.util.List;
 import com.kyowon.sms.wells.web.withdrawal.idvrve.dvo.WwdbDepositDetailSearchDvo;
 import org.apache.ibatis.annotations.Mapper;
 
-import com.kyowon.sms.wells.web.withdrawal.idvrve.dto.WwdbDepositDetailDto.SearchReq;
-import com.kyowon.sms.wells.web.withdrawal.idvrve.dto.WwdbDepositDetailDto.SearchRes;
+import com.kyowon.sms.wells.web.withdrawal.idvrve.dto.WwdbDepositDetailDto.*;
 import com.sds.sflex.system.config.datasource.PageInfo;
 import com.sds.sflex.system.config.datasource.PagingResult;
 
 @Mapper
 public interface WwdbDepositDetailMapper {
 
-    PagingResult<SearchRes> selectDepositDetail(WwdbDepositDetailSearchDvo dVo, PageInfo pageInfo);
+    PagingResult<SearchRes> selectDepositDetail(SearchReq dto, PageInfo pageInfo);
 
-    List<SearchRes> selectDepositDetail(WwdbDepositDetailSearchDvo dVo);
+    List<WwdbDepositDetailSearchDvo> selectDepositDetailBulk(SearchReq dto);
 
 }
