af6a6cd8f74cfdfaab9c1d08556aad7422421ee0
gs.piit238 <gs.piit238@kyowon.co.kr>
2023-11-29 16:18:02 +0900

    chore : BS소모품 배부기준 수정
    * 주석 작성, 소스 정리

--- src/main/java/com/kyowon/sms/wells/web/service/stock/converter/WsnaBsCsmbDeliveryBaseConverter.java
+++ src/main/java/com/kyowon/sms/wells/web/service/stock/converter/WsnaBsCsmbDeliveryBaseConverter.java
@@ -2,16 +2,14 @@ package com.kyowon.sms.wells.web.service.stock.converter;
 
 import org.mapstruct.Mapper;
 
-import com.kyowon.sms.wells.web.service.stock.dto.WsnaBsCsmbDeliveryBaseDto.CreateCrdovrReq;
 import com.kyowon.sms.wells.web.service.stock.dto.WsnaBsCsmbDeliveryBaseDto.CreateReq;
 import com.kyowon.sms.wells.web.service.stock.dto.WsnaBsCsmbDeliveryBaseDto.EditReq;
 import com.kyowon.sms.wells.web.service.stock.dvo.WsnaBsCsmbDeliveryBaseDvo;
 
 @Mapper(componentModel = "spring")
 public interface WsnaBsCsmbDeliveryBaseConverter {
-    WsnaBsCsmbDeliveryBaseDvo mapCreateReqToDeliveryBaseDto(CreateReq dto);
+    WsnaBsCsmbDeliveryBaseDvo mapCreateReqToDeliveryBaseDvo(CreateReq dto);
 
-    WsnaBsCsmbDeliveryBaseDvo mapEditReqToDeliveryBaseDto(EditReq dto);
+    WsnaBsCsmbDeliveryBaseDvo mapEditReqToDeliveryBaseDvo(EditReq dto);
 
-    WsnaBsCsmbDeliveryBaseDvo mapCreateCrdovrReqToDeliveryBaseDto(CreateCrdovrReq dto);
 }
