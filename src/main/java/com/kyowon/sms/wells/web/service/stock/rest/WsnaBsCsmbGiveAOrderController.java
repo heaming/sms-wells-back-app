00ebef9578a831a5b5a0602f690c75d091f535d6
gs.piit238 <gs.piit238@kyowon.co.kr>
2023-11-30 17:00:52 +0900

    chore : BS소모품 발주수량 산출 수정
    * 주석작성, 소스정리

--- src/main/java/com/kyowon/sms/wells/web/service/stock/rest/WsnaBsCsmbGiveAOrderController.java
+++ src/main/java/com/kyowon/sms/wells/web/service/stock/rest/WsnaBsCsmbGiveAOrderController.java
@@ -3,20 +3,31 @@ package com.kyowon.sms.wells.web.service.stock.rest;
 import java.util.List;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
 
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import com.kyowon.sms.wells.web.service.stock.dto.WsnaBsCsmbGiveAOrderDto;
-import com.kyowon.sms.wells.web.service.stock.dto.WsnaBsCsmbGiveAOrderDto.CreatReq;
+import com.kyowon.sms.wells.web.service.stock.dto.WsnaBsCsmbGiveAOrderDto.CreateReq;
+import com.kyowon.sms.wells.web.service.stock.dto.WsnaBsCsmbGiveAOrderDto.ProdutCodeRes;
 import com.kyowon.sms.wells.web.service.stock.dto.WsnaBsCsmbGiveAOrderDto.SearchReq;
-import com.kyowon.sms.wells.web.service.stock.dto.WsnaBsCsmbGiveAOrderDto.SearchRes;
+import com.kyowon.sms.wells.web.service.stock.dvo.WsnaBsCsmbGiveAOrderDvo;
 import com.kyowon.sms.wells.web.service.stock.service.WsnaBsCsmbGiveAOrderService;
 import com.kyowon.sms.wells.web.service.zcommon.constants.SnServiceConst;
 import com.sds.sflex.system.config.response.SaveResponse;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+
+/**
+ * <pre>
+ * W-SV-U-0014M01 BS소모품 발주수량 산출 Controller
+ * </pre>
+ *
+ * @author SaeRomI.Kim
+ * @since 2023-11-30
+ */
 
 @Api(tags = "[WSNA] BS소모품 발주수량 산출")
 @Validated
@@ -27,27 +38,33 @@ public class WsnaBsCsmbGiveAOrderController {
 
     private final WsnaBsCsmbGiveAOrderService service;
 
+    @GetMapping("/{itmKndCd}/product-codes")
+    @ApiOperation(value = "품목 조회", notes = "품목종류에 해당하는 품목을 조회한다.")
+    public List<ProdutCodeRes> getProductCodesByItmKndCd(
+        @PathVariable
+        String itmKndCd
+    ) {
+        return service.getProductCodesByItmKndCd(itmKndCd);
+    }
+
     @GetMapping
-    public List<SearchRes> getBsCsmbGiveAOrderQty(SearchReq dto) {
+    @ApiOperation(value = "BS소모품 발주수량 산출 조회", notes = "BS소모품 발주수량 산출 내역을 조회한다.")
+    public List<WsnaBsCsmbGiveAOrderDvo> getBsCsmbGiveAOrderQty(@Valid
+    SearchReq dto) {
         return service.getBsCsmbGiveAOrderQty(dto);
     }
 
     @PostMapping
+    @ApiOperation(value = "BS소모품 발주수량 산출 저장", notes = "BS소모품 발주수량 산출 내역을 저장한다.")
     public SaveResponse createBsCsmbGiveAOrderQty(
         @RequestBody
         @Valid
-        List<CreatReq> dtos
+        @NotEmpty
+        List<CreateReq> dtos
     ) {
         return SaveResponse.builder()
             .processCount(service.createBsCsmbGiveAOrderQty(dtos))
             .build();
     }
 
-    @GetMapping("/{itmKndCd}/product-codes")
-    public List<WsnaBsCsmbGiveAOrderDto.ProdutCodeRes> getProductCodesByItmKndCd(
-        @PathVariable
-        String itmKndCd
-    ) {
-        return service.getProductCodesByItmKndCd(itmKndCd);
-    }
 }
--- src/main/java/com/kyowon/sms/wells/web/service/stock/service/WsnaBsCsmbGiveAOrderService.java
+++ src/main/java/com/kyowon/sms/wells/web/service/stock/service/WsnaBsCsmbGiveAOrderService.java
@@ -1,27 +1,38 @@
 package com.kyowon.sms.wells.web.service.stock.service;
 
+import java.math.BigDecimal;
 import java.text.ParseException;
 import java.util.List;
 
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.ObjectUtils;
 
 import com.kyowon.sms.wells.web.service.stock.converter.WsnaBsCsmbGiveAOrderConverter;
-import com.kyowon.sms.wells.web.service.stock.dto.WsnaBsCsmbGiveAOrderDto.CreatReq;
+import com.kyowon.sms.wells.web.service.stock.dto.WsnaBsCsmbGiveAOrderDto.CreateReq;
 import com.kyowon.sms.wells.web.service.stock.dto.WsnaBsCsmbGiveAOrderDto.ProdutCodeRes;
 import com.kyowon.sms.wells.web.service.stock.dto.WsnaBsCsmbGiveAOrderDto.SearchReq;
-import com.kyowon.sms.wells.web.service.stock.dto.WsnaBsCsmbGiveAOrderDto.SearchRes;
 import com.kyowon.sms.wells.web.service.stock.dvo.WsnaBsCsmbGiveAOrderDvo;
 import com.kyowon.sms.wells.web.service.stock.ivo.EAI_CBDO1007.response.RealTimeGradeStockResIvo;
 import com.kyowon.sms.wells.web.service.stock.mapper.WsnaBsCsmbGiveAOrderMapper;
 import com.sds.sflex.common.utils.DateUtil;
+import com.sds.sflex.system.config.exception.BizException;
 
 import lombok.RequiredArgsConstructor;
+
+/**
+ * <pre>
+ * W-SV-U-0014M01 BS소모품 발주수량 산출 서비스
+ * </pre>
+ *
+ * @author SaeRomI.Kim
+ * @since 2023-11-30
+ */
 
 @Service
 @RequiredArgsConstructor
 public class WsnaBsCsmbGiveAOrderService {
+
     private final WsnaBsCsmbGiveAOrderMapper mapper;
     private final WsnaBsCsmbGiveAOrderConverter converter;
     private final WsnaItemStockItemizationService stockService;
@@ -32,190 +43,173 @@ public class WsnaBsCsmbGiveAOrderService {
     // 성수창고
     private static final String SGSU_SAP_SAVE_LCT_CD = "21082090";
 
-    public List<SearchRes> getBsCsmbGiveAOrderQty(SearchReq dto) {
-        if (Integer.parseInt(DateUtil.getNowDayString().substring(0, 4)) <= Integer.parseInt(dto.mngtYm())) {
-            List<WsnaBsCsmbGiveAOrderDvo> dvos = this.getBeforeBsCsmbGiveAOrderQty(dto);
+    /**
+    * 품목 조회
+    * @param itmKndCd
+    * @return
+    */
+    public List<ProdutCodeRes> getProductCodesByItmKndCd(String itmKndCd) {
+        return mapper.selectProductCodesByItmKndCd(itmKndCd);
+    }
 
-            return converter.listDvoToBsCsmbGiveAOrderDto(dvos);
+    /**
+     * BS소모품 발주수량 산출 조회
+     * @param dto
+     * @return
+     */
+    public List<WsnaBsCsmbGiveAOrderDvo> getBsCsmbGiveAOrderQty(SearchReq dto) {
+        String curDt = DateUtil.getNowDayString();
+
+        // 현재년월 <= 관리년월
+        if (Integer.parseInt(curDt.substring(0, 6)) <= Integer.parseInt(dto.mngtYm())) {
+            return this.getBeforeBsCsmbGiveAOrderQty(dto);
         } else {
-            List<WsnaBsCsmbGiveAOrderDvo> dvos = mapper.selectBsCsmbGiveAOrderQty(dto);
-
-            return converter.listDvoToBsCsmbGiveAOrderDto(dvos);
+            // 관리년월이 현재년월 이전인 경우 저장된 데이터로 보여줌
+            return mapper.selectBsCsmbGiveAOrderQty(dto);
         }
     }
 
+    /**
+     * 이전 BS소모품 발주수량 산출내역 조회
+     * @param dto
+     * @return
+     */
     private List<WsnaBsCsmbGiveAOrderDvo> getBeforeBsCsmbGiveAOrderQty(SearchReq dto) {
+
+        String baseDay = DateUtil.getNowDayString();
+        String nowMonth = DateUtil.getNowDayString().substring(0, 6);
+        // 관리년월
+        String mngtYm = dto.mngtYm();
+        if (!nowMonth.equals(mngtYm)) {
+            baseDay = mngtYm + "01";
+        }
+
+        // 성수재고 (0: 포함, 1: 제외)
+        String excludeYn = dto.sgsuExcludeYn();
+
         List<WsnaBsCsmbGiveAOrderDvo> dvos = mapper.selectBeforeBsCsmbGiveAOrderQty(dto);
 
-        // 파주재고 set
+        // 품목 리스트
         List<String> itmPds = dvos.stream().map(WsnaBsCsmbGiveAOrderDvo::getCsmbPdCd).toList();
+        if (CollectionUtils.isNotEmpty(itmPds)) {
 
-        if (!ObjectUtils.isEmpty(itmPds)) {
+            // 파주 재고 조회
             List<RealTimeGradeStockResIvo> pajuStocks = stockService
                 .getRealTimeGradeStocks(SAP_PLNT_CD, PAJU_SAP_SAVE_LCT_CD, itmPds);
 
             dvos.forEach(dvo -> {
                 pajuStocks.forEach(stock -> {
                     if (dvo.getCsmbPdCd().equals(stock.getItmPdCd()) && "N".equals(dvo.getRgstYn())) {
-                        int pajuLgstCnrStocQty = stock.getLgstAGdQty().intValue() + stock.getLgstBGdQty().intValue()
-                            + stock.getLgstCGdQty().intValue() + stock.getLgstEGdQty().intValue()
-                            + stock.getLgstRGdQty().intValue();
 
-                        dvo.setPajuLgstCnrStocQty(pajuLgstCnrStocQty);
+                        // A등급 재고만 셋팅
+                        dvo.setPajuLgstCnrStocQty(stock.getLgstAGdQty());
                     }
                 });
             });
 
-            if (!"1".equals(dto.sgsuExcludeYn())) { // 성수재고 제외가 아니면
-                // 성수재고 set
+            // 성수재고 포함
+            if ("1".equals(excludeYn)) {
                 List<RealTimeGradeStockResIvo> sgsuStocks = stockService
                     .getRealTimeGradeStocks(SAP_PLNT_CD, SGSU_SAP_SAVE_LCT_CD, itmPds);
 
                 dvos.forEach(dvo -> {
                     sgsuStocks.forEach(stock -> {
                         if (dvo.getCsmbPdCd().equals(stock.getItmPdCd()) && "N".equals(dvo.getRgstYn())) {
-                            int sgsuLgstCnrStocQty = stock.getLgstAGdQty().intValue() + stock.getLgstBGdQty().intValue()
-                                + stock.getLgstCGdQty().intValue() + stock.getLgstEGdQty().intValue()
-                                + stock.getLgstRGdQty().intValue();
-
-                            dvo.setSgsuLgstCnrStocQty(sgsuLgstCnrStocQty);
+                            // A등급만 셋팅
+                            dvo.setSgsuLgstCnrStocQty(stock.getLgstAGdQty());
                         }
                     });
                 });
             }
         }
 
-        // 최종재고 set
-        dvos.forEach(dvo -> {
-            if ("N".equals(dvo.getRgstYn())) {
-                dvo.setWoStocQty(dvo.getPajuLgstCnrStocQty() + dvo.getSgsuLgstCnrStocQty());
-            }
-        });
+        for (WsnaBsCsmbGiveAOrderDvo dvo : dvos) {
+            // 등록여부
+            String rgstYn = dvo.getRgstYn();
+            if ("N".equals(rgstYn)) {
+                // 전체 재고 = 입고대기 + 파주재고 + 성수재고
+                BigDecimal woStocQty = dvo.getPajuLgstCnrStocQty().add(dvo.getSgsuLgstCnrStocQty())
+                    .add(dvo.getStrStnbQty());
+                // 월평균 배부수량
+                BigDecimal mmAvDdlvQty = dvo.getMmAvDdlvQty();
+                // 전체재고 셋팅
+                dvo.setWoStocQty(woStocQty);
 
-        // 재고지속월 set
-        dvos.forEach(dvo -> {
-            if ("N".equals(dvo.getRgstYn())) {
-                if (dvo.getWoStocQty() == 0 || dvo.getMmAvDdlvQty() == 0) {
+                // 재고지속월, 필요수량 셋팅
+                if (BigDecimal.ZERO == woStocQty || BigDecimal.ZERO == mmAvDdlvQty) {
                     dvo.setStocPersMmN(0);
-                } else {
-                    dvo.setStocPersMmN(
-                        (float)Math.round(((float)dvo.getWoStocQty() / (float)dvo.getMmAvDdlvQty()) * 10) / 10
-                    );
-                }
-            }
-        });
-
-        // 예상소진일자 set
-        //        String baseDay = "";
-        String nowMonth = DateUtil.getNowDayString().substring(0, 6);
-
-        if (nowMonth.equals(dto.mngtYm())) {
-            String baseDay = DateUtil.getNowDayString();
-
-            dvos.forEach(dvo -> {
-                if ("N".equals(dvo.getRgstYn())) {
-                    try {
-                        dvo.setEtExsDt(DateUtil.addMonths(baseDay, (int)dvo.getStocPersMmN()));
-                    } catch (ParseException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-            });
-        } else {
-            String baseDay = dto.mngtYm() + "01";
-
-            dvos.forEach(dvo -> {
-                if ("N".equals(dvo.getRgstYn())) {
-                    try {
-                        dvo.setEtExsDt(DateUtil.addMonths(baseDay, (int)dvo.getStocPersMmN()));
-                    } catch (ParseException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-            });
-        }
-
-        //        dvos.forEach(dvo -> {
-        //            try {
-        //                dvo.setEtExsDt(DateUtil.addMonths(baseDay, dvo.getStocPersMmN()));
-        //            } catch (ParseException e) {
-        //                throw new RuntimeException(e);
-        //            }
-        //        });
-
-        // 필요량 set
-        dvos.forEach(dvo -> {
-            if ("N".equals(dvo.getRgstYn())) {
-                if (dvo.getMmAvDdlvQty() == 0 || dvo.getWoStocQty() == 0) {
                     dvo.setNcstQty(0);
                 } else {
-                    dvo.setNcstQty((dvo.getMmAvDdlvQty() * 3) / dvo.getWoStocQty());
+                    dvo.setStocPersMmN(Math.round(woStocQty.divide(mmAvDdlvQty).floatValue()));
+                    // 월평균배부수량 * 3 - 최종재고
+                    BigDecimal ncstQty = mmAvDdlvQty.multiply(BigDecimal.valueOf(3L)).subtract(woStocQty);
+                    dvo.setNcstQty(BigDecimal.ZERO.compareTo(ncstQty) > 0 ? 0 : ncstQty.intValue());
                 }
-            }
-        });
 
-        // 발주수량 set
-        dvos.forEach(dvo -> {
-            if ("N".equals(dvo.getRgstYn())) {
-                if (dvo.getMinOrdQty() == 0 || dvo.getNcstQty() == 0) {
-                    dvo.setGoQty(0);
+                // 최소주문수량
+                BigDecimal minOrdQty = dvo.getMinOrdQty();
+                // 필요수량
+                int ncstQty = dvo.getNcstQty();
+
+                // 발주수량 셋팅
+                if (BigDecimal.ZERO == minOrdQty || ncstQty == 0) {
+                    dvo.setGoQty(BigDecimal.ZERO);
+
+                    // 최소수량이 필요수량보다 클 경우
+                } else if (minOrdQty.intValue() >= ncstQty) {
+                    // 최소수량만큼 발주수량을 셋팅
+                    dvo.setGoQty(minOrdQty);
                 } else {
-                    if (dvo.getMinOrdQty() > dvo.getNcstQty()) {
-                        dvo.setGoQty(dvo.getMinOrdQty());
-                    } else {
-                        int i = 1;
-
-                        while (dvo.getMinOrdQty() > 0) {
-                            int goQty = dvo.getMinOrdQty() * i;
-
-                            if (dvo.getNcstQty() >= goQty) {
-                                dvo.setGoQty(goQty);
-                                break;
-                            }
-
-                            i++;
-                        }
+                    // 최소수량 < 필요수량
+                    int i = 1;
+                    while (minOrdQty.intValue() >= ncstQty) {
+                        // 최소수량
+                        minOrdQty = minOrdQty.multiply(BigDecimal.valueOf(i++));
                     }
+                    // 최소수량만큼 발주수량을 셋팅
+                    dvo.setGoQty(minOrdQty);
+                }
+
+                // 발주수량
+                BigDecimal goQty = dvo.getGoQty();
+                // 발주단가
+                BigDecimal goUprc = dvo.getGoUprc();
+
+                // 발주금액 셋팅
+                dvo.setGoAmt(goQty.multiply(goUprc));
+
+                // 재고지속월
+                int stocPersMmN = dvo.getStocPersMmN();
+
+                try {
+                    // 예상소진일자 셋팅
+                    dvo.setEtExsDt(DateUtil.addMonths(baseDay, stocPersMmN));
+                } catch (ParseException e) {
+                    throw new BizException(e);
                 }
             }
-        });
-
-        // 발주금액 set
-        dvos.forEach(dvo -> {
-            if ("N".equals(dvo.getRgstYn())) {
-                dvo.setGoAmt(dvo.getGoQty() * dvo.getGoUprc());
-            }
-        });
+        }
 
         return dvos;
     }
 
+    /**
+     * BS 소모품 발주수량 산출 저장
+     * @param dtos
+     * @return
+     */
     @Transactional
-    public int createBsCsmbGiveAOrderQty(List<CreatReq> dtos) {
+    public int createBsCsmbGiveAOrderQty(List<CreateReq> dtos) {
+
         int processCount = 0;
 
-        List<String> csmbPdCds = dtos.stream().map(CreatReq::csmbPdCd).toList();
-
-        // 해당 관리년월 데이터 존재여부 확인
-        int exstBsCsmbGiveAOrderQtyYn = mapper.selectExistBsCsmbGiveAOrderQtyYn(dtos.get(0).mngtYm(), csmbPdCds);
-
-        if (exstBsCsmbGiveAOrderQtyYn > 0) { // 해당월 데이터 존재할경우 품목코드별로 삭제 후 insert
-            for (String csmbPdCd : csmbPdCds) {
-                mapper.deleteBsCsmbGiveAOrderQty(dtos.get(0).mngtYm(), csmbPdCd);
-            }
-        }
-
-        List<WsnaBsCsmbGiveAOrderDvo> dvos = converter.listCreatReqToBsCsmbGiveAOrderDvos(dtos);
+        List<WsnaBsCsmbGiveAOrderDvo> dvos = converter.mapAllCreateReqToWsnaBsCsmbGiveAOrderDvo(dtos);
 
         for (WsnaBsCsmbGiveAOrderDvo dvo : dvos) {
-            mapper.insertBsCsmbGiveAOrderQty(dvo);
-            processCount++;
+            processCount += mapper.mergeBsCsmbGiveAOrderQty(dvo);
         }
 
         return processCount;
     }
 
-    public List<ProdutCodeRes> getProductCodesByItmKndCd(String itmKndCd) {
-        return mapper.selectProductCodesByItmKndCd(itmKndCd);
-    }
 }
