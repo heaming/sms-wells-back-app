e5de4f09a8e78ec09cef7b08795bae0637918ea7
hyewon.kim <gs.piit130@kyowon.com>
2023-12-04 15:54:16 +0900

    chore: 모종 패키지 변경 서비스 수정

--- src/main/java/com/kyowon/sms/wells/web/service/interfaces/dto/WsniSidingServiceChangesDto.java
+++ src/main/java/com/kyowon/sms/wells/web/service/interfaces/dto/WsniSidingServiceChangesDto.java
@@ -1,11 +1,10 @@
 package com.kyowon.sms.wells.web.service.interfaces.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import io.swagger.annotations.ApiModel;
-
 import javax.validation.constraints.NotBlank;
 
-import jdk.jfr.Label;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import io.swagger.annotations.ApiModel;
 
 public class WsniSidingServiceChangesDto {
 
@@ -14,44 +13,39 @@ public class WsniSidingServiceChangesDto {
 
         @NotBlank
         @JsonProperty(value = "CNTR_NO")
-        String cntrNo,
+        String cntrNo, // 계약번호
 
         @NotBlank
         @JsonProperty(value = "CNTR_SN")
-        String cntrSn,
+        String cntrSn, // 계약일련번호
 
-        //AS요청구분코드 (1:패키지변경, 4:차월 방문 중지)
         @NotBlank
         @JsonProperty(value = "AS_AK_DV_CD")
-        String asAkDvCd,
+        String asAkDvCd, // AS요청구분코드 (1:패키지변경, 4:차월 방문 중지)
 
-        //요청변경일자
         @JsonProperty(value = "AK_CHDT")
-        String akChdt,
+        String akChdt, // 요청변경일자
 
-        //변경전상품코드
         @JsonProperty(value = "BFCH_PD_CD")
-        String bfchPdCd,
+        String bfchPdCd, // 변경전상품코드
 
-        //변경후상품코드
         @JsonProperty(value = "AFCH_PD_CD")
-        String afchPdCd,
+        String afchPdCd, // 변경후상품코드
 
-        //자료처리상태코드 (1:신규, 2:변경, 3:취소)
         @NotBlank
         @JsonProperty(value = "MTR_PROCS_STAT_CD")
-        String mtrProcsStatCd,
+        String mtrProcsStatCd, // 자료처리상태코드 (1:신규, 2:변경, 3:취소)
 
-        // 자유선택인 경우 제품상품코드목록 (제품상품코드,수량|제품상품코드,수량| ....)
         @JsonProperty(value = "CHO_CAPSL_CN")
-        String choCapslCn
+        String choCapslCn // 자유선택인 경우 제품상품코드목록 (제품상품코드,수량|제품상품코드,수량| ....)
+
     ) {}
 
     @ApiModel(value = "WsniSidingServiceChangesDto-SaveRes")
     public record SaveRes(
         @NotBlank
         String rsCd, // 결과코드(S:성공, F:실패)
-        String rsMsg
+        String rsMsg // 결과메시지
     ) {}
 
 }
