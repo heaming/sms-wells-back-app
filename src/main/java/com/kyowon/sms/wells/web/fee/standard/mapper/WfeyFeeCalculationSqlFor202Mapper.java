628eac264cc4a075e866c1bcf023281aa17abf4a
37612 <cws@kyowon.co.kr>
2023-12-11 16:01:54 +0900

    수수료 수수료코드(W020016 조직배출2(지점장)) 202308~ 적용

--- src/main/java/com/kyowon/sms/wells/web/fee/standard/mapper/WfeyFeeCalculationSqlFor202Mapper.java
+++ src/main/java/com/kyowon/sms/wells/web/fee/standard/mapper/WfeyFeeCalculationSqlFor202Mapper.java
@@ -1,8 +1,9 @@
 package com.kyowon.sms.wells.web.fee.standard.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import com.kyowon.sms.common.web.fee.standard.annotation.SqlMapperInfo;
 import com.kyowon.sms.common.web.fee.standard.annotation.SqlMethodInfo;
-import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 @SqlMapperInfo(organizationTypeCode = "W02")
@@ -18,7 +19,23 @@ public interface WfeyFeeCalculationSqlFor202Mapper {
      * @return 처리개수
      */
     @SqlMethodInfo(methodTypeCode = "01", methodName = "M조직 지국장 조직배출수수료", methodExplanation = "M조직 지국장 조직배출수수료 계산 SQL 매퍼")
-    Integer insertOrganizationEjectFee(String baseYm, String ogTpCd, String coCd, String feeCd, String dtaCrtFeeCd, String feeTcntDvCd, String fnlFeeYn);
+    Integer insertOrganizationEjectFee(
+        String baseYm, String ogTpCd, String coCd, String feeCd, String dtaCrtFeeCd, String feeTcntDvCd, String fnlFeeYn
+    );
+
+    /**
+    * M조직 지국장 조직배출수수료계산 SQL Mapper (W020016, 5)
+    *
+    * @param baseYm      기준년월
+    * @param ogTpCd      조직유형코드
+    * @param feeCd       수수료코드
+    * @param feeTcntDvCd 수수료차수구분코드
+    * @return 처리개수
+    */
+    @SqlMethodInfo(methodTypeCode = "01", methodName = "M조직 지국장 조직배출수수료(202308)", methodExplanation = "M조직 지국장 조직배출수수료 계산 SQL 매퍼")
+    Integer insertOrganizationEjectFee2308(
+        String baseYm, String ogTpCd, String coCd, String feeCd, String dtaCrtFeeCd, String feeTcntDvCd, String fnlFeeYn
+    );
 
     /**
      * M조직 지국장 신설지점수수료 SQL Mapper (W020025, 2)
@@ -30,7 +47,9 @@ public interface WfeyFeeCalculationSqlFor202Mapper {
      * @return 처리개수
      */
     @SqlMethodInfo(methodTypeCode = "01", methodName = "M조직 지국장 신설지점수수료", methodExplanation = "M조직 지국장 신설지점수수료 계산 SQL 매퍼")
-    Integer insertNewEstablishmentBranchFee(String baseYm, String ogTpCd, String coCd, String feeCd, String dtaCrtFeeCd, String feeTcntDvCd, String fnlFeeYn);
+    Integer insertNewEstablishmentBranchFee(
+        String baseYm, String ogTpCd, String coCd, String feeCd, String dtaCrtFeeCd, String feeTcntDvCd, String fnlFeeYn
+    );
 
     /**
      * M조직 지국장 순증관리수수료 SQL Mapper (W020025, 2)
@@ -44,7 +63,9 @@ public interface WfeyFeeCalculationSqlFor202Mapper {
      * @return
      */
     @SqlMethodInfo(methodTypeCode = "01", methodName = "M조직 지국장 순증관리수수료", methodExplanation = "M조직 지국장 순증관리수수료 계산 SQL 매퍼")
-    Integer insertNetIncreseManagementFee(String baseYm, String ogTpCd, String coCd, String feeCd, String dtaCrtFeeCd, String feeTcntDvCd, String fnlFeeYn);
+    Integer insertNetIncreseManagementFee(
+        String baseYm, String ogTpCd, String coCd, String feeCd, String dtaCrtFeeCd, String feeTcntDvCd, String fnlFeeYn
+    );
 
     /**
      * M조직 지점장 BS장려수수료계산 SQL Mapper (W020123, 1)
@@ -58,7 +79,9 @@ public interface WfeyFeeCalculationSqlFor202Mapper {
      * @return
      */
     @SqlMethodInfo(methodTypeCode = "01", methodName = "M조직 지점장 BS장려수수료", methodExplanation = "M조직 지점장 BS장려수수료 계산 SQL 매퍼")
-    Integer insertBsEncouragementFeeForBrmgr(String baseYm, String ogTpCd, String coCd, String feeCd, String dtaCrtFeeCd, String feeTcntDvCd, String fnlFeeYn);
+    Integer insertBsEncouragementFeeForBrmgr(
+        String baseYm, String ogTpCd, String coCd, String feeCd, String dtaCrtFeeCd, String feeTcntDvCd, String fnlFeeYn
+    );
 
     /**
      * M조직 지점장 유니폼수수료 SQL Mapper (W020099, 1)
@@ -70,7 +93,9 @@ public interface WfeyFeeCalculationSqlFor202Mapper {
      * @return 처리개수
      */
     @SqlMethodInfo(methodTypeCode = "01", methodName = "M조직 지점장 유니폼수수료계산", methodExplanation = "M조직 지점장 유니폼수수료 계산 SQL 매퍼")
-    Integer insertUniformFeesForBrmgr(String baseYm, String ogTpCd, String coCd, String feeCd, String dtaCrtFeeCd, String feeTcntDvCd, String fnlFeeYn);
+    Integer insertUniformFeesForBrmgr(
+        String baseYm, String ogTpCd, String coCd, String feeCd, String dtaCrtFeeCd, String feeTcntDvCd, String fnlFeeYn
+    );
 
     /******************************************* 되물림 *************************************************/
 
@@ -86,7 +111,10 @@ public interface WfeyFeeCalculationSqlFor202Mapper {
      * @return 처리개수
      */
     @SqlMethodInfo(methodTypeCode = "02", methodName = "M조직 플래너지점장 정착수수료 되물림 계산(W020080)", methodExplanation = "M조직 플래너지점장 정착수수료 되물림 계산(W020080) SQL 매퍼")
-    Integer insertPlarBrmgrSettleFeeRedf(String baseYm, String ogTpCd, String coCd, String feeCd, String dtaCrtFeeCd, String perfAgrgCrtDvCd, String apyStrtYm, String apyEndYm);
+    Integer insertPlarBrmgrSettleFeeRedf(
+        String baseYm, String ogTpCd, String coCd, String feeCd, String dtaCrtFeeCd, String perfAgrgCrtDvCd,
+        String apyStrtYm, String apyEndYm
+    );
 
     /**
      * M조직 플래너 추가장려수수료 되물림 계산 SQL Mapper (W020124)
@@ -100,7 +128,10 @@ public interface WfeyFeeCalculationSqlFor202Mapper {
      * @return 처리개수
      */
     @SqlMethodInfo(methodTypeCode = "02", methodName = "M조직 플래너 추가장려수수료 되물림 계산(W020124)", methodExplanation = "M조직 플래너 추가장려수수료 되물림 계산(W020124) SQL 매퍼")
-    Integer insertPlarSpmtEncrgRedf(String baseYm, String ogTpCd, String coCd, String feeCd, String dtaCrtFeeCd, String perfAgrgCrtDvCd, String apyStrtYm, String apyEndYm);
+    Integer insertPlarSpmtEncrgRedf(
+        String baseYm, String ogTpCd, String coCd, String feeCd, String dtaCrtFeeCd, String perfAgrgCrtDvCd,
+        String apyStrtYm, String apyEndYm
+    );
 
     /**
      * M조직 지점장 조직관리수수료 되물림 계산 SQL Mapper (W020018, 3)
@@ -113,7 +144,9 @@ public interface WfeyFeeCalculationSqlFor202Mapper {
      * @return 처리개수
      */
     @SqlMethodInfo(methodTypeCode = "02", methodName = "M조직 지점장 조직관리수수료 되물림 계산(W020018, 3)", methodExplanation = "M조직 지점장 조직관리수수료 되물림 계산(W020018, 3) SQL 매퍼")
-    Integer insertBrmgrOgMngtFeeRedfForApyTn3(String baseYm, String ogTpCd, String coCd, String feeCd, String dtaCrtFeeCd, String apyStrtYm, String apyEndYm);
+    Integer insertBrmgrOgMngtFeeRedfForApyTn3(
+        String baseYm, String ogTpCd, String coCd, String feeCd, String dtaCrtFeeCd, String apyStrtYm, String apyEndYm
+    );
 
     /**
      * M조직 지점장 조직관리수수료 되물림 계산 SQL Mapper (W020018, 4)
@@ -126,5 +159,7 @@ public interface WfeyFeeCalculationSqlFor202Mapper {
      * @return 처리개수
      */
     @SqlMethodInfo(methodTypeCode = "02", methodName = "M조직 지점장 조직관리수수료 되물림 계산(W020018, 4)", methodExplanation = "M조직 지점장 조직관리수수료 되물림 계산(W020018, 4) SQL 매퍼")
-    Integer insertBrmgrOgMngtFeeRedfForApyTn4(String baseYm, String ogTpCd, String coCd, String feeCd, String dtaCrtFeeCd, String apyStrtYm, String apyEndYm);
+    Integer insertBrmgrOgMngtFeeRedfForApyTn4(
+        String baseYm, String ogTpCd, String coCd, String feeCd, String dtaCrtFeeCd, String apyStrtYm, String apyEndYm
+    );
 }
