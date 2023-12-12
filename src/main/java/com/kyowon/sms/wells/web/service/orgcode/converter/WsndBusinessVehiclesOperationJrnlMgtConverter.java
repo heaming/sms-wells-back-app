c8012b5ee79a4ed66bdb2deedb9cf79c8f130c82
조혜미 <hey._.mi@kyowon.co.kr>
2023-12-12 17:21:46 +0900

    fix: 회사설치 현황 쿼리 수정 * wsnc-company-installation-mapper.xml

---
+++ src/main/java/com/kyowon/sms/wells/web/service/orgcode/converter/WsndBusinessVehiclesOperationJrnlMgtConverter.java
@@ -0,0 +1,9 @@
+package com.kyowon.sms.wells.web.service.orgcode.converter;
+
+import com.kyowon.sms.wells.web.service.orgcode.dvo.WsndBusinessVehiclesOperationJrnlMgtDvo;
+
+public interface WsndBusinessVehiclesOperationJrnlMgtConverter {
+    WsndBusinessVehiclesOperationJrnlMgtDvo mapEditBusinessVehiclesOperationJrnlMgt(
+        WsndBusinessVehiclesOperationJrnlMgtDvo dvo
+    ); //
+}
