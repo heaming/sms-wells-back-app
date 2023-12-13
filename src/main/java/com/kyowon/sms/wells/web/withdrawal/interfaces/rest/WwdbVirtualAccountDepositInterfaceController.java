264b997c7e0fe55d5d0c9b5420da759ad6509192
jaehaYeon <gs.piit167@kyowon.com>
2023-12-13 18:41:43 +0900

    chore : 가상계좌 입금처리
    Signed-off-by: jaehaYeon <gs.piit167@kyowon.com>

---
+++ src/main/java/com/kyowon/sms/wells/web/withdrawal/interfaces/rest/WwdbVirtualAccountDepositInterfaceController.java
@@ -0,0 +1,49 @@
+package com.kyowon.sms.wells.web.withdrawal.interfaces.rest;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kyowon.sms.wells.web.withdrawal.interfaces.dto.WwdbVirtualAccountDepositInterfaceDto.SearchReq;
+import com.kyowon.sms.wells.web.withdrawal.interfaces.dto.WwdbVirtualAccountDepositInterfaceDto.SearchRes;
+import com.kyowon.sms.wells.web.withdrawal.interfaces.service.WwdbVirtualAccountDepositInterfaceService;
+import com.kyowon.sms.wells.web.withdrawal.zcommon.constants.WdWithdrawalConst;
+import com.sds.sflex.system.config.annotation.InterfaceController;
+import com.sds.sflex.system.config.webclient.ivo.EaiWrapper;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+
+@InterfaceController
+@Api(tags = "[WWDB] wells 가상계좌 입금 처리 I/F API")
+@RequestMapping(value = WdWithdrawalConst.INTERFACE_URL_V1 + "/idvrve/batch-service")
+@RequiredArgsConstructor
+@Validated
+public class WwdbVirtualAccountDepositInterfaceController {
+
+    private final WwdbVirtualAccountDepositInterfaceService service;
+
+    @ApiOperation(value = "[EAI_] wells 가상계좌 입금 처리")
+    @PostMapping("/virtual-account-deposit")
+    public EaiWrapper saveVirtualAccountDeposit(
+        @Valid
+        @RequestBody
+        EaiWrapper<SearchReq> reqWrapper
+    ) {
+        // Response용 EaiWrapper 생성
+        EaiWrapper<List<SearchRes>> resWrapper = reqWrapper.newResInstance();
+        // 서비스 메소드 호출
+        List<SearchRes> res = service.saveVirtualAccountDeposit(reqWrapper.getBody());
+
+        // Response Body 세팅
+        resWrapper.setBody(res);
+
+        return resWrapper;
+    }
+}
