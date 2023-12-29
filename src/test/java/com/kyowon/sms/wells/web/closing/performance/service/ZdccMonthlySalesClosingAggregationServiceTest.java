eae5ae68400364f2b76d2fcf5a9254841a2ac13b
gugyeongu <gs.piit163@kyowon.co.kr>
2023-12-29 16:42:59 +0900

    fix : 서비스 다시 조정
    * edu, wells -> common

--- src/test/java/com/kyowon/sms/wells/web/closing/performance/service/ZdccMonthlySalesClosingAggregationServiceTest.java
+++ src/test/java/com/kyowon/sms/wells/web/closing/performance/service/ZdccMonthlySalesClosingAggregationServiceTest.java
@@ -1,5 +1,6 @@
 package com.kyowon.sms.wells.web.closing.performance.service;
 
+import com.kyowon.sms.common.web.closing.performance.service.ZdccMonthlySalesClosingAggregationService;
 import com.sds.sflex.system.config.test.SpringTestSupport;
 import lombok.RequiredArgsConstructor;
 import org.assertj.core.api.Assertions;
@@ -8,12 +9,13 @@ import org.junit.jupiter.api.Test;
 
 @RequiredArgsConstructor
 class ZdccMonthlySalesClosingAggregationServiceTest extends SpringTestSupport {
-    private final WdccMonthlySalesClosingAggregationService service;
+    private final ZdccMonthlySalesClosingAggregationService service;
 
     @Test
     @DisplayName("WELLS 매출 월마감 내역 집계 테스트")
     void createSalesMonthlyClosingDetailsTally() {
-        int result = service.createSalesMonthlyClosingDetailsTallyForWells("20231228", "W20196086124", "1");
+        // service.createSalesMonthlyClosingDetailsTallyForWells("20231228", "W20196086124", "1");
+        int result = service.createSalesMonthlyClosingDetailsTallyForWells("20231025", "W20233701936", "1");
         Assertions.assertThat(result).isEqualTo(1);
     }
 }
