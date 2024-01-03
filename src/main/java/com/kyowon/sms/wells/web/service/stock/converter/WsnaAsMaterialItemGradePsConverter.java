3e53641c78407560aaf7c6abfa1c9b05b2ca572c
gs.piit238 <gs.piit238@kyowon.co.kr>
2024-01-03 11:11:21 +0900

    chore : 자재구분 조건 변경

--- src/main/java/com/kyowon/sms/wells/web/service/stock/converter/WsnaAsMaterialItemGradePsConverter.java
+++ src/main/java/com/kyowon/sms/wells/web/service/stock/converter/WsnaAsMaterialItemGradePsConverter.java
@@ -1,13 +1,14 @@
 package com.kyowon.sms.wells.web.service.stock.converter;
 
+import static com.kyowon.sms.wells.web.service.stock.dto.WsnaAsMaterialItemGradePsDto.SearchReq;
+
 import org.mapstruct.Mapper;
 
-import com.kyowon.sms.wells.web.service.stock.dto.WsnaAsMaterialItemGradePsDto;
 import com.kyowon.sms.wells.web.service.stock.dvo.WsnaAsMaterialItemGradePsDvo;
 
 @Mapper(componentModel = "spring")
 public interface WsnaAsMaterialItemGradePsConverter {
 
-    WsnaAsMaterialItemGradePsDvo mapSearchReqToWsnaAsMaterialItemGradePsDvo(WsnaAsMaterialItemGradePsDto.SearchReq dto);
+    WsnaAsMaterialItemGradePsDvo mapSearchReqToWsnaAsMaterialItemGradePsDvo(SearchReq dto);
 
 }
