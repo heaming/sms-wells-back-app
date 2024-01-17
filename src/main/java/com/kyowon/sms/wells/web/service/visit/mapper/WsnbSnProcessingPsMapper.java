4d9dce03ba1d80ff464ab76761cf30fa3e5cf023
조혜미 <hey._.mi@kyowon.co.kr>
2024-01-17 15:02:58 +0900

    chore: DEF_4786 [S/N 처리 현황] 모종 관련 수정 / 고객서명 api 추가 * wsnb-snb-processing-ps.xml * WsnbSnProcessingPsController.java * WsnbSnProcessingPsDto.java * WsnbSnProcessingPsMapper.java * WsnbSnProcessingPsService.java

--- src/main/java/com/kyowon/sms/wells/web/service/visit/mapper/WsnbSnProcessingPsMapper.java
+++ src/main/java/com/kyowon/sms/wells/web/service/visit/mapper/WsnbSnProcessingPsMapper.java
@@ -18,4 +18,5 @@ public interface WsnbSnProcessingPsMapper {
 
     List<SearchPuPartPdRes> selectSnProcessingPsPuPartPds(SearchPuPartPdReq dto);
 
+    SearchCstSignCn selectSnProcessingPsCstSignCn(String cstSvAsnNo);
 }
