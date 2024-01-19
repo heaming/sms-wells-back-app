850458d426bc08753f772d665a993cb210fe498b
gs.piit238 <gs.piit238@kyowon.co.kr>
2024-01-19 10:59:24 +0900

    chore : 실사재고 수정

--- src/main/java/com/kyowon/sms/wells/web/service/stock/dvo/WsnaStockAcinspRgstMngtDvo.java
+++ src/main/java/com/kyowon/sms/wells/web/service/stock/dvo/WsnaStockAcinspRgstMngtDvo.java
@@ -9,11 +9,8 @@ public class WsnaStockAcinspRgstMngtDvo {
     String apyYm; /* 적용년월 */
     String statusT; /* 상태값 */
     String wareNo; /* 창고번호 */
-    String wareNm; /* 창고명 */
     String itmKnd; /* 품목구분 */
-    String sapCd; /* SAP코드*/
     String itmPdCd; /* 품목상품코드 */
-    String pdAbbrNm; /* 품목약어명 */
     int acinspQty; /* 실사수량 */
     int eotStoc; /* 기말수량 */
     int minusQty; /* 차이수량*/
