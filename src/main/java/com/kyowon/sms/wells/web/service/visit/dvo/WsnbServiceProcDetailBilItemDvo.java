a801aa708314114706d8f5c6ce2cf8577e3573f0
gs.piit231 <gs.piit231@kyowon.co.kr>
2024-01-23 18:33:40 +0900

    fix: 서비스처리상세내역 작업취소 및 컨택등록 추가

--- src/main/java/com/kyowon/sms/wells/web/service/visit/dvo/WsnbServiceProcDetailBilItemDvo.java
+++ src/main/java/com/kyowon/sms/wells/web/service/visit/dvo/WsnbServiceProcDetailBilItemDvo.java
@@ -1,5 +1,6 @@
 package com.kyowon.sms.wells.web.service.visit.dvo;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,4 +13,17 @@ public class WsnbServiceProcDetailBilItemDvo {
     String bilCtrSumAmt;
     String bilRveAmt;
 
+    String cttRcpDtm; // 컨택일[컨택접수일시]
+    String cttDuedt; // 방문예정일[컨택예정일]
+    String cttMoCn; // 전달사항[컨택요청내용]
+    String cntrNo; // 계약번호
+    String cntrSn; // 계약일련번호
+    String cttOjId; // 컨택대상ID
+    String cttSn; // 컨택일련번호
+    String wkKnd; // 작업구분
+    String beforeRcpDtm; // 기등록 컨택일
+    String beforeDuedt; // 기등록 방문예정일
+    String beforeMoCn; // 기등록 전달사항
+    String cttPrgsStatCd; // 컨택진행상태코드
+    String cstNo; // 고객번호
 }
