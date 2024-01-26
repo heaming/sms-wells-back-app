73e30d4726e108ece306ffe313b9118892287539
gs.piit231 <gs.piit231@kyowon.co.kr>
2024-01-26 18:26:19 +0900

    fix: 엑셀다운로드 대용량으로 변경

--- src/main/java/com/kyowon/sms/wells/web/service/allocate/mapper/WsncRoutineBsPsicAssignStateMapper.java
+++ src/main/java/com/kyowon/sms/wells/web/service/allocate/mapper/WsncRoutineBsPsicAssignStateMapper.java
@@ -7,6 +7,7 @@ import org.apache.ibatis.annotations.Mapper;
 
 import com.kyowon.sms.wells.web.service.allocate.dto.WsncRoutineBsPsicAssignStateDto.SearchReq;
 import com.kyowon.sms.wells.web.service.allocate.dto.WsncRoutineBsPsicAssignStateDto.SearchRes;
+import com.kyowon.sms.wells.web.service.allocate.dto.WsncRoutineBsPsicAssignStateDto.BulkExcelSearchRes;
 import com.kyowon.sms.wells.web.service.allocate.dvo.WsncRoutineBsPsicAssignStateMngrInfoDvo;
 import com.sds.sflex.system.config.datasource.PageInfo;
 import com.sds.sflex.system.config.datasource.PagingResult;
@@ -17,6 +18,7 @@ public interface WsncRoutineBsPsicAssignStateMapper {
     PagingResult<SearchRes> selectRoutineBsPsicAssignState(SearchReq dto, PageInfo pageInfo);
 
     List<SearchRes> selectRoutineBsPsicAssignState(SearchReq dto);
+    List<BulkExcelSearchRes> selectRoutineBsPsicAssignStateBulkExcelDownload(SearchReq dto);
 
     List<HashMap<String, String>> selectWellsManager(String dgr2LevlOgId);
 
