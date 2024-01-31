f8285b89e5029c192528459e79aec882b8e6354c
gs.piit231 <gs.piit231@kyowon.co.kr>
2024-01-31 17:21:11 +0900

    fix: 정기 B/S 담당자 배정 현황 그리드 셋팅 및 엑셀 필드 출력 수정

--- src/main/java/com/kyowon/sms/wells/web/service/allocate/dto/WsncRoutineBsPsicAssignStateDto.java
+++ src/main/java/com/kyowon/sms/wells/web/service/allocate/dto/WsncRoutineBsPsicAssignStateDto.java
@@ -38,326 +38,215 @@ public class WsncRoutineBsPsicAssignStateDto {
 
     @ApiModel(value = "WsncRoutineBsPsicAssignStateDto-SearchRes")
     public record SearchRes(
-        String chk,
         String snRpblYn,
         String cntrCstNo,//new
         String cstGdCd,
         String cstGdNm,
-
         String cstCnttYn,
         String vstPrgsStatCd,
         String vstPrgsStatNm,
         String statDtl,
         String cntrNo,
-
         String cntrSn,
         String cntrDtlNo,
         String rcgvpKnm,
         String cralLocaraTno,
         @DBDecField
         String mexnoEncr, //휴대전화국번호암호화(휴대폰번호)
-
         String cralIdvTno,
         String locaraTno,
         @DBDecField
         String exnoEncr, //전화국번호암호화(전화번호)
         String idvTno,
         String pdCd,
-
         String pdNm,
         String sellTpCd,
         String sellTpNm,
         String sellTpDtlCd,
         String sellTpDtlNm,
-
         String mngerRglvlDvCd,
         String mngerRglvlDvNm,
         String newAdrZip,
         String rndadr,
         String rltdSvBizDclsfCd,//new
-
         String clsfCdSrnPrntCn,//new
         String bfVstDuedt,
         String vstDuedt,
         String vstCnfmdt,
         String vstCnfmHh,
-
         String vstFshDt,
         String vstFshHh,
         String cstUnuitmCn,
         String fstRgstDtm,
         String l1HgrOgCd,//new
-
         String l2HgrOgCd,//new
         String l3HgrOgCd,//new
         String l1HgrOgNm,//new
         String l2HgrOgNm,//new
         String l3HgrOgNm,//new
-
         String cnfmPsicDvCd,//new
         String cnfmPsicPrtnrOgTpCd,//new
         String cnfmPsicPrtnrNo,//new
         String prtnrKnm,
         String pstnDvCd,
-
         String pstnDvNm,//new
         String siteAwAtcCd,
         String siteAwAtcNm,
         String engPrtnrGdCd,//new
         String awAmt,
-
         String svProcsCn,
         String cstSignCn,
         String bgColo,
         String cntrDtlStatCd,
         String cntrDtlStatNm,
-
         String cstSvAsnNo,
         String pdAbbrNm,
         String ogNm,
         String ogCd,
         String prtnrNo,
-
-        String fstRgstTm
+        String fstRgstTm,
+        String svBizDclsfNm,
+        String spcAsTpCd
     ) {
         @AutomapConstructor
         public SearchRes(
-            String chk,
             String snRpblYn,
             String cntrCstNo,//new
             String cstGdCd,
             String cstGdNm,
-
             String cstCnttYn,
             String vstPrgsStatCd,
             String vstPrgsStatNm,
             String statDtl,
             String cntrNo,
-
             String cntrSn,
             String cntrDtlNo,
             String rcgvpKnm,
             String cralLocaraTno,
             String mexnoEncr, //휴대전화국번호암호화(휴대폰번호)
-
             String cralIdvTno,
             String locaraTno,
             String exnoEncr, //전화국번호암호화(전화번호)
             String idvTno,
             String pdCd,
-
             String pdNm,
             String sellTpCd,
             String sellTpNm,
             String sellTpDtlCd,
             String sellTpDtlNm,
-
             String mngerRglvlDvCd,
             String mngerRglvlDvNm,
             String newAdrZip,
             String rndadr,
             String rltdSvBizDclsfCd,//new
-
             String clsfCdSrnPrntCn,//new
             String bfVstDuedt,
             String vstDuedt,
             String vstCnfmdt,
             String vstCnfmHh,
-
             String vstFshDt,
             String vstFshHh,
             String cstUnuitmCn,
             String fstRgstDtm,
             String l1HgrOgCd,//new
-
             String l2HgrOgCd,//new
             String l3HgrOgCd,//new
             String l1HgrOgNm,//new
             String l2HgrOgNm,//new
             String l3HgrOgNm,//new
-
             String cnfmPsicDvCd,//new
             String cnfmPsicPrtnrOgTpCd,//new
             String cnfmPsicPrtnrNo,//new
             String prtnrKnm,
             String pstnDvCd,
-
             String pstnDvNm,//new
             String siteAwAtcCd,
             String siteAwAtcNm,
             String engPrtnrGdCd,//new
             String awAmt,
-
             String svProcsCn,
             byte[] cstSignCn,
             String bgColo,
             String cntrDtlStatCd,
             String cntrDtlStatNm,
-
             String cstSvAsnNo,
             String pdAbbrNm,
             String ogNm,
             String ogCd,
             String prtnrNo,
-
-            String fstRgstTm
+            String fstRgstTm,
+            String svBizDclsfNm,
+            String spcAsTpCd
         ) {
             this(
-                chk,
                 snRpblYn,
                 cntrCstNo,//new
                 cstGdCd,
                 cstGdNm,
-
                 cstCnttYn,
                 vstPrgsStatCd,
                 vstPrgsStatNm,
                 statDtl,
                 cntrNo,
-
                 cntrSn,
                 cntrDtlNo,
                 rcgvpKnm,
                 cralLocaraTno,
                 StringUtils.isNotEmpty(mexnoEncr) ? DbEncUtil.dec(mexnoEncr) : mexnoEncr,
-
                 cralIdvTno,
                 locaraTno,
                 StringUtils.isNotEmpty(exnoEncr) ? DbEncUtil.dec(exnoEncr) : exnoEncr,
                 idvTno,
                 pdCd,
-
                 pdNm,
                 sellTpCd,
                 sellTpNm,
                 sellTpDtlCd,
                 sellTpDtlNm,
-
                 mngerRglvlDvCd,
                 mngerRglvlDvNm,
                 newAdrZip,
                 rndadr,
                 rltdSvBizDclsfCd,//new
-
                 clsfCdSrnPrntCn,//new
                 bfVstDuedt,
                 vstDuedt,
                 vstCnfmdt,
                 vstCnfmHh,
-
                 vstFshDt,
                 vstFshHh,
                 cstUnuitmCn,
                 fstRgstDtm,
                 l1HgrOgCd,//new
-
                 l2HgrOgCd,//new
                 l3HgrOgCd,//new
                 l1HgrOgNm,//new
                 l2HgrOgNm,//new
                 l3HgrOgNm,//new
-
                 cnfmPsicDvCd,//new
                 cnfmPsicPrtnrOgTpCd,//new
                 cnfmPsicPrtnrNo,//new
                 prtnrKnm,
                 pstnDvCd,
-
                 pstnDvNm,//new
                 siteAwAtcCd,
                 siteAwAtcNm,
                 engPrtnrGdCd,//new
                 awAmt,
-
                 svProcsCn,
                 cstSignCn != null ? Base64.getEncoder().encodeToString(cstSignCn) : "",
                 bgColo,
                 cntrDtlStatCd,
                 cntrDtlStatNm,
-
                 cstSvAsnNo,
                 pdAbbrNm,
                 ogNm,
                 ogCd,
                 prtnrNo,
-
-                fstRgstTm
+                fstRgstTm,
+                svBizDclsfNm,
+                spcAsTpCd
             );
         }
     }
-
-    @ApiModel(value = "WsncRoutineBsPsicAssignStateDto-BulkExcelSearchRes")
-    public record BulkExcelSearchRes(
-        String snRpblYn,
-        String cntrCstNo,//new
-        String cstGdCd,
-        String cstGdNm,
-
-        String cstCnttYn,
-        String vstPrgsStatCd,
-        String vstPrgsStatNm,
-        String statDtl,
-        String cntrNo,
-
-        String cntrSn,
-        String cntrDtlNo,
-        String rcgvpKnm,
-        String cralLocaraTno,
-        String mexnoEncr, //휴대전화국번호암호화(휴대폰번호)
-
-        String cralIdvTno,
-        String locaraTno,
-        String exnoEncr, //전화국번호암호화(전화번호)
-        String idvTno,
-        String pdCd,
-
-        String pdNm,
-        String sellTpCd,
-        String sellTpNm,
-        String sellTpDtlCd,
-        String sellTpDtlNm,
-
-        String mngerRglvlDvCd,
-        String mngerRglvlDvNm,
-        String newAdrZip,
-        String rndadr,
-        String rltdSvBizDclsfCd,//new
-
-        String clsfCdSrnPrntCn,//new
-        String bfVstDuedt,
-        String vstDuedt,
-        String vstCnfmdt,
-        String vstCnfmHh,
-
-        String vstFshDt,
-        String vstFshHh,
-        String cstUnuitmCn,
-        String fstRgstDtm,
-        String l1HgrOgCd,//new
-
-        String l2HgrOgCd,//new
-        String l3HgrOgCd,//new
-        String l1HgrOgNm,//new
-        String l2HgrOgNm,//new
-        String l3HgrOgNm,//new
-
-        String cnfmPsicDvCd,//new
-        String cnfmPsicPrtnrOgTpCd,//new
-        String cnfmPsicPrtnrNo,//new
-        String prtnrKnm,
-        String pstnDvCd,
-
-        String pstnDvNm,//new
-        String siteAwAtcCd,
-        String siteAwAtcNm,
-        String engPrtnrGdCd,//new
-        String awAmt,
-
-        String svProcsCn,
-        String bgColo,
-        String cntrDtlStatCd,
-        String cntrDtlStatNm,
-
-        String cstSvAsnNo
-    ) {}
 }
