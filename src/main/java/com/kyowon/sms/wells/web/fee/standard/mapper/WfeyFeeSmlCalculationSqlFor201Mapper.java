8ba5e7c5225c9b0438603974967ec4fe1e92458d
gs.itdev000 <gs.itdev000@kyowon.co.kr>
2024-02-13 09:00:28 +0900

    fix : wells M 상조수수료 2024.01월 기준적용 SQL 재약정 추가 및 sim sql 수수료 추가

--- src/main/java/com/kyowon/sms/wells/web/fee/standard/mapper/WfeyFeeSmlCalculationSqlFor201Mapper.java
+++ src/main/java/com/kyowon/sms/wells/web/fee/standard/mapper/WfeyFeeSmlCalculationSqlFor201Mapper.java
@@ -23,9 +23,10 @@ public interface WfeyFeeSmlCalculationSqlFor201Mapper {
      * @return 처리개수
      */
     @SqlMethodInfo(methodTypeCode = "04", methodName = "M조직 판매자 정착수수료계산", methodExplanation = "M조직 판매자 정착수수료 계산 SQL 매퍼")
-    Integer insertSettlementFeesForSeller(String baseYm, String ogTpCd, String coCd, String feeCd, String dtaCrtFeeCd,
-                                          String fnlFeeYn, String mmAcuPerfAgrgCrtDvCd, String smlCrtTpCd, String prtnrNo);
-
+    Integer insertSettlementFeesForSeller(
+        String baseYm, String ogTpCd, String coCd, String feeCd, String dtaCrtFeeCd,
+        String fnlFeeYn, String mmAcuPerfAgrgCrtDvCd, String smlCrtTpCd, String prtnrNo
+    );
 
     /**
      * M조직 판매자 유니폼수수료 SQL Mapper (W020098, 1)
@@ -42,8 +43,10 @@ public interface WfeyFeeSmlCalculationSqlFor201Mapper {
      * @return 처리개수
      */
     @SqlMethodInfo(methodTypeCode = "04", methodName = "M조직 판매자 유니폼수수료계산", methodExplanation = "M조직 판매자 유니폼수수료 계산 SQL 매퍼")
-    Integer insertUniformFeesForSeller(String baseYm, String ogTpCd, String coCd, String feeCd, String dtaCrtFeeCd,
-                                       String fnlFeeYn, String mmAcuPerfAgrgCrtDvCd, String smlCrtTpCd, String prtnrNo);
+    Integer insertUniformFeesForSeller(
+        String baseYm, String ogTpCd, String coCd, String feeCd, String dtaCrtFeeCd,
+        String fnlFeeYn, String mmAcuPerfAgrgCrtDvCd, String smlCrtTpCd, String prtnrNo
+    );
 
     /**
      * M조직 판매자 BS장려수수료계산 SQL Mapper (W020122, 1)
@@ -60,8 +63,10 @@ public interface WfeyFeeSmlCalculationSqlFor201Mapper {
      * @return 처리개수
      */
     @SqlMethodInfo(methodTypeCode = "04", methodName = "M조직 판매자 BS장려수수료계산", methodExplanation = "M조직 판매자 BS장려수수료 계산 SQL 매퍼")
-    Integer insertBsEncouragementFeeForSeller(String baseYm, String ogTpCd, String coCd, String feeCd, String dtaCrtFeeCd,
-                                              String fnlFeeYn, String mmAcuPerfAgrgCrtDvCd, String smlCrtTpCd, String prtnrNo);
+    Integer insertBsEncouragementFeeForSeller(
+        String baseYm, String ogTpCd, String coCd, String feeCd, String dtaCrtFeeCd,
+        String fnlFeeYn, String mmAcuPerfAgrgCrtDvCd, String smlCrtTpCd, String prtnrNo
+    );
 
     /**
      * M조직 플래너 추가장려수수료계산 SQL Mapper (W020124, 1)
@@ -78,6 +83,28 @@ public interface WfeyFeeSmlCalculationSqlFor201Mapper {
      * @return 처리개수
      */
     @SqlMethodInfo(methodTypeCode = "04", methodName = "M조직 플래너 추가장려수수료계산", methodExplanation = "M조직 플래너 추가장려수수료 계산 SQL 매퍼")
-    Integer insertSupplementaryEncrgFeeForPlanner(String baseYm, String ogTpCd, String coCd, String feeCd, String dtaCrtFeeCd,
-                                                  String fnlFeeYn, String mmAcuPerfAgrgCrtDvCd, String smlCrtTpCd, String prtnrNo);
+    Integer insertSupplementaryEncrgFeeForPlanner(
+        String baseYm, String ogTpCd, String coCd, String feeCd, String dtaCrtFeeCd,
+        String fnlFeeYn, String mmAcuPerfAgrgCrtDvCd, String smlCrtTpCd, String prtnrNo
+    );
+
+    /**
+     /* 2024.01 이후 상조개인 W020125, W020127 (15, 7) (계약단위) 수수료 계산 SQL
+     *
+     * @param baseYm      기준년월
+     * @param coCd      회사코드
+     * @param ogTpCd      조직유형코드
+     * @param feeCd       수수료코드
+     * @param dtaCrtFeeCd 데이터생성수수료코드
+     * @param fnlFeeYn 최종수수료여부
+     * @param mmAcuPerfAgrgCrtDvCd 월누적실적집계생성구분코드
+     * @param smlCrtTpCd 시뮬레이션생성유형코드
+     * @param prtnrNo 파트너번호
+     * @return 처리개수
+     */
+    @SqlMethodInfo(methodTypeCode = "04", methodName = "M조직 플래너(지점장) 개인 상조 수료계산", methodExplanation = "M조직 플래너(지점장) 개인 상조 수료계산 SQL 매퍼")
+    Integer insertManagerLifeAlncFee(
+        String baseYm, String ogTpCd, String coCd, String feeCd, String dtaCrtFeeCd,
+        String fnlFeeYn, String mmAcuPerfAgrgCrtDvCd, String smlCrtTpCd, String prtnrNo
+    );
 }
