5baad4f0679eb3e355e4b5dd49ccfe388e4f6ab0
park yesol <gs.piit239@kyowon.co.kr>
2024-02-15 15:46:01 +0900

    fix: 알려Zoom - 조직유형 코드 추가

--- src/main/java/com/kyowon/sms/wells/web/competence/educations/dvo/WpsbZoomMgtDvo.java
+++ src/main/java/com/kyowon/sms/wells/web/competence/educations/dvo/WpsbZoomMgtDvo.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class WpsbZoomMgtDvo {
+    private String ogTpCd;      /* 조직유형코드 */
     private String svEducMnalId; /* 서비스교육매뉴얼ID */
     private String hgrSvEducMnalId; /* 상위서비스교육매뉴얼ID */
     private String svEducMnalNm; /* 서비스교육매뉴얼명 */
@@ -15,4 +16,5 @@ public class WpsbZoomMgtDvo {
     private String svEducMnalCn; /* 서비스교육매뉴얼내용 */
     private String rsbDvCd; /* 직책구분코드 */
     private String dtaDlYn; /* 데이터삭제여부 */
+    private String rowState;
 }
